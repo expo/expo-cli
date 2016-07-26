cb6e08717b88fd572a374df0fcc10e5806864718
Brent Vatne <brentvatne@gmail.com>
2016-07-25 18:31:35 -0700

    [exp] Remove flag for exp build for @next tag

--- dev/exp/src/commands/build.js
+++ dev/exp/src/commands/build.js
@@ -7,49 +7,41 @@ import IOSBuilder from './build/IOSBuilder';
 import AndroidBuilder from './build/AndroidBuilder';
 import BuildError from './build/BuildError';
 
-let command;
+export default (program: any) => {
+  program
+    .command('build:ios [project-dir]')
+    .alias('bi')
+    .option('-c, --clear-credentials', 'Clear stored credentials.')
+    .description('Build a standalone IPA for your project, signed and ready for submission to the Apple App Store.')
+    .asyncActionProjectDir((projectDir, options) => {
+      const iosBuilder = new IOSBuilder(projectDir, options);
+      return iosBuilder.command();
+    });
 
-if (process.env.TURTLE_POWER) {
-  command = (program: any) => {
-    program
-      .command('build:ios [project-dir]')
-      .alias('bi')
-      .option('-c, --clear-credentials', 'Clear stored credentials.')
-      .description('Build a standalone IPA for your project, signed and ready for submission to the Apple App Store.')
-      .asyncActionProjectDir((projectDir, options) => {
-        const iosBuilder = new IOSBuilder(projectDir, options);
-        return iosBuilder.command();
-      });
+  program
+    .command('build:android [project-dir]')
+    .alias('bi')
+    .option('-c, --clear-credentials', 'Clear stored credentials.')
+    .description('Build a standalone APK for your project, signed and ready for submission to the Apple App Store.')
+    .asyncActionProjectDir((projectDir, options) => {
+      const androidBuilder = new AndroidBuilder(projectDir, options);
+      return androidBuilder.command();
+    });
 
-    program
-      .command('build:android [project-dir]')
-      .alias('bi')
-      .option('-c, --clear-credentials', 'Clear stored credentials.')
-      .description('Build a standalone APK for your project, signed and ready for submission to the Apple App Store.')
-      .asyncActionProjectDir((projectDir, options) => {
-        const androidBuilder = new AndroidBuilder(projectDir, options);
-        return androidBuilder.command();
-      });
-
-    program
-      .command('build:status [project-dir]')
-      .alias('bs')
-      .description(`Gets the status of a current (or most recently finished) build for your project.`)
-      //.help('You must have the server running for this command to work')
-      .asyncActionProjectDir(async (projectDir, options) => {
-        const builder = new BaseBuilder(projectDir, options);
-        try {
-          return await builder.checkStatus(false);
-        } catch (e) {
-          if (e instanceof BuildError) {
-            return;
-          }
-          throw e;
+  program
+    .command('build:status [project-dir]')
+    .alias('bs')
+    .description(`Gets the status of a current (or most recently finished) build for your project.`)
+    //.help('You must have the server running for this command to work')
+    .asyncActionProjectDir(async (projectDir, options) => {
+      const builder = new BaseBuilder(projectDir, options);
+      try {
+        return await builder.checkStatus(false);
+      } catch (e) {
+        if (e instanceof BuildError) {
+          return;
         }
-      });
-  };
-} else {
-  command = () => ({});
-}
-
-export default command;
+        throw e;
+      }
+    });
+};
