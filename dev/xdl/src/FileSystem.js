7825324eb47ac1e9908ea8633029fcff6d5bebaa
Jesse Ruder <jesse@exp.host>
2016-08-29 17:33:25 -0700

    [xde] [xdl] Fix project open commands on windows

--- dev/xdl/src/FileSystem.js
+++ dev/xdl/src/FileSystem.js
@@ -2,16 +2,53 @@
  * @flow
  */
 
-import osascript from '@exponent/osascript';
+import child_process from 'child_process';
+import spawnAsync from '@exponent/spawn-async';
 
-export async function openFinderToFolderAsync(dir: string) {
-  return await osascript.openFinderToFolderAsync(dir);
+import ErrorCode from './ErrorCode';
+import XDLError from './XDLError';
+
+let osascript;
+if (process.platform === 'darwin') {
+  osascript = require('@exponent/osascript');
 }
 
-export async function openFolderInItermOrTerminalAsync(dir: string) {
-  return await osascript.openFolderInTerminalAppAsync(dir);
+export function openFolderName() {
+  if (process.platform === 'darwin') {
+    return 'Show in Finder';
+  } else if (process.platform === 'win32') {
+    return 'Show in File Explorer'
+  }
+}
+
+export function openConsoleName() {
+  if (process.platform === 'darwin') {
+    return 'Open in Terminal';
+  } else if (process.platform === 'win32') {
+    return 'Open in Cmd'
+  }
+}
+
+export async function openFolderAsync(dir: string) {
+  if (process.platform === 'darwin') {
+    return await osascript.openFinderToFolderAsync(dir);
+  } else if (process.platform === 'win32') {
+    return await spawnAsync('explorer', [dir]);
+  }
+}
+
+export async function openConsoleAsync(dir: string) {
+  if (process.platform === 'darwin') {
+    return await osascript.openFolderInTerminalAppAsync(dir);
+  } else if (process.platform === 'win32') {
+    child_process.exec(`start cmd /K "cd /d ${dir}"`);
+  }
 }
 
 export async function openProjectInEditorAsync(dir: string) {
-  return await osascript.openInEditorAsync(dir);
+  if (process.platform === 'darwin') {
+    return await osascript.openInEditorAsync(dir);
+  } else if (process.platform === 'win32') {
+    throw new XDLError(ErrorCode.PLATFORM_NOT_SUPPORTED, 'openProjectInEditorAsync not supported');
+  }
 }
