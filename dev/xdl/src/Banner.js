99cc93eb63ea1ccdba05fc6ec4a7863583509351
Adam Perry <adam.n.perry@gmail.com>
2016-12-02 16:38:00 -0700

    [xdl] Add a colored ASCII banner.

---
+++ dev/xdl/src/Banner.js
@@ -0,0 +1,13 @@
+import chalk from 'chalk';
+
+const exponentBanner = '      ' + chalk.blue('########') + '         ______                                   _\n' +
+  '     ' + chalk.blue('##########') + '       |  ____|                                 | |\n' +
+  '    ' + chalk.blue('#####  #####') + '      | |__  __  ___ __   ___  _ __   ___ _ __ | |_\n' +
+  '   ' + chalk.blue('#####    #####') + '     |  __| \\ \\/ / \'_ \\ / _ \\| \'_ \\ / _ \\ \'_ \\| __|\n' +
+  '  ' + chalk.blue('#####      #####') + '    | |____ >  <| |_) | (_) | | | |  __/ | | | |_\n' +
+  ' ' + chalk.blue('#####        #####') + '   |______/_/\\_\\ .__/ \\___/|_| |_|\\___|_| |_|\\__|\n' +
+  chalk.blue('#####          #####') + '              | |\n' +
+  chalk.blue('####            ####') + '              |_|   ' + chalk.underline(chalk.cyan('https://getexponent.com'));
+
+
+export { exponentBanner };
