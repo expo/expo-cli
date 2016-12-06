9b9b7a0941c542231ab49a75fb0fd9ddc35874f1
James Ide <ide@exp.host>
2016-12-06 15:34:23 -0800

    [XDL] Fix postinstall script when installed from another package
    Move from gulp to a plain JS script (this is still x-plat unlike bash so that ex: Microsoft can use it in VSCode)

---
+++ dev/xdl/scripts/postinstall.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const path = require('path');
+const rimraf = require('rimraf');
+
+function postinstall(done) {
+  let dtraceProviderPath;
+  try {
+    dtraceProviderPath = require.resolve('dtrace-provider');
+  } catch (error) {
+    // dtrace-provider is not installed or has been removed; do nothing
+    done();
+    return;
+  }
+
+  // Find the package directory path
+  while (dtraceProviderPath !== path.dirname(dtraceProviderPath) &&
+      !path.dirname(dtraceProviderPath).endsWith(`${path.sep}node_modules`)) {
+    dtraceProviderPath = path.dirname(dtraceProviderPath);
+  }
+
+  console.log('Deleting dtrace-provider from XDL...');
+  rimraf(dtraceProviderPath, done);
+}
+
+if (module === require.main) {
+  postinstall((error) => {
+    if (error) {
+      console.error(`Something went wrong with XDL's postinstall script:`);
+      console.error(error);
+    }
+  });
+}
