08569bc5762c16d9aa07bed9a1591f4c81e5e771
Jesse Ruder <jesse@getexponent.com>
2016-12-08 10:04:10 -0800

    [xdl] Move cloning code up in detach

--- dev/xdl/src/detach/Detach.js
+++ dev/xdl/src/detach/Detach.js
@@ -34,21 +34,6 @@ const EXPONENT_SRC_URL = 'https://github.com/exponentjs/exponent.git';
 const EXPONENT_ARCHIVE_URL = 'https://api.github.com/repos/exponentjs/exponent/tarball/master';
 const DETACH_DIRECTORIES = ['exponent', 'ios', 'android'];
 
-function validateArgs(args) {
-  if (!args.outputDirectory) {
-    throw new Error(`Must specify --outputDirectory <path to create detached project>`);
-  }
-  if (!args.sdkVersion) {
-    // TODO: maybe just default to newest?
-    // we will need to figure out newest SDK version anyway to configure the kernel properly.
-    throw new Error(`Must specify --sdkVersion <sdk version of project to detach>`);
-  }
-  if (!args.url) {
-    throw new Error(`Must specify --url <url of project to detach>`);
-  }
-  return args;
-}
-
 function validateManifest(manifest) {
   if (!manifest.name) {
     throw new Error('Manifest is missing `name`');
@@ -68,7 +53,7 @@ async function configureDetachedVersionsPlistAsync(configFilePath, detachedSDKVe
 }
 
 async function cleanPropertyListBackupsAsync(configFilePath) {
-  console.log('Cleaning up...');
+  console.log('Cleaning up plist...');
   await cleanIOSPropertyListBackupAsync(configFilePath, 'EXShell', false);
   await cleanIOSPropertyListBackupAsync(configFilePath, 'Info', false);
   await cleanIOSPropertyListBackupAsync(configFilePath, 'EXSDKVersions', false);
@@ -93,6 +78,9 @@ export async function detachAsync(projectRoot) {
   let experienceName = `@${username}/${exp.slug}`;
   let experienceUrl = `exp://exp.host/${experienceName}`;
 
+  console.log('Validating project manifest...');
+  exp = validateManifest(exp);
+
   // Check to make sure project isn't detached already
   let badDirectories = [];
   for (let i = 0; i < DETACH_DIRECTORIES.length; i++) {
@@ -105,11 +93,20 @@ export async function detachAsync(projectRoot) {
     throw new XDLError(ErrorCode.DIRECTORY_ALREADY_EXISTS, `Error detaching. Please remove ${badDirectories.join(', ')} director${badDirectories.length === 1 ? 'y' : 'ies'} first. Are you sure you aren't already detached?`);
   }
 
-  await detachIOSAsync({
-    outputDirectory: projectRoot,
-    sdkVersion: exp.sdkVersion,
-    url: experienceUrl,
-  }, exp);
+  let tmpExponentDirectory = path.join(projectRoot, 'exponent-src-tmp');
+
+  // Download exponent repo
+  console.log('Downloading Exponent kernel...');
+  // TODO: Make this method work
+  // await spawnAsync(`/usr/bin/curl -L ${EXPONENT_ARCHIVE_URL} | tar xzf -`, null, { shell: true });
+  await spawnAsyncThrowError('/usr/bin/git', ['clone', EXPONENT_SRC_URL, tmpExponentDirectory]);
+
+  // iOS
+  await detachIOSAsync(projectRoot, tmpExponentDirectory, exp.sdkVersion, experienceUrl, exp);
+
+  // Clean up
+  console.log('Cleaning up...');
+  await spawnAsync('/bin/rm', ['-rf', tmpExponentDirectory]);
 }
 
 /**
@@ -117,28 +114,17 @@ export async function detachAsync(projectRoot) {
  *  @param args.url url of the Exponent project.
  *  @param args.outputDirectory directory to create the detached project.
  */
-export async function detachIOSAsync(args, manifest) {
+export async function detachIOSAsync(projectRoot, tmpExponentDirectory, sdkVersion, experienceUrl, manifest) {
   if (process.platform === 'win32') {
     return;
   }
 
-  args = validateArgs(args);
-
-  console.log('Validating project manifest...');
-  manifest = validateManifest(manifest);
-
-  let tmpExponentDirectory = path.join(args.outputDirectory, 'exponent-src-tmp');
-  let exponentDirectory = path.join(args.outputDirectory, 'exponent');
-  let iosProjectDirectory = path.join(args.outputDirectory, 'ios');
+  let exponentDirectory = path.join(projectRoot, 'exponent');
+  let iosProjectDirectory = path.join(projectRoot, 'ios');
   let projectNameLabel = manifest.name;
   let projectName = projectNameLabel.replace(/[^a-z0-9_\-]/gi, '-').toLowerCase();
 
-  console.log('Downloading Exponent kernel...');
-  // TODO: Make this method work
-  // await spawnAsync(`/usr/bin/curl -L ${EXPONENT_ARCHIVE_URL} | tar xzf -`, null, { shell: true });
-  await spawnAsyncThrowError('/usr/bin/git', ['clone', EXPONENT_SRC_URL, tmpExponentDirectory]);
-
-  console.log('Moving project files...');
+  console.log('Moving iOS project files...');
   mkdirp.sync(exponentDirectory);
   await Utils.ncpAsync(path.join(tmpExponentDirectory, 'ios'), `${exponentDirectory}/ios`);
   await Utils.ncpAsync(path.join(tmpExponentDirectory, 'cpp'), `${exponentDirectory}/cpp`);
@@ -167,9 +153,9 @@ export async function detachIOSAsync(args, manifest) {
   let infoPlistPath = `${iosProjectDirectory}/${projectName}/Supporting`;
   let iconPath = `${iosProjectDirectory}/${projectName}/Assets.xcassets/AppIcon.appiconset`;
   await configureStandaloneIOSInfoPlistAsync(infoPlistPath, manifest);
-  await configureStandaloneIOSShellPlistAsync(infoPlistPath, manifest, args.url);
+  await configureStandaloneIOSShellPlistAsync(infoPlistPath, manifest, experienceUrl);
   // TODO: logic for when kernel sdk version is different from detached sdk version
-  await configureDetachedVersionsPlistAsync(infoPlistPath, args.sdkVersion, args.sdkVersion);
+  await configureDetachedVersionsPlistAsync(infoPlistPath, sdkVersion, sdkVersion);
   await configureIOSIconsAsync(manifest, iconPath);
   // we don't pre-cache JS in this case, TODO: think about whether that's correct
 
@@ -177,9 +163,8 @@ export async function detachIOSAsync(args, manifest) {
   await renderExponentViewPodspecAsync(`${tmpExponentDirectory}/template-files/ios/ExponentView.podspec`, `${exponentDirectory}/ExponentView.podspec`);
   await renderPodfileAsync(projectName, `${tmpExponentDirectory}/template-files/ios/ExponentView-Podfile`, `${iosProjectDirectory}/Podfile`);
 
-  console.log('Cleaning up...');
+  console.log('Cleaning up iOS...');
   await cleanPropertyListBackupsAsync(infoPlistPath);
-  await spawnAsync('/bin/rm', ['-rf', tmpExponentDirectory]);
 
   return;
 }
