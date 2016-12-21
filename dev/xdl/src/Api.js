55d26224877fc40941f1de6bc8cdd53de1ee1413
Brent Vatne <brentvatne@gmail.com>
2016-12-21 15:31:40 -0800

    [xdl] Add missing UserSettings import

--- dev/xdl/src/Api.js
+++ dev/xdl/src/Api.js
@@ -14,6 +14,7 @@ import Config from './Config';
 import * as Extract from './Extract';
 import * as Session from './Session';
 import UserManager from './User';
+import UserSettings from './UserSettings';
 
 function ApiError(code, message) {
   let err = new Error(message);
