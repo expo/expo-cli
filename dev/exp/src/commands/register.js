847b1c191f3a483ab3c184d6191c5c9df1888e91
Adam Perry <adam.n.perry@gmail.com>
2017-01-02 11:23:35 -0800

    [exp] Update help text for register command.

--- dev/exp/src/commands/register.js
+++ dev/exp/src/commands/register.js
@@ -6,7 +6,7 @@ import { register } from '../accounts';
 
 export default (program: any) => {
   program
-    .command('signup')
+    .command('register')
     .description('Sign up for a new Exponent account')
     .option('--github', 'Sign up with a GitHub account')
     .asyncAction(register);
