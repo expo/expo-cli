a6fa6ec40d32d8976c036bd0f76e41c9ef6c84f2
Ben Roth <spongebucket@gmail.com>
2017-02-13 18:34:36 -0800

    [exp] Fix arg format for prepare-detached-build

--- dev/exp/src/commands/prepare-detached-build.js
+++ dev/exp/src/commands/prepare-detached-build.js
@@ -10,7 +10,7 @@ export default (program) => {
   program
     .command('prepare-detached-build [project-dir]')
     .option('--platform [platform]', 'detached project platform')
-    .option('--skipXcodeConfig', '[iOS only] if true, do not configure Xcode project')
+    .option('--skipXcodeConfig [bool]', '[iOS only] if true, do not configure Xcode project')
     .description('Prepares a detached project for building')
     .asyncActionProjectDir(action, true, true);
 };
