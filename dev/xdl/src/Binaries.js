36e24b9d39c42e0e80e6fb720aee54409b8a697d
Adam Perry <adam.n.perry@gmail.com>
2017-03-15 22:36:50 -0700

    [xde][xdl] Move the runas dependency to XDE and fix a number of flow errors.
    Including the runas dep in xdl is causing a yarn bug to prevent installation for a number of CRNA users. Also, stray flow errors were making it harder to tell if the refactor was correct.

--- dev/xdl/src/Binaries.js
+++ dev/xdl/src/Binaries.js
@@ -6,23 +6,18 @@ import 'instapromise';
 
 import fs from 'fs';
 import hasbin from 'hasbin';
-import mkdirp from 'mkdirp';
 import spawnAsync from '@exponent/spawn-async';
 import path from 'path';
 
 import Config from './Config';
 import ErrorCode from './ErrorCode';
 import Logger from './Logger';
-import NotificationCode from './NotificationCode';
 import UserSettings from './UserSettings';
 import XDLError from './XDLError';
-import * as Utils from './Utils';
-let runas = null; // defer until used
 
 let hasSourcedBashLoginScripts = false;
 
 export const OSX_SOURCE_PATH = path.join(__dirname, '..', 'binaries', 'osx');
-const INSTALL_PATH = '/usr/local/bin';
 const ERROR_MESSAGE = '\nPlease run `npm install -g exp && exp path`';
 
 function _hasbinAsync(name) {
@@ -31,97 +26,6 @@ function _hasbinAsync(name) {
       resolve(result);
     });
   });
-}
-
-async function _binaryInstalledAsync(name) {
-  try {
-    let result = await spawnAsync('which', [name]);
-    // We add watchman to PATH when starting packager, so make sure we're not using that version
-    return (result.stdout && result.stdout.length > 1 && !result.stdout.includes(OSX_SOURCE_PATH));
-  } catch (e) {
-    console.log(e.toString());
-    return false;
-  }
-}
-
-// Sometimes `which` fails mysteriously, so try to just look for the file too.
-async function _binaryExistsAsync(name) {
-  try {
-    if (fs.lstatSync(path.join(INSTALL_PATH, name)).isSymbolicLink()) {
-      return true;
-    }
-
-    return false;
-  } catch (e) {
-    return false;
-  }
-}
-
-function _expoBinaryDirectory() {
-  let dotExpoHomeDirectory = UserSettings.dotExpoHomeDirectory();
-  let dir = path.join(dotExpoHomeDirectory, 'bin');
-  mkdirp.sync(dir);
-  return dir;
-}
-
-// Only called on darwin
-async function _installBinaryAsync(name) {
-  if (await _binaryInstalledAsync(name) || await _binaryExistsAsync(name)) {
-    return false;
-  }
-
-  try {
-    if (!runas) {
-      runas = require('runas');
-    }
-
-    // adb lives at ~/.expo/adb/adb
-    try {
-      runas('/bin/rm', ['-f', path.join(INSTALL_PATH, name)], { admin: true });
-    } catch (e) {
-      // Don't worry if we can't rm the file, we'll just get an error later
-    }
-
-    if (runas('/bin/mkdir', ['-p', INSTALL_PATH], { admin: true }) !== 0) {
-      throw new Error(`Could not run \`mkdir -p ${INSTALL_PATH}\`.`);
-    }
-
-    if (runas('/bin/ln', ['-s', path.join(_expoBinaryDirectory(), name, name), path.join(INSTALL_PATH, name)], { admin: true }) !== 0) {
-      throw new Error(`Could not symlink \`${name}\`.`);
-    }
-
-    return true;
-  } catch (e) {
-    Logger.notifications.error({code: NotificationCode.INSTALL_SHELL_COMMANDS_RESULT}, `Error installing ${name}: ${e.message}`);
-    throw e;
-  }
-}
-
-async function _copyBinariesToExpoDirAsync() {
-  if (process.platform !== 'darwin') {
-    throw new XDLError(ErrorCode.PLATFORM_NOT_SUPPORTED, 'Platform not supported.');
-  }
-
-  await Utils.ncpAsync(OSX_SOURCE_PATH, _expoBinaryDirectory());
-}
-
-export async function installShellCommandsAsync() {
-  await sourceBashLoginScriptsAsync();
-  await _copyBinariesToExpoDirAsync();
-
-  let binaries = ['adb', 'watchman', 'xde'];
-  let installedBinaries = [];
-  for (let i = 0; i < binaries.length; i++) {
-    if (await _installBinaryAsync(binaries[i])) {
-      installedBinaries.push(binaries[i]);
-    }
-  }
-
-  if (installedBinaries.length === 0) {
-    Logger.notifications.warn({code: NotificationCode.INSTALL_SHELL_COMMANDS_RESULT}, `Shell commands ${binaries.join(', ')} are already installed`);
-  } else {
-    Logger.notifications.info({code: NotificationCode.INSTALL_SHELL_COMMANDS_RESULT}, `Installed ${installedBinaries.join(', ')} to your shell`);
-  }
 }
 
 export function getBinariesPath(): string {
@@ -208,16 +112,21 @@ export async function sourceBashLoginScriptsAsync() {
   } else {
     try {
       // No ~/.expo/bashrc file found. Run `env` in process.env.SHELL.
+      const shellName = process.env.SHELL;
+      if (!shellName) {
+        throw new Error('This command requires being run within a shell.');
+      }
+
       let result;
-      if (/t?csh$/.test(process.env.SHELL)) {
+      if (/t?csh$/.test(shellName)) {
         // csh
-        result = await spawnAsync(process.env.SHELL, ['-d', '-c', 'env']);
-      } else if (/zsh$/.test(process.env.SHELL)) {
+        result = await spawnAsync(shellName, ['-d', '-c', 'env']);
+      } else if (/zsh$/.test(shellName)) {
         // zsh
-        result = await spawnAsync(process.env.SHELL, ['-l', '-c', 'env']);
+        result = await spawnAsync(shellName, ['-l', '-c', 'env']);
       } else {
         // bash, fish
-        result = await spawnAsync(process.env.SHELL, ['-l', '-c', 'env']);
+        result = await spawnAsync(shellName, ['-l', '-c', 'env']);
       }
 
       if (result.stderr) {
