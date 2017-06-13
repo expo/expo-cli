6c056ccf53eecfb054798bb16cd8e9dfe065818d
Adam Perry <adam.n.perry@gmail.com>
2017-06-13 16:04:55 -0700

    [exp] Add fetch command for iOS certificates.

--- dev/exp/src/commands/fetch.js
+++ dev/exp/src/commands/fetch.js
@@ -11,6 +11,61 @@ import log from '../log';
 
 export default (program: any) => {
   program
+    .command('fetch:ios:certs [project-dir]')
+    .description(
+      "Fetch this project's iOS certificates. Writes to PROJECT_DIR/PROJECT_NAME_(dist|push).p12 and prints passwords to stdout."
+    )
+    .asyncActionProjectDir(async (projectDir, options) => {
+      const {
+        args: {
+          username,
+          remotePackageName,
+          remoteFullPackageName: experienceName,
+        },
+      } = await Exp.getPublishInfoAsync(process.cwd());
+
+      let distOutputFile = path.resolve(`${remotePackageName}_dist.p12`);
+      let pushOutputFile = path.resolve(`${remotePackageName}_push.p12`);
+
+      const credentialMetadata = {
+        username,
+        experienceName,
+        platform: 'ios',
+      };
+
+      log(`Retreiving iOS certificates for ${experienceName}`);
+
+      try {
+        const {
+          certP12,
+          certPassword,
+          pushP12,
+          pushPassword,
+        } = await Credentials.getCredentialsForPlatform(credentialMetadata);
+
+        log(`Writing distribution cert to ${distOutputFile}...`);
+        fs.writeFileSync(distOutputFile, Buffer.from(certP12, 'base64'));
+        log('Done writing distribution cert to disk.');
+
+        log(`Writing push cert to ${pushOutputFile}...`);
+        fs.writeFileSync(pushOutputFile, Buffer.from(pushP12, 'base64'));
+        log('Done writing push cert to disk.');
+
+        log(`Save these important values as well:
+
+Distribution p12 password: ${chalk.bold(certPassword)}
+Push p12 password:         ${chalk.bold(pushPassword)}
+`);
+      } catch (e) {
+        throw new Error(
+          'Unable to fetch credentials for this project. Are you sure they exist?'
+        );
+      }
+
+      log('All done!');
+    }, true);
+
+  program
     .command('fetch:android:keystore [project-dir]')
     .description(
       "Fetch this project's Android keystore. Writes keystore to PROJECT_DIR/PROJECT_NAME.jks and prints passwords to stdout."
