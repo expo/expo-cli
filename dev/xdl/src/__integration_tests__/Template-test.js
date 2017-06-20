8c33b0a99104a61e845c29db44c00a6fb2f290ed
Jesse Ruder <jesse@getexponent.com>
2017-06-19 17:37:26 -0700

    [xdl] Fix template test

---
+++ dev/xdl/src/__integration_tests__/Template-test.js
@@ -0,0 +1,41 @@
+/**
+ * @flow
+ */
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 240000;
+import path from 'path';
+import uuid from 'uuid';
+import rimraf from 'rimraf';
+import {
+  clearXDLCacheAsync,
+  downloadTemplateApp,
+  extractTemplateApp,
+} from '../Exp';
+
+describe('Template Apps', () => {
+  it('should download the starter app template and extract it', async () => {
+    process.env.UNSAFE_EXPO_HOME_DIRECTORY = path.join(
+      '/',
+      'tmp',
+      `.expo-${uuid.v1()}`
+    );
+
+    await clearXDLCacheAsync();
+    let dir = path.join('/', 'tmp', `.expo-${uuid.v1()}`);
+    let templateDownload = await downloadTemplateApp('tabs', dir, {
+      name: `test-template-${new Date().valueOf()}`,
+      progressFunction: () => {},
+      retryFunction: () => {},
+    });
+    expect(templateDownload).not.toBeNull();
+    // Extract the template we just downloaded
+    let projectRoot = await extractTemplateApp(
+      templateDownload.starterAppPath,
+      templateDownload.name,
+      templateDownload.root
+    );
+
+    rimraf.sync(process.env.UNSAFE_EXPO_HOME_DIRECTORY);
+    rimraf.sync(dir);
+  });
+});
