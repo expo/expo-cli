e6cb220ef167a54b582185f6e34fbad29b46c3de
Brent Vatne <brentvatne@gmail.com>
2017-07-25 18:19:30 -0700

    [exp] Add exp install:ios and exp install:android

---
+++ dev/exp/src/commands/install.js
@@ -0,0 +1,28 @@
+/**
+ * @flow
+ */
+
+import { Android, Simulator } from 'xdl';
+import log from '../log';
+
+export default (program: any) => {
+  program
+    .command('install:ios')
+    .description(
+      'Install the latest version of Expo Client for iOS on the simulator'
+    )
+    .asyncAction(async () => {
+      await Simulator.upgradeExpoAsync();
+      log('Done!');
+    }, true);
+
+  program
+    .command('install:android')
+    .description(
+      'Install the latest version of Expo Client for Android on a connected device or emulator'
+    )
+    .asyncAction(async () => {
+      await Android.upgradeExpoAsync();
+      log('Done!');
+    }, true);
+};
