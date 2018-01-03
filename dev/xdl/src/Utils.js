35f9d519ef7dcb60c1e82c91a5e104361d6edb2b
Hugo Dozois <hugo@letsroundup.com>
2018-01-03 15:39:57 +0200

    [xdl] Fixed issue where promise was rejected with null
    This makes it really hard to find the underlying issue. Rejecting error at least gives a stack trace.
    
    Closes expo/xdl#20

--- dev/xdl/src/Utils.js
+++ dev/xdl/src/Utils.js
@@ -8,7 +8,7 @@ export function ncpAsync(source: string, dest: string, options: any = {}) {
   return new Promise((resolve, reject) => {
     ncp(source, dest, options, err => {
       if (err) {
-        reject();
+        reject(err);
       } else {
         resolve();
       }
