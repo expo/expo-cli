c8c6f68c222090a36b3e105eb1bf4a68b17cbf2c
Ville Immonen <ville.immonen@iki.fi>
2018-03-03 19:02:32 +0200

    [exp][cli] Display correct program name (exp or expo)

--- dev/exp/src/commands/publish.js
+++ dev/exp/src/commands/publish.js
@@ -30,7 +30,7 @@ export async function action(projectDir: string, options: Options = {}) {
 
   let startedOurOwn = false;
   if (status !== 'running') {
-    log('Unable to find an existing exp instance for this directory, starting a new one...');
+    log(`Unable to find an existing ${options.parent.name} instance for this directory, starting a new one...`);
     installExitHooks(projectDir);
 
     const startOpts = { reset: options.clear, nonPersistent: true };
