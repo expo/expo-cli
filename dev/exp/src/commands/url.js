78a0af4030515c3b484d5bd5c38495ec0d427182
Mark Glagola <mark.glagola@gmail.com>
2018-03-06 17:16:23 +0200

    [exp] Ability to stdout standalone app url via url:ipa and url:apk cmds
    @fson - Per our discussion to merge some of [exptool](https://github.com/mglagola/exptool) functionality with `exp`!
    
    This functionality will allow for commands like:
    ```bash
    curl -o app.ipa "$(exp url:ipa)"
    curl -o app.apk "$(exp url:apk)"
    ```
    
    Closes expo/exp#103

--- dev/exp/src/commands/url.js
+++ dev/exp/src/commands/url.js
@@ -1,10 +1,26 @@
 import chalk from 'chalk';
+import fp from 'lodash/fp';
 
 import { Project, UrlUtils } from 'xdl';
 
 import CommandError from '../CommandError';
 import log from '../log';
 import urlOpts from '../urlOpts';
+
+const logArtifactUrl = (platform) => async (projectDir, options) => {
+  const res = await Project.buildAsync(projectDir, { current: false, mode: 'status' });
+  const url = fp.compose(
+    fp.get(['artifacts', 'url']),
+    fp.head,
+    fp.filter(job => platform && job.platform === platform),
+    fp.getOr([], 'jobs')
+  )(res);
+  if (url) {
+    log.nested(url);
+  } else {
+    throw new Error(`No ${platform} binary file found. Use "exp build:${platform}" to create one.`);
+  }
+}
 
 async function action(projectDir, options) {
   await urlOpts.optsAsync(projectDir, options);
@@ -35,4 +51,16 @@ export default program => {
     .allowOffline()
     .allowNonInteractive()
     .asyncActionProjectDir(action, /* skipProjectValidation: */ true, /* skipAuthCheck: */ true);
+
+  program
+    .command('url:ipa [project-dir]')
+    .description('Displays the standalone iOS binary URL you can use to download your app binary')
+    .allowNonInteractive()
+    .asyncActionProjectDir(logArtifactUrl('ios'), true);
+
+  program
+    .command('url:apk [project-dir]')
+    .description('Displays the standalone Android binary URL you can use to download your app binary')
+    .allowNonInteractive()
+    .asyncActionProjectDir(logArtifactUrl('android'), true);
 };
