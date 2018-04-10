84e13e02cde88780f2805e701a74a88f20139c1b
Ville Immonen <ville.immonen@iki.fi>
2018-04-11 00:36:08 +0300

    [xdl] Add missing double quotes to the module resolution error

--- dev/xdl/src/logs/PackagerLogsStream.js
+++ dev/xdl/src/logs/PackagerLogsStream.js
@@ -388,7 +388,7 @@ export default class PackagerLogsStream {
         return `You attempted attempted to import the Node standard library module "${moduleName}" from "${relativePath}". It failed because React Native does not include the Node standard library. Read more at ${DOCS_PAGE_URL}`;
       }
     }
-    return `Unable to resolve ${moduleName}" from "${relativePath}"`;
+    return `Unable to resolve "${moduleName}" from "${relativePath}"`;
   }
 
   _formatBundlingError(error: any) {
