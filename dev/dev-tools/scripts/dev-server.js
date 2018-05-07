143664d5686a7aa3859cef991e1dfe7dd5fa4e7f
Ville Immonen <ville.immonen@iki.fi>
2018-05-07 17:06:24 +0300

    [dev-tools] Add a separate .babelrc for the server

--- dev/dev-tools/scripts/dev-server.js
+++ dev/dev-tools/scripts/dev-server.js
@@ -1,5 +1,4 @@
 require('babel-register')({
-  presets: ['latest'],
   only: ['dev-tools/server/*.js', 'dev-tools/server/graphql/*.js', 'dev-tools/scripts/*.js'],
 });
 require('../server/dev-server.js');
