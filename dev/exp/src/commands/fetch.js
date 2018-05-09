8949f90bdc856483cd2fa2c8aa35c53dcd9a61f1
Jakub Stępak <jakub.ste@gmail.com>
2018-05-09 20:50:56 +0200

    [exp][docs] Simplify getting Android key hashes (#2364)
    * [exp][docs] Simplify getting Android key hashes
    
    * [exp][docs] Separate fetch:hashes from fetch:keystore and better error handling
    
    * [exp] Review changes
    
    * [exp] More review changes

--- dev/exp/src/commands/fetch.js
+++ dev/exp/src/commands/fetch.js
@@ -6,8 +6,61 @@ import chalk from 'chalk';
 import fs from 'fs';
 import path from 'path';
 import { Credentials, Exp } from 'xdl';
-
+import crypto from 'crypto';
+import spawnAsync from '@expo/spawn-async';
 import log from '../log';
+
+function exportCertAsync(keystoreFile, keystorePassword, keyAlias, certFile) {
+  return spawnAsync('keytool', [
+    '-exportcert',
+    '-keystore',
+    keystoreFile,
+    '-storepass',
+    keystorePassword,
+    '-alias',
+    keyAlias,
+    '-file',
+    certFile,
+    '-noprompt',
+    '-storetype',
+    'JKS',
+  ]);
+}
+
+async function writeKeystore(projectDir, temporary = false) {
+  const {
+    args: { username, remotePackageName, remoteFullPackageName: experienceName },
+  } = await Exp.getPublishInfoAsync(projectDir);
+
+  const outputFile = path.resolve(projectDir, `${remotePackageName}${temporary ? '.tmp' : ''}.jks`);
+  const credentialMetadata = { username, experienceName, platform: 'android' };
+
+  log(`Retreiving Android keystore for ${experienceName}`);
+  const credentials: ?AndroidCredentials = await Credentials.getCredentialsForPlatform(
+    credentialMetadata
+  );
+
+  if (!credentials) {
+    throw new Error(
+      'Unable to fetch credentials for this project. Are you sure they exist? Did you build a standalone Android app?'
+    );
+  }
+
+  const { keystore, keystorePassword, keystoreAlias: keyAlias, keyPassword } = credentials;
+  const storeBuf = Buffer.from(keystore, 'base64');
+
+  if (!temporary) {
+    log(`Writing keystore to ${outputFile}...`);
+  }
+  fs.writeFileSync(outputFile, storeBuf);
+
+  return {
+    outputFile,
+    keystorePassword,
+    keyPassword,
+    keyAlias,
+  };
+}
 
 export default (program: any) => {
   program
@@ -85,38 +138,71 @@ Push p12 password:         ${chalk.bold(pushPassword)}
       "Fetch this project's Android keystore. Writes keystore to PROJECT_DIR/PROJECT_NAME.jks and prints passwords to stdout."
     )
     .asyncActionProjectDir(async (projectDir, options) => {
-      const {
-        args: { username, remotePackageName, remoteFullPackageName: experienceName },
-      } = await Exp.getPublishInfoAsync(projectDir);
+      const { keystorePassword, keyPassword, keyAlias } = await writeKeystore(projectDir);
 
-      let outputFile = path.resolve(projectDir, `${remotePackageName}.jks`);
-
-      const credentialMetadata = { username, experienceName, platform: 'android' };
-
-      log(`Retreiving Android keystore for ${experienceName}`);
-
-      const credentials: ?AndroidCredentials = await Credentials.getCredentialsForPlatform(
-        credentialMetadata
-      );
-
-      if (!credentials) {
-        throw new Error('Unable to fetch credentials for this project. Are you sure they exist?');
-      }
-
-      const { keystore, keystorePassword, keystoreAlias: keyAlias, keyPassword } = credentials;
-
-      const storeBuf = Buffer.from(keystore, 'base64');
-
-      log(`Writing keystore to ${outputFile}...`);
-      fs.writeFileSync(outputFile, storeBuf);
       log('Done writing keystore to disk.');
-
       log(`Save these important values as well:
 
 Keystore password: ${chalk.bold(keystorePassword)}
 Key alias:         ${chalk.bold(keyAlias)}
 Key password:      ${chalk.bold(keyPassword)}
 `);
+
+      log('All done!');
+    }, true);
+
+  program
+    .command('fetch:android:hashes [project-dir]')
+    .description(
+      "Fetch this project's Android key hashes needed to setup Google/Facebook authentication."
+    )
+    .asyncActionProjectDir(async (projectDir, options) => {
+      const { outputFile, keystorePassword, keyAlias } = await writeKeystore(projectDir, true);
+      const certFile = outputFile.replace('jks', 'cer');
+      try {
+        await exportCertAsync(outputFile, keystorePassword, keyAlias, certFile);
+        const data = fs.readFileSync(certFile);
+        const googleHash = crypto
+          .createHash('sha1')
+          .update(data)
+          .digest('hex')
+          .toUpperCase();
+        const fbHash = crypto
+          .createHash('sha1')
+          .update(data)
+          .digest('base64');
+        log(`Google Certificate Fingerprint:  ${googleHash.replace(/(.{2}(?!$))/g, '$1:')}`);
+        log(`Google Certificate Hash:         ${googleHash}`);
+        log(`Facebook Key Hash:               ${fbHash}`);
+      } catch (err) {
+        if (err.code === 'ENOENT') {
+          log.warn('Are you sure you have keytool installed?');
+          log('keytool is part of openJDK: http://openjdk.java.net/');
+          log('Also make sure that keytool is in your PATH after installation.');
+        }
+        if (err.stdout) {
+          log(err.stdout);
+        }
+        if (err.stderr) {
+          log.error(err.stderr);
+        }
+        throw err;
+      } finally {
+        try {
+          fs.unlinkSync(certFile);
+        } catch (err) {
+          if (err.code !== 'ENOENT') {
+            log.error(err);
+          }
+        }
+        try {
+          fs.unlinkSync(outputFile);
+        } catch (err) {
+          if (err.code !== 'ENOENT') {
+            log.error(err);
+          }
+        }
+      }
 
       log('All done!');
     }, true);
