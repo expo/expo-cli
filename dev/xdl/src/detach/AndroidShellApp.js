272e36e83940704a19deaa8fde3556af695ddb90
Jesse Ruder <jesse@expo.io>
2018-06-29 17:27:44 -0700

    [android] Fix issues in turtle builds caused by detach and unimodules changes

--- dev/xdl/src/detach/AndroidShellApp.js
+++ dev/xdl/src/detach/AndroidShellApp.js
@@ -252,6 +252,7 @@ export async function copyInitialShellAppFilesAsync(
   await copyToShellApp('settings.gradle');
   await copyToShellApp('debug.keystore');
   await copyToShellApp('run.sh');
+  await copyToShellApp('maven'); // this is a symlink
 }
 
 exports.createAndroidShellAppAsync = async function createAndroidShellAppAsync(args: any) {
