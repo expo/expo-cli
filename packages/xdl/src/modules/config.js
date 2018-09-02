811310a3e01dc77f2cd489b1b628d7e56125ee38
Eric Samelson <esamelson@users.noreply.github.com>
2018-09-02 07:09:49 -0700

    [xdl] add sdkVersions to unimodules config (#21)
    * [xdl] stop appending index.exp to manifest requests, add appropriate Accept headers
    
    * [xdl] configure filenames/dirs for selfhosted apps to play well with OTA updates
    
    * [xdl] add sdkVersions to module config

--- packages/xdl/src/modules/config.js
+++ packages/xdl/src/modules/config.js
@@ -30,40 +30,66 @@ const defaultUniversalModuleConfig = {
 
 const expoSdkUniversalModules = [
   // versioned modules
-  { podName: 'EXGL', libName: 'expo-gl' },
-  { podName: 'EXSMS', libName: 'expo-sms' },
-  { podName: 'EXCore', libName: 'expo-core' },
-  { podName: 'EXFont', libName: 'expo-font' },
-  { podName: 'EXPrint', libName: 'expo-print' },
-  { podName: 'EXCamera', libName: 'expo-camera' },
-  { podName: 'EXSensors', libName: 'expo-sensors' },
-  { podName: 'EXLocation', libName: 'expo-location' },
-  { podName: 'EXConstants', libName: 'expo-constants' },
-  { podName: 'EXFileSystem', libName: 'expo-file-system' },
-  { podName: 'EXPermissions', libName: 'expo-permissions' },
-  { podName: 'EXSegment', libName: 'expo-analytics-segment' },
-  { podName: 'EXMediaLibrary', libName: 'expo-media-library' },
-  { podName: 'EXFontInterface', libName: 'expo-font-interface' },
-  { podName: 'EXCameraInterface', libName: 'expo-camera-interface' },
-  { podName: 'EXSensorsInterface', libName: 'expo-sensors-interface' },
-  { podName: 'EXConstantsInterface', libName: 'expo-constants-interface' },
-  { podName: 'EXReactNativeAdapter', libName: 'expo-react-native-adapter' },
-  { podName: 'EXFileSystemInterface', libName: 'expo-file-system-interface' },
-  { podName: 'EXPermissionsInterface', libName: 'expo-permissions-interface' },
-  { podName: 'EXImageLoaderInterface', libName: 'expo-image-loader-interface' },
-  { podName: 'EXFaceDetectorInterface', libName: 'expo-face-detector-interface' },
+  { podName: 'EXGL', libName: 'expo-gl', sdkVersions: '>=29.0.0' },
+  { podName: 'EXSMS', libName: 'expo-sms', sdkVersions: '>=29.0.0' },
+  { podName: 'EXCore', libName: 'expo-core', sdkVersions: '>=29.0.0' },
+  { podName: 'EXFont', libName: 'expo-font', sdkVersions: '>=30.0.0' },
+  { podName: 'EXPrint', libName: 'expo-print', sdkVersions: '>=30.0.0' },
+  { podName: 'EXCamera', libName: 'expo-camera', sdkVersions: '>=29.0.0' },
+  { podName: 'EXSensors', libName: 'expo-sensors', sdkVersions: '>=29.0.0' },
+  { podName: 'EXLocation', libName: 'expo-location', sdkVersions: '>=30.0.0' },
+  { podName: 'EXConstants', libName: 'expo-constants', sdkVersions: '>=29.0.0' },
+  { podName: 'EXFileSystem', libName: 'expo-file-system', sdkVersions: '>=29.0.0' },
+  { podName: 'EXPermissions', libName: 'expo-permissions', sdkVersions: '>=29.0.0' },
+  { podName: 'EXSegment', libName: 'expo-analytics-segment', sdkVersions: '>=30.0.0' },
+  { podName: 'EXMediaLibrary', libName: 'expo-media-library', sdkVersions: '>=30.0.0' },
+  { podName: 'EXFontInterface', libName: 'expo-font-interface', sdkVersions: '>=30.0.0' },
+  { podName: 'EXCameraInterface', libName: 'expo-camera-interface', sdkVersions: '>=29.0.0' },
+  { podName: 'EXSensorsInterface', libName: 'expo-sensors-interface', sdkVersions: '>=29.0.0' },
+  { podName: 'EXConstantsInterface', libName: 'expo-constants-interface', sdkVersions: '>=29.0.0' },
+  {
+    podName: 'EXReactNativeAdapter',
+    libName: 'expo-react-native-adapter',
+    sdkVersions: '>=29.0.0',
+  },
+  {
+    podName: 'EXFileSystemInterface',
+    libName: 'expo-file-system-interface',
+    sdkVersions: '>=29.0.0',
+  },
+  {
+    podName: 'EXPermissionsInterface',
+    libName: 'expo-permissions-interface',
+    sdkVersions: '>=29.0.0',
+  },
+  {
+    podName: 'EXImageLoaderInterface',
+    libName: 'expo-image-loader-interface',
+    sdkVersions: '>=30.0.0',
+  },
+  {
+    podName: 'EXFaceDetectorInterface',
+    libName: 'expo-face-detector-interface',
+    sdkVersions: '>=29.0.0',
+  },
   {
     podName: 'EXFaceDetector',
     libName: 'expo-face-detector',
     detachable: false,
+    sdkVersions: '>=29.0.0',
   },
-  { podName: 'EXBarCodeScannerInterface', libName: 'expo-barcode-scanner-interface' },
-  { podName: 'EXBarCodeScanner', libName: 'expo-barcode-scanner' },
+  {
+    podName: 'EXBarCodeScannerInterface',
+    libName: 'expo-barcode-scanner-interface',
+    sdkVersions: '>=30.0.0',
+  },
+  { podName: 'EXBarCodeScanner', libName: 'expo-barcode-scanner', sdkVersions: '>=30.0.0' },
 
   // unversioned modules
   {
     podName: 'EXGL-CPP',
     libName: 'expo-gl-cpp',
+    sdkVersions: '>=29.0.0',
     versionable: false,
     config: {
       ios: {
@@ -75,10 +101,12 @@ const expoSdkUniversalModules = [
   // JS-only modules
   {
     libName: 'expo-asset',
+    sdkVersions: '>=29.0.0',
     isNativeModule: false,
   },
   {
     libName: 'expo-module-template',
+    sdkVersions: '>=29.0.0',
     isNativeModule: false,
   },
 ];
@@ -94,10 +122,11 @@ function defaults(defaultConfig, ...customConfigs) {
 }
 
 const expoSdkUniversalModulesConfigs = expoSdkUniversalModules.map(
-  ({ config, podName, libName, isNativeModule, ...params }) => {
+  ({ config, podName, libName, sdkVersions, isNativeModule, ...params }) => {
     return {
       podName,
       libName,
+      sdkVersions,
       isNativeModule: isNativeModule == null ? true : isNativeModule,
       config: {
         ios: defaults(defaultUniversalModuleConfig.ios, params, config && config.ios),
