79d119c182b030c112aae102a1a6ffe7e9cf6b47
Ville Immonen <ville.immonen@iki.fi>
2018-10-03 18:32:52 +0300

    [expo-cli] Remove the old expo-cli folder

--- packages/expo-cli/gulpfile.js
+++ packages/expo-cli/gulpfile.js
@@ -11,8 +11,6 @@ const package = require('./package.json');
 const paths = {
   source: 'src/**/*.js',
   build: 'build',
-  builtFiles: 'build/**/*.{js,map}',
-  nextBuild: 'expo-cli/build',
 };
 
 const tasks = {
@@ -31,23 +29,16 @@ const tasks = {
       .pipe(gulp.dest(paths.build));
   },
 
-  copy() {
-    return gulp.src(paths.builtFiles).pipe(gulp.dest(paths.nextBuild));
-  },
-
   watchBabel(done) {
-    gulp.watch(paths.source, gulp.series([tasks.babel, tasks.copy]));
+    gulp.watch(paths.source, tasks.babel);
     done();
   },
 };
 
-gulp.task('build', gulp.series([tasks.babel, tasks.copy]));
+gulp.task('build', tasks.babel);
 gulp.task('watch', tasks.watchBabel);
 gulp.task('clean', done => {
-  rimraf(paths.build, error => {
-    if (error) return done(error);
-    rimraf(paths.nextBuild, done);
-  });
+  rimraf(paths.build, done);
 });
 
 gulp.task('default', gulp.series('watch'));
