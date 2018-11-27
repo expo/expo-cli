b95aa4c078a33ae385e8f2d35f1f3ee8a6608984
Ville Immonen <ville.immonen@iki.fi>
2018-11-27 14:19:55 +0000

    Code review changes

--- packages/xdl/src/Exp.js
+++ packages/xdl/src/Exp.js
@@ -61,14 +61,17 @@ export async function extractTemplateApp(
 
   let appFile = new JsonFile(path.join(projectRoot, 'app.json'));
   let appJson = await appFile.readAsync();
-  appJson.expo = { ...appJson.expo, name, slug: name };
+  appJson = {
+    ...appJson,
+    expo: { ...appJson.expo, name, slug: name },
+  };
   await appFile.writeAsync(appJson);
 
   let packageFile = new JsonFile(path.join(projectRoot, 'package.json'));
   let packageJson = await packageFile.readAsync();
   // Adding `private` stops npm from complaining about missing `name` and `version` fields.
   // We don't add a `name` field because it also exists in `app.json`.
-  packageJson.private = true;
+  packageJson = { ...packageJson, private: true };
   // These are metadata fields related to the template package, let's remove them from the package.json.
   delete packageJson.name;
   delete packageJson.version;
