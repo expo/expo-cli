0c692ceeb3760b9538d6ba0aaa389baf12f86fb9
Ville Immonen <ville.immonen@iki.fi>
2018-12-04 18:52:02 +0000

    [xdl] Remove the browser version of Raven

--- packages/xdl/src/Sentry.js
+++ packages/xdl/src/Sentry.js
@@ -1,28 +1,13 @@
 /**
  * @flow
  */
+import Raven from 'raven';
 import Config from './Config';
 
-let Raven;
-let SENTRY_DSN;
-
-if (global.XMLHttpRequest) {
-  // use browser version and DSN for xde
-  Raven = require('raven-js');
-  SENTRY_DSN = 'https://8554f14d112d4ed4b0558154762760ef@sentry.io/194120';
-} else {
-  // use node version and DSN for crna and exp
-  Raven = require('raven');
-  SENTRY_DSN = `
-    https://8554f14d112d4ed4b0558154762760ef:bae5673d5e5243abac5563d70861b5d8@sentry.io/194120
-  `;
-}
+const SENTRY_DSN =
+  'https://8554f14d112d4ed4b0558154762760ef:bae5673d5e5243abac5563d70861b5d8@sentry.io/194120';
 
 Raven.config(SENTRY_DSN).install();
-
-type TagType = {
-  [key: string]: string,
-};
 
 export function logError(message: string, options?: Object): void {
   // send error to Sentry
