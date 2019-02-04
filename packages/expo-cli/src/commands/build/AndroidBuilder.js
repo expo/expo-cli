ba0dd0524ebd0047429fe3b118de74c0e8e1e583
Rafael Lebre <rafabonalebre@gmail.com>
2019-02-04 12:37:15 -0200

    [expo-cli] Fix broken links to the docs
    Closes #350.
    Co-authored-by: Ville Immonen <ville.immonen@iki.fi>

--- packages/expo-cli/src/commands/build/AndroidBuilder.js
+++ packages/expo-cli/src/commands/build/AndroidBuilder.js
@@ -77,7 +77,7 @@ See https://docs.expo.io/versions/latest/guides/building-standalone-apps.html`);
       'Android keystores must be identical to the one previously used to submit your app to the Google Play Store.'
     );
     log.warn(
-      'Please read https://docs.expo.io/versions/latest/guides/building-standalone-apps.html#if-you-choose-to-build-for-android for more info before proceeding.'
+      'Please read https://docs.expo.io/versions/latest/distribution/building-standalone-apps#if-you-choose-to-build-for-android for more info before proceeding.'
     );
     log.warn(
       "We'll store a backup of your Android keystore in this directory in case you decide to delete it from our servers."
