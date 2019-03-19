925868522671c5e1f1e17e5b4e4b68c3b8bcf101
Evan Bacon <baconbrix@gmail.com>
2019-03-18 22:49:01 -0700

    Removed the ability to automatically use a `webpack.config.js` in the root project folder
    More valuable to import the entire config.

--- packages/webpack-config/webpack.config.js
+++ packages/webpack-config/webpack.config.js
@@ -1,7 +1,7 @@
-const developmentConfig = require('./webpack/webpack.dev');
-const productionConfig = require('./webpack/webpack.prod');
 const fs = require('fs');
 const path = require('path');
+const developmentConfig = require('./webpack/webpack.dev');
+const productionConfig = require('./webpack/webpack.prod');
 
 function invokePossibleFunction(objectOrMethod, ...args) {
   if (typeof objectOrMethod === 'function') {
@@ -11,23 +11,16 @@ function invokePossibleFunction(objectOrMethod, ...args) {
   }
 }
 
-module.exports = function(env = {}, argv) {
-  // Check if the project has a webpack.config.js in the root.
-  const projectWebpackConfig = path.resolve(process.cwd(), 'webpack.config.js');
-  if (fs.existsSync(projectWebpackConfig)) {
-    const webpackConfig = require(projectWebpackConfig);
-    return invokePossibleFunction(webpackConfig, env, argv);
+function getConfigFromRootIfPossible(filename) {
+  const overridePath = path.resolve(process.cwd(), filename);
+  if (fs.existsSync(overridePath)) {
+    return require(overridePath);
+  } else {
+    return require(path.join(__dirname, 'webpack', filename));
   }
+}
 
-  const getConfigFromRootIfPossible = filename => {
-    const overridePath = path.resolve(process.cwd(), filename);
-    if (fs.existsSync(overridePath)) {
-      return require(overridePath);
-    } else {
-      return require(path.join(__dirname, 'webpack', filename));
-    }
-  };
-
+module.exports = function(env = {}, argv) {
   if (env.development) {
     const config = getConfigFromRootIfPossible('webpack.dev.js');
     return invokePossibleFunction(config, env, argv);
