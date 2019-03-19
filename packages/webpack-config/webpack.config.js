1e4cec21eeb1fb2b1b1da28c3e95a646b5f070c0
Evan Bacon <baconbrix@gmail.com>
2019-03-19 14:01:51 -0700

    Removed root dev, and prod config options

--- packages/webpack-config/webpack.config.js
+++ packages/webpack-config/webpack.config.js
@@ -1,29 +1,10 @@
-const fs = require('fs');
-const path = require('path');
-
-function invokePossibleFunction(objectOrMethod, ...args) {
-  if (typeof objectOrMethod === 'function') {
-    return objectOrMethod(...args);
-  } else {
-    return objectOrMethod;
-  }
-}
-
-function getConfigFromRootIfPossible(filename) {
-  const overridePath = path.resolve(process.cwd(), filename);
-  if (fs.existsSync(overridePath)) {
-    return require(overridePath);
-  } else {
-    return require(path.join(__dirname, 'webpack', filename));
-  }
-}
+const developmentConfig = require('./webpack/webpack.dev');
+const productionConfig = require('./webpack/webpack.prod');
 
 module.exports = function(env = {}, argv) {
   if (env.development) {
-    const config = getConfigFromRootIfPossible('webpack.dev.js');
-    return invokePossibleFunction(config, env, argv);
+    return developmentConfig(env, argv);
   } else {
-    const config = getConfigFromRootIfPossible('webpack.prod.js');
-    return invokePossibleFunction(config, env, argv);
+    return productionConfig(env, argv);
   }
 };
