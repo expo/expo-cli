4826ef87f83b12e048f9753097eb610967a24cd8
Evan Bacon <baconbrix@gmail.com>
2019-03-19 13:46:44 -0700

    Added `--web-only` flag to CLI to make usage in `react-native init` projects work better. (#438)
    * Added a case where webOnly is enabled if `expo.platforms` only contains `web`

--- packages/xdl/src/Project.js
+++ packages/xdl/src/Project.js
@@ -1858,7 +1858,7 @@ export async function stopExpoServerAsync(projectRoot: string) {
 
 let webpackDevServerInstance;
 
-function getWebpackInstance() {
+function getWebpackInstance(projectRoot) {
   if (webpackDevServerInstance == null) {
     ProjectUtils.logError(projectRoot, 'expo', 'Webpack is not running.');
   }
@@ -1870,8 +1870,8 @@ async function startWebpackServerAsync(projectRoot, options, verbose) {
     ProjectUtils.logError(projectRoot, 'expo', 'Webpack is already running.');
     return;
   }
-  let { exp } = await ProjectUtils.readConfigJsonAsync(projectRoot);
-  if (!exp.platforms.includes('web')) {
+  const hasWebSupport = await Web.hasWebSupportAsync(projectRoot);
+  if (!hasWebSupport) {
     return;
   }
   let { dev, https } = await ProjectSettings.readAsync(projectRoot);
@@ -1899,7 +1899,7 @@ async function startWebpackServerAsync(projectRoot, options, verbose) {
 }
 
 async function stopWebpackServerAsync(projectRoot) {
-  const devServer = getWebpackInstance();
+  const devServer = getWebpackInstance(projectRoot);
   if (devServer) {
     await new Promise(resolve => devServer.close(() => resolve()));
     webpackDevServerInstance = null;
@@ -2182,8 +2182,10 @@ export async function startAsync(
     projectRoot,
     developerTool: Config.developerTool,
   });
-  await startExpoServerAsync(projectRoot);
-  await startReactNativeServerAsync(projectRoot, options, verbose);
+  if (!options.webOnly) {
+    await startExpoServerAsync(projectRoot);
+    await startReactNativeServerAsync(projectRoot, options, verbose);
+  }
   await startWebpackServerAsync(projectRoot, options, verbose);
   if (!Config.offline) {
     try {
