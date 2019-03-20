6a5fa9bb86837b4be708f2378a45b5951b8266ee
Evan Bacon <baconbrix@gmail.com>
2019-03-20 11:10:25 -0700

    Move root `webpack.config.js` check to XDL (#437)
    * Removed the ability to automatically use a `webpack.config.js` in the root project folder
    
    More valuable to import the entire config.
    
    * Added root webpack config check to xdl
    
    * Update webpack.config.js
    
    * Using correct project root
    
    * Removed root dev, and prod config options

--- packages/webpack-config/webpack.config.js
+++ packages/webpack-config/webpack.config.js
@@ -1,38 +1,10 @@
 const developmentConfig = require('./webpack/webpack.dev');
 const productionConfig = require('./webpack/webpack.prod');
-const fs = require('fs');
-const path = require('path');
-
-function invokePossibleFunction(objectOrMethod, ...args) {
-  if (typeof objectOrMethod === 'function') {
-    return objectOrMethod(...args);
-  } else {
-    return objectOrMethod;
-  }
-}
 
 module.exports = function(env = {}, argv) {
-  // Check if the project has a webpack.config.js in the root.
-  const projectWebpackConfig = path.resolve(process.cwd(), 'webpack.config.js');
-  if (fs.existsSync(projectWebpackConfig)) {
-    const webpackConfig = require(projectWebpackConfig);
-    return invokePossibleFunction(webpackConfig, env, argv);
-  }
-
-  const getConfigFromRootIfPossible = filename => {
-    const overridePath = path.resolve(process.cwd(), filename);
-    if (fs.existsSync(overridePath)) {
-      return require(overridePath);
-    } else {
-      return require(path.join(__dirname, 'webpack', filename));
-    }
-  };
-
   if (env.development) {
-    const config = getConfigFromRootIfPossible('webpack.dev.js');
-    return invokePossibleFunction(config, env, argv);
+    return developmentConfig(env, argv);
   } else {
-    const config = getConfigFromRootIfPossible('webpack.prod.js');
-    return invokePossibleFunction(config, env, argv);
+    return productionConfig(env, argv);
   }
 };
