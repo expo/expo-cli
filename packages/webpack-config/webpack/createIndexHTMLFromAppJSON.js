0776d68fd52098b92cc391523eae2b39f6215744
Evan Bacon <baconbrix@gmail.com>
2019-03-21 17:00:52 -0700

    Moved metatags

--- packages/webpack-config/webpack/createIndexHTMLFromAppJSON.js
+++ packages/webpack-config/webpack/createIndexHTMLFromAppJSON.js
@@ -1,5 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const Metatags = require('./Metatags');
 /**
  * To work with the iPhone X "notch" add `viewport-fit=cover` to the `viewport` meta tag.
  */
@@ -17,40 +17,6 @@ const DEFAULT_MINIFY = {
   minifyCSS: true,
   minifyURLs: true,
 };
-
-const ogTags = [
-  // <link rel="canonical" href="absolute-path">
-  { name: 'og:title', propNames: ['title'] },
-  { name: 'og:site_name', propNames: ['siteName'] },
-  { name: 'og:description', propNames: ['description'] },
-  { name: 'article:author', propNames: ['author'] },
-  { name: 'og:image', propNames: ['image'] },
-  { name: 'og:image:width', propNames: ['imageWidth'] },
-  { name: 'og:image:height', propNames: ['imageHeight'] },
-  { name: 'og:locale', propNames: ['locale'] },
-  // https://developers.facebook.com/docs/sharing/opengraph#types
-  { name: 'og:type', propNames: ['type'], fallback: 'website' },
-  { name: 'og:url', propNames: ['url'] },
-];
-
-const twitterTags = [
-  { name: 'twitter:card', propNames: ['card'] },
-  { name: 'twitter:title', propNames: ['title'] },
-  { name: 'twitter:description', propNames: ['description'] },
-  { name: 'twitter:site', propNames: ['site'] },
-  { name: 'twitter:image', propNames: ['image'] },
-  { name: 'twitter:creator', propNames: ['creator'] },
-];
-
-const msTags = [
-  // 'msapplication-window': `width=${microsoft.width};height=${microsoft.height}`,
-  { name: 'mssmarttagspreventparsing', propNames: ['preventParsing'] },
-  { name: 'msapplication-navbutton-color', propNames: ['buttonColor'] },
-  { name: 'application-name', propNames: ['appName'] },
-  { name: 'msapplication-tooltip', propNames: ['tooltip'] },
-  { name: 'msapplication-TileColor', propNames: ['tileColor'] },
-  { name: 'msapplication-TileImage', propNames: ['tileImage'] },
-];
 
 function possibleProperty(input, possiblePropertyNames, fallback) {
   for (const propertyName of possiblePropertyNames) {
@@ -85,9 +51,9 @@ function createIndexHTMLFromAppJSON(appManifest, locations) {
     metatags = {},
   } = web;
 
-  const openGraphMetatags = populateMetatagObject(ogTags, facebook);
-  const twitterMetatags = populateMetatagObject(twitterTags, twitter);
-  const microsoftMetatags = populateMetatagObject(msTags, microsoft);
+  const openGraphMetatags = populateMetatagObject(Metatags.openGraph, facebook);
+  const twitterMetatags = populateMetatagObject(Metatags.twitter, twitter);
+  const microsoftMetatags = populateMetatagObject(Metatags.microsoft, microsoft);
 
   const appleMetatags = {
     'format-detection': 'telephone=no',
