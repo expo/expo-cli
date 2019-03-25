958a6239087794bb9fa4c0ba3d896b5d99053f61
Dominik Sokal <dominik.sokal@swmansion.com>
2019-03-25 10:16:49 +0100

    [expo-cli] pr feedback

--- packages/expo-cli/src/commands/build/ios/utils/image.js
+++ packages/expo-cli/src/commands/build/ios/utils/image.js
@@ -1,15 +1,18 @@
 import fs from 'fs-extra';
 import { PNG } from 'pngjs';
+import pick from 'lodash/pick';
 import { XDLError, ErrorCode } from 'xdl';
 
-export async function ensurePNGIsNotTransparent(imagePath) {
+async function ensurePNGIsNotTransparent(imagePath) {
   let hasAlreadyResolved = false;
   return new Promise((res, rej) => {
-    fs.createReadStream(imagePath)
+    const stream = fs.createReadStream(imagePath);
+    stream
       .pipe(new PNG({ filterType: 4 }))
       .on('metadata', ({ colorType }) => {
         if (colorType !== 6) {
           hasAlreadyResolved = true;
+          stream.close();
           res();
         }
       })
@@ -17,21 +20,29 @@ export async function ensurePNGIsNotTransparent(imagePath) {
         if (hasAlreadyResolved) {
           return;
         }
-        for (let y = 0; y < this.height; y++) {
-          for (let x = 0; x < this.width; x++) {
-            let idx = (this.width * y + x) * 4;
-
-            if (this.data[idx + 3] !== 255) {
-              const err = new XDLError(
-                ErrorCode.INVALID_ASSETS,
-                `Your application icon (${imagePath}) can't have transparency if you wish to upload your app to Apple Store.`
-              );
-              return rej(err);
-            }
-          }
+        try {
+          valideAlphaChannelIsEmpty(this.data, pick(this, ['width', 'height']));
+        } catch (err) {
+          return rej(err);
         }
         res();
       })
       .on('error', err => rej(err));
   });
 }
+
+function valideAlphaChannelIsEmpty(data, { width, height }) {
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      let idx = (width * y + x) * 4;
+      if (data[idx + 3] !== 255) {
+        throw new XDLError(
+          ErrorCode.INVALID_ASSETS,
+          `Your application icon can't have transparency if you wish to upload your app to Apple Store.`
+        );
+      }
+    }
+  }
+}
+
+export { ensurePNGIsNotTransparent };
