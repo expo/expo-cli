062004b548d5e6cce6b3fce238f233ea826787fd
Thomas P <TPXP@users.noreply.github.com>
2019-04-15 23:18:45 +0800

    Don't use a simple spinner when waiting for the build (#382)
    * ENH: Don't use a simple spinner when waiting for the build
    
    The simple spinner can cause issues in basic terminals (e.g. CI terminals), in which it will create a new line per spinning tick. By using a more advanced spinner, we keep the spinner but avoid issues with these terminals.
    
    * Remove unused import
    
    Co-Authored-By: TPXP <TPXP@users.noreply.github.com>

--- packages/expo-cli/src/commands/build/BaseBuilder.js
+++ packages/expo-cli/src/commands/build/BaseBuilder.js
@@ -6,7 +6,7 @@ import { Project, ProjectUtils, User } from 'xdl';
 import chalk from 'chalk';
 import fp from 'lodash/fp';
 import get from 'lodash/get';
-import simpleSpinner from '@expo/simple-spinner';
+import ora from 'ora';
 
 import * as UrlUtils from '../utils/url';
 import log from '../../log';
@@ -336,10 +336,10 @@ ${job.id}
     }
 
     if (this.options.wait) {
-      simpleSpinner.start();
+      let spinner = ora(`Waiting for build to finish...`).start();
       const waitOpts = publicUrl ? { publicUrl } : {};
       const completedJob = await this.wait(buildId, waitOpts);
-      simpleSpinner.stop();
+      spinner.stop();
       const artifactUrl = completedJob.artifactId
         ? UrlUtils.constructArtifactUrl(completedJob.artifactId)
         : completedJob.artifacts.url;
