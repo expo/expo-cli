5aca5c5a4fa2738da534ccc1d41ba340382c8566
Sergei Chestakov <schestakov@ucdavis.edu>
2019-04-25 10:59:22 -0700

    Add toggle for asset optimization in dev-tools

--- packages/expo-cli/src/commands/optimize.js
+++ packages/expo-cli/src/commands/optimize.js
@@ -184,7 +184,7 @@ export default program => {
   program
     .command('optimize [project-dir]')
     .alias('o')
-    .description('Compress the assets in your expo project')
+    .description('Compress the assets in your Expo project')
     .option('-s, --save', 'Save the original assets with the extension .expo')
     .allowOffline()
     .asyncAction(action);
