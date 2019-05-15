019d493440475a76983ccdb01dd54e2689e5339c
Quinlan Jung <quin@expo.io>
2019-05-15 10:26:56 -0700

    pr feedback: use symbol as object key

--- packages/expo-cli/src/commands/client/tagger.js
+++ packages/expo-cli/src/commands/client/tagger.js
@@ -2,25 +2,26 @@
  * @flow
  */
 
+const updateSymbol = Symbol(); // a unique key to mark an object on whether we should perform an update
 class Updater {
   constructor(updateAllFn) {
     this.updateAllFn = updateAllFn;
   }
 
   async updateAllAsync(objs: Array<Object>) {
-    const taggedObjs = objs.filter(obj => obj._shouldUpdate);
+    const taggedObjs = objs.filter(obj => obj[updateSymbol]);
     clearTags(objs);
     return await this.updateAllFn(taggedObjs);
   }
 }
 
 function tagForUpdate(obj: Object) {
-  obj._shouldUpdate = true;
+  obj[updateSymbol] = true;
   return obj;
 }
 
 function clearTags(objs: Array<Object>) {
-  objs.forEach(obj => delete obj._shouldUpdate);
+  objs.forEach(obj => delete obj[updateSymbol]);
 }
 
 export { Updater, tagForUpdate, clearTags };
