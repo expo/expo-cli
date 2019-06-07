debd3f9d137f74b15bac0ef698923b87384fd952
Evan Bacon <baconbrix@gmail.com>
2019-06-07 14:09:48 -0700

    Removed unused info

--- packages/webpack-config/webpack/utils/Diagnosis.js
+++ packages/webpack-config/webpack/utils/Diagnosis.js
@@ -1,7 +1,11 @@
 const chalk = require('chalk');
 const getenv = require('getenv');
 
-//https://stackoverflow.com/a/51319962/4047926
+const diff = require('deep-diff');
+const { ensurePWAConfig, readConfigJson } = require('@expo/config');
+const getPaths = require('./getPaths');
+
+// https://stackoverflow.com/a/51319962/4047926
 function colorizeKeys(json) {
   if (typeof json !== 'string') {
     json = JSON.stringify(json, undefined, 2);
@@ -9,44 +13,40 @@ function colorizeKeys(json) {
   return json.replace(
     /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
     function(match) {
-      let cls = '\x1b[36m';
       if (/--pop/.test(match)) {
-        return chalk.red.bold(match.substring(0, match.length - 7) + '":'); // '\x1b[30m' + '+' + match.substring(0, match.length - 7) + '": ' + '\x1b[0m';
+        // Bacon: If a key is prefixed with --pop it'll be colored red
+        return chalk.red.bold(match.substring(0, match.length - 7) + '":');
       } else if (/--push/.test(match)) {
-        return chalk.green.bold(match.substring(0, match.length - 8) + '":'); // '\x1b[30m' + '+' + match.substring(0, match.length - 7) + '": ' + '\x1b[0m';
+        // Bacon: If a key is prefixed with --push it'll be colored green
+        return chalk.green.bold(match.substring(0, match.length - 8) + '":');
       } else if (/^"/.test(match)) {
         if (/:$/.test(match)) {
-          // cls = '\x1b[34m';
           return chalk.magenta(match);
         } else {
           return chalk.blueBright(match);
-          // cls = '\x1b[32m';
         }
       } else if (/true|false/.test(match)) {
-        return chalk.magenta();
+        return chalk.cyanBright(match);
       } else if (/null/.test(match)) {
-        cls = '\x1b[31m';
+        return chalk.cyan(match);
       }
-      return cls + match + '\x1b[0m';
+      return chalk.green(match);
     }
   );
 }
 
-function log(report) {
-  console.log(chalk.green('Report'));
-  console.log(colorizeKeys(report));
-}
-
+// Only diagnose the project if EXPO_DEBUG=true is set. ex: EXPO_DEBUG=true expo start
 function shouldDiagnose() {
   return getenv.boolish('EXPO_DEBUG', false);
 }
 
-function report(webpackConfig, { config, ...env } = {}) {
-  if (!shouldDiagnose()) {
-    return;
-  }
-  const paths = getPaths(env);
+function logTitle(title) {
+  console.log(chalk.bgGreen.black(`[${title}]`));
+}
 
+// Log the main risky parts of webpack.config
+function logWebpackConfigComponents(webpackConfig) {
+  logTitle('Webpack Info');
   const {
     mode,
     resolve: { alias = {} } = {},
@@ -55,23 +55,25 @@ function report(webpackConfig, { config, ...env } = {}) {
     context,
     devServer: { https, hot, contentBase } = {},
   } = webpackConfig;
-  console.log('WEBPACK', JSON.stringify(webpackConfig, null, 2));
+  // console.log('WEBPACK', JSON.stringify(webpackConfig, null, 2));
+  console.log(
+    colorizeKeys({
+      mode,
+      devtool,
+      entry,
+      context,
+      https,
+      hot,
+      contentBase,
+      alias,
+    })
+  );
+}
 
-  log({
-    mode,
-    devtool,
-    entry,
-    context,
-    https,
-    hot,
-    contentBase,
-    alias,
-    paths,
-    config,
-    env,
-  });
+function logStatics(env = {}) {
+  logTitle('Statics Info');
 
-  console.log(chalk.bold('\nStatics'));
+  const paths = getPaths(env);
 
   // Detect if the default template files aren't being used.
   const { template: statics = {} } = paths;
@@ -90,22 +92,31 @@ function report(webpackConfig, { config, ...env } = {}) {
     }
     console.log(message);
   }
-  console.log('\n');
-
-  logAutoConfigValues(env);
 }
 
-module.exports = {
-  log,
-  shouldDiagnose,
-  report,
-};
+function logEnvironment({ config, ...env } = {}) {
+  logTitle('Environment Info');
+  console.log(colorizeKeys(env));
+}
 
-const diff = require('deep-diff');
-const { ensurePWAConfig, readConfigJson } = require('@expo/config');
-const getPaths = require('./getPaths');
-const getConfig = require('../utils/getConfig');
-const getPaths = require('./getPaths');
+function setDeepValue(pathComponents, object, value) {
+  if (!pathComponents.length) {
+    return;
+  }
+  const key = pathComponents.shift();
+  if (!(key in object)) {
+    if (pathComponents.length) object[key] = {};
+    else {
+      if (value.kind === 'N') {
+        object[key] = value.lhs;
+      } else {
+        if ('rhs' in value) object[`${key}--push`] = value.rhs;
+        if ('lhs' in value) object[`${key}--pop`] = value.lhs;
+      }
+    }
+  }
+  setDeepValue(pathComponents, object[key], value);
+}
 
 function logAutoConfigValues(env) {
   const locations = getPaths(env);
@@ -121,36 +132,33 @@ function logAutoConfigValues(env) {
 
   const nonStandard = diff(standardConfig, pwaConfig);
 
-  console.log('NONNON', nonStandard);
-  function setDeepValue(pathComponents, object, value) {
-    if (!pathComponents.length) {
-      return;
-    }
-    const key = pathComponents.shift();
-    if (!(key in object)) {
-      if (pathComponents.length) object[key] = {};
-      else {
-        if (value.kind === 'N') {
-          object[key] = value.lhs;
-        } else {
-          if ('rhs' in value) object[`${key}--push`] = value.rhs;
-          if ('lhs' in value) object[`${key}--pop`] = value.lhs;
-        }
-      }
-    }
-    setDeepValue(pathComponents, object[key], value);
-  }
-
   let obj = {};
+
   for (const diff of nonStandard) {
-    // console.log(
-    //   chalk.bold(diff.path.join('/') + ': ') + chalk.bgRed(JSON.stringify(diff.rhs, null, 2))
-    // );
+    // console.log(chalk.bold(diff.path.join('/') + ': ') + chalk.bgRed(JSON.stringify(diff.rhs, null, 2)));
+
+    // TODO: Bacon: add support for array updates: https://www.npmjs.com/package/deep-diff#differences
     if (diff.kind !== 'A') {
       setDeepValue(diff.path, obj, diff);
     }
   }
 
-  console.log(chalk.bold('Internal Config:\n'));
+  logTitle('App.json');
   console.log(colorizeKeys(obj));
 }
+
+function report(webpackConfig, { config, ...env } = {}) {
+  if (!shouldDiagnose()) {
+    return;
+  }
+
+  logWebpackConfigComponents(webpackConfig);
+  logEnvironment(env);
+  logStatics(env);
+  logAutoConfigValues(env);
+}
+
+module.exports = {
+  shouldDiagnose,
+  report,
+};
