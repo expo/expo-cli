f532eae85851fe5ff02f8381ec004a54866c35f5
Evan Bacon <baconbrix@gmail.com>
2019-06-09 13:56:18 -0700

    Added EXPO_WEB_DEBUG to disable minimizer and add file paths (#688)
    * Added EXPO_WEB_DEBUG to disable minimizer and add file paths
    
    * Added production check

--- packages/xdl/src/Web.js
+++ packages/xdl/src/Web.js
@@ -2,29 +2,70 @@ import fs from 'fs';
 import openBrowser from 'react-dev-utils/openBrowser';
 import path from 'path';
 
+import getenv from 'getenv';
+import chalk from 'chalk';
 import Logger from './Logger';
 import * as Doctor from './project/Doctor';
-import { readConfigJsonAsync } from './project/ProjectUtils';
+import { readConfigJsonAsync, logWarning } from './project/ProjectUtils';
 import * as UrlUtils from './UrlUtils';
 
-function invokePossibleFunction(objectOrMethod, ...args) {
+// When you have errors in the production build that aren't present in the development build you can use `EXPO_WEB_DEBUG=true expo start --no-dev` to debug those errors.
+// - Prevent the production build from being minified
+// - Include file path info comments in the bundle
+export function isDebugModeEnabled() {
+  return getenv.boolish('EXPO_WEB_DEBUG', false);
+}
+
+export function logEnvironmentInfo(projectRoot, tag, config) {
+  if (isDebugModeEnabled() && config.mode === 'production') {
+    logWarning(
+      projectRoot,
+      tag,
+      `Webpack is bundling your project in \`production\` mode with the ${chalk.bold(
+        '`EXPO_WEB_DEBUG`'
+      )} environment variable enabled. You should toggle it off before building for production.`
+    );
+  }
+}
+
+async function invokePossibleFunctionAsync(objectOrMethod, ...args) {
   if (typeof objectOrMethod === 'function') {
-    return objectOrMethod(...args);
+    return await objectOrMethod(...args);
   } else {
     return objectOrMethod;
   }
 }
 
+function applyEnvironmentVariables(config) {
+  // Use EXPO_DEBUG_WEB=true to enable debugging features for cases where the prod build
+  // has errors that aren't caught in development mode.
+  // Related: https://github.com/expo/expo-cli/issues/614
+  if (isDebugModeEnabled() && config.mode === 'production') {
+    // TODO: Bacon: Should this throw if not running in prod mode?
+
+    // Add comments that describe the file import/exports.
+    // This will make it easier to debug.
+    config.output.pathinfo = true;
+    // Prevent minimizing when running in debug mode.
+    config.optimization.minimize = false;
+  }
+
+  return config;
+}
+
 export async function invokeWebpackConfigAsync(env, argv) {
   // Check if the project has a webpack.config.js in the root.
   const projectWebpackConfig = path.resolve(env.projectRoot, 'webpack.config.js');
+  let config;
   if (fs.existsSync(projectWebpackConfig)) {
     const webpackConfig = require(projectWebpackConfig);
-    return await invokePossibleFunction(webpackConfig, env, argv);
+    config = await invokePossibleFunctionAsync(webpackConfig, env, argv);
+  } else {
+    // Fallback to the default expo webpack config.
+    const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+    config = await createExpoWebpackConfigAsync(env, argv);
   }
-  // Fallback to the default expo webpack config.
-  const config = require('@expo/webpack-config');
-  return await config(env, argv);
+  return applyEnvironmentVariables(config);
 }
 
 export async function openProjectAsync(projectRoot) {
