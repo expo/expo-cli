e97e5ab8e6e44b4edb97d3e7ffc100b74bad59bf
Tomasz Sapeta <tomasz.sapeta@swmansion.com>
2019-06-13 15:25:52 +0200

    [xdl] use `plist` npm package instead of `plutil`

--- packages/xdl/src/detach/IosPlist.js
+++ packages/xdl/src/detach/IosPlist.js
@@ -3,7 +3,6 @@ import path from 'path';
 import plist from 'plist';
 
 import { spawnAsyncThrowError } from './ExponentTools';
-import logger from './Logger';
 
 function _getNormalizedPlistFilename(plistName) {
   let plistFilename;
@@ -15,54 +14,29 @@ function _getNormalizedPlistFilename(plistName) {
   return plistFilename;
 }
 
+function buildPlistFromObject(object) {
+  // We start with an offset of -1, because Xcode maintains a custom indentation of the plist.
+  // Ref: https://github.com/facebook/react-native/issues/11668
+  return plist.build(object, { indent: '\t', offset: -1 }) + '\n';
+}
+
 /**
  *  @param plistName base filename of property list. if no extension, assumes .plist
  */
 async function modifyAsync(plistPath, plistName, transform) {
-  let plistFilename = _getNormalizedPlistFilename(plistName);
-  let configPlistName = path.join(plistPath, plistFilename);
-  let configFilename = path.join(plistPath, `${plistName}.json`);
+  const plistFilename = _getNormalizedPlistFilename(plistName);
+  const configPlistName = path.join(plistPath, plistFilename);
 
   // grab original plist as json object
-  let config;
-  if (process.platform === 'darwin') {
-    await spawnAsyncThrowError('plutil', [
-      '-convert',
-      'json',
-      configPlistName,
-      '-o',
-      configFilename,
-    ]);
-    let configContents = await fs.readFile(configFilename, 'utf8');
-
-    try {
-      config = JSON.parse(configContents);
-    } catch (e) {
-      logger.info(`Error parsing ${configFilename}`, e);
-      logger.info('The erroneous file contents was:', configContents);
-      config = {};
-    }
-  } else {
-    config = plist.parse(fs.readFileSync(configPlistName, 'utf8'));
-  }
+  const config = plist.parse(fs.readFileSync(configPlistName, 'utf8'));
 
   // apply transformation
-  config = transform(config);
+  const transformedConfig = transform(config);
 
-  // back up old plist and swap in modified one
-  await spawnAsyncThrowError('/bin/cp', [configPlistName, `${configPlistName}.bak`]);
-  await fs.writeFile(configFilename, JSON.stringify(config));
-  if (process.platform === 'darwin') {
-    await spawnAsyncThrowError('plutil', [
-      '-convert',
-      'xml1',
-      configFilename,
-      '-o',
-      configPlistName,
-    ]);
-  } else {
-    await fs.writeFile(configPlistName, plist.build(config));
-  }
+  // build plist content from object
+  const newPlistContent = buildPlistFromObject(transformedConfig);
+
+  await fs.writeFile(configPlistName, newPlistContent);
 
   return config;
 }
@@ -74,19 +48,10 @@ async function createBlankAsync(plistPath, plistName) {
   await fs.writeFile(tmpConfigFile, JSON.stringify(emptyConfig));
 
   // convert to plist
-  let plistFilename = _getNormalizedPlistFilename(plistName);
-  let configPlistName = path.join(plistPath, plistFilename);
-  if (process.platform === 'darwin') {
-    await spawnAsyncThrowError('plutil', [
-      '-convert',
-      'xml1',
-      tmpConfigFile,
-      '-o',
-      configPlistName,
-    ]);
-  } else {
-    await fs.writeFile(configPlistName, JSON.stringify(plist.build(emptyConfig)));
-  }
+  const plistFilename = _getNormalizedPlistFilename(plistName);
+  const configPlistName = path.join(plistPath, plistFilename);
+
+  await fs.writeFile(configPlistName, buildPlistFromObject(emptyConfig));
 
   // remove tmp json file
   await spawnAsyncThrowError('/bin/rm', [tmpConfigFile]);
