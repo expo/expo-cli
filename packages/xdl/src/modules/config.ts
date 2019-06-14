745a2e0c09d62a55e7777d567886747c42e5a873
Evan Bacon <baconbrix@gmail.com>
2019-06-14 14:41:07 -0700

    Added expo-device package (#729)
    * Added DeviceInfo package
    
    * change name to expo-device

--- packages/xdl/src/modules/config.ts
+++ packages/xdl/src/modules/config.ts
@@ -310,6 +310,11 @@ const expoUniversalModules = [
     sdkVersions: '>=29.0.0',
   },
   {
+    podName: 'EXDevice',
+    libName: 'expo-device',
+    sdkVersions: '>=34.0.0',
+  },
+  {
     podName: 'EXGL-CPP',
     libName: 'expo-gl-cpp',
     sdkVersions: '>=29.0.0',
