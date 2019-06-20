d376418e1a6f161424ecdf6f7aebac68b9b3a2c9
Evan Bacon <baconbrix@gmail.com>
2019-06-20 14:29:34 -0700

    Fixed expo webpack template

--- packages/webpack-config/template/webpack.config.js
+++ packages/webpack-config/template/webpack.config.js
@@ -1,7 +1,7 @@
 const createExpoWebpackConfig = require('@expo/webpack-config');
 
-module.exports = function(env, argv) {
-  const config = createExpoWebpackConfig(env, argv);
+module.exports = async function(env, argv) {
+  const config = await createExpoWebpackConfig(env, argv);
   // Customize the config before returning it.
   return config;
 };
