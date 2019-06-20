d2d689933c82679a3bd14f653de52d247594b7ff
Ville Immonen <ville.immonen@iki.fi>
2019-06-20 15:20:04 +0300

    refactor(xdl): convert Webhooks to TypeScript

--- packages/xdl/src/Webhooks.ts
+++ packages/xdl/src/Webhooks.ts
@@ -1,7 +1,15 @@
 import UserManager from './User';
 import ApiV2Client from './ApiV2';
 
-export async function setWebhookAsync(experienceName, { url, secret, event }) {
+export type WebhookEvent = 'build';
+
+export type WebhookData = {
+  url: string;
+  secret: string;
+  event: WebhookEvent;
+};
+
+export async function setWebhookAsync(experienceName: string, { url, secret, event }: WebhookData) {
   const user = await UserManager.getCurrentUserAsync();
   const api = ApiV2Client.clientForUser(user);
   return await api.postAsync('webhook/set', {
@@ -12,13 +20,13 @@ export async function setWebhookAsync(experienceName, { url, secret, event }) {
   });
 }
 
-export async function getWebhooksAsync(experienceName) {
+export async function getWebhooksAsync(experienceName: string) {
   const user = await UserManager.getCurrentUserAsync();
   const api = ApiV2Client.clientForUser(user);
   return await api.postAsync('webhook/get', { experienceName });
 }
 
-export async function deleteWebhooksAsync(experienceName, event) {
+export async function deleteWebhooksAsync(experienceName: string, event: WebhookEvent) {
   const user = await UserManager.getCurrentUserAsync();
   const api = ApiV2Client.clientForUser(user);
   return await api.postAsync('webhook/delete', { experienceName, event });
