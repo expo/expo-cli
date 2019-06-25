9c3ff1d86d86f13e62c5adec026861f12b21c034
Ville Immonen <ville.immonen@iki.fi>
2019-06-25 18:56:44 +0300

    refactor(xdl): convert xdl/tools/ImageUtils.js to TypeScript

--- packages/xdl/src/tools/ImageUtils.ts
+++ packages/xdl/src/tools/ImageUtils.ts
@@ -1,13 +1,10 @@
-/**
- * @flow
- */
-
-import probeImageSize from 'probe-image-size';
 import path from 'path';
 import fs from 'fs';
 
+import probeImageSize from 'probe-image-size';
+
 import { spawnAsyncThrowError } from '../detach/ExponentTools';
-import logger from '../detach/Logger';
+import LoggerDetach from '../detach/Logger';
 
 /**
  * @param {string} projectDirname
@@ -17,7 +14,7 @@ import logger from '../detach/Logger';
 async function getImageDimensionsAsync(
   projectDirname: string,
   basename: string
-): Promise<?{ width: number, height: number }> {
+): Promise<{ width: number; height: number } | null> {
   try {
     return await _getImageDimensionsAsync(projectDirname, basename);
   } catch (_) {}
@@ -27,7 +24,7 @@ async function getImageDimensionsAsync(
 async function _getImageDimensionsWithImageProbeAsync(
   projectDirname: string,
   basename: string
-): Promise<{ width: number, height: number }> {
+): Promise<{ width: number; height: number }> {
   const imagePath = path.resolve(projectDirname, basename);
   const readStream = fs.createReadStream(imagePath);
   const { width, height } = await probeImageSize(readStream);
@@ -46,7 +43,7 @@ async function resizeImageAsync(
     _resizeImageAsync === _resizeImageWithSipsAsync &&
     !_hasWarned
   ) {
-    logger.warn('`sips` utility may or may not work outside of macOS');
+    LoggerDetach.warn('`sips` utility may or may not work outside of macOS');
     _hasWarned = true;
   }
   return _resizeImageAsync(iconSizePx, iconFilename, destinationIconPath);
@@ -57,7 +54,7 @@ async function _resizeImageWithSipsAsync(
   iconFilename: string,
   destinationIconPath: string
 ) {
-  return spawnAsyncThrowError('sips', ['-Z', iconSizePx, iconFilename], {
+  return spawnAsyncThrowError('sips', ['-Z', iconSizePx.toFixed(), iconFilename], {
     stdio: ['ignore', 'ignore', 'inherit'], // only stderr
     cwd: destinationIconPath,
   });
@@ -65,7 +62,10 @@ async function _resizeImageWithSipsAsync(
 
 // Allow us to swap out the default implementations of image functions
 let _resizeImageAsync = _resizeImageWithSipsAsync;
-let _getImageDimensionsAsync = _getImageDimensionsWithImageProbeAsync;
+let _getImageDimensionsAsync: (
+  dirname: string,
+  filename: string
+) => Promise<{ width: number; height: number } | null> = _getImageDimensionsWithImageProbeAsync;
 
 // Allow users to provide an alternate implementation for our image resize function.
 // This is used internally in order to use sharp instead of sips in standalone builder.
@@ -78,7 +78,7 @@ function setResizeImageFunction(
 // Allow users to provide an alternate implementation for our image dimensions function.
 // This is used internally in order to use sharp instead of sips in standalone builder.
 function setGetImageDimensionsFunction(
-  fn: (dirname: string, filename: string) => Promise<?{ width: number, height: number }>
+  fn: (dirname: string, filename: string) => Promise<{ width: number; height: number } | null>
 ) {
   _getImageDimensionsAsync = fn;
 }
