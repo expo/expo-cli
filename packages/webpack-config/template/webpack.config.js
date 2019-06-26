0f4da0f8ad73660ce218eeb413e32d0a57dfb44e
Evan Bacon <baconbrix@gmail.com>
2019-06-26 14:47:07 -0700

    createExpoWebpackConfig -> createExpoWebpackConfigAsync

--- packages/webpack-config/template/webpack.config.js
+++ packages/webpack-config/template/webpack.config.js
@@ -1,7 +1,7 @@
-const createExpoWebpackConfig = require('@expo/webpack-config');
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function(env, argv) {
-  const config = await createExpoWebpackConfig(env, argv);
+  const config = await createExpoWebpackConfigAsync(env, argv);
   // Customize the config before returning it.
   return config;
 };
