daba903e776bef03f84848f57c0353b886ed4047
Ville Immonen <ville.immonen@iki.fi>
2019-07-04 18:23:53 +0300

    refactor(xdl): convert Watchman module to TypeScript

---
+++ packages/xdl/src/Watchman.ts
@@ -0,0 +1,78 @@
+import path from 'path';
+
+import fs from 'fs-extra';
+import spawnAsync from '@expo/spawn-async';
+import pTimeout from 'p-timeout';
+
+import * as Binaries from './Binaries';
+
+const WAIT_FOR_WATCHMAN_VERSION_MS = 3000;
+
+export function isPlatformSupported() {
+  return process.platform === 'darwin';
+}
+
+export async function addToPathAsync() {
+  if (!isPlatformSupported()) {
+    return;
+  }
+
+  await Binaries.addToPathAsync('watchman');
+}
+
+export async function unblockAndGetVersionAsync(projectRoot?: string) {
+  if (!isPlatformSupported()) {
+    return null;
+  }
+
+  try {
+    // `watchman version` returns:
+    // {
+    //  "version": "4.7.0"
+    // }
+    const result = await _unblockAndVersionAsync(projectRoot);
+    const watchmanVersion = JSON.parse(result.stdout.trim()).version;
+    return watchmanVersion;
+  } catch (e) {
+    // TODO: Maybe check to make sure this is ENOENT (which means watchman isn't installed)
+    // We might want to report other errors
+    return null;
+  }
+}
+
+async function _unblockAndVersionAsync(projectRoot?: string) {
+  try {
+    return await pTimeout(_versionAsync(), WAIT_FOR_WATCHMAN_VERSION_MS);
+  } catch (error) {
+    await _unblockAsync(projectRoot);
+    return await pTimeout(
+      _versionAsync(),
+      WAIT_FOR_WATCHMAN_VERSION_MS,
+      '`watchman version` failed even after `launchctl unload`'
+    );
+  }
+}
+
+async function _unblockAsync(projectRoot?: string) {
+  if (process.env.TMPDIR && process.env.USER) {
+    // XDL's copy of watchman:
+    fs.removeSync(path.join(process.env.TMPDIR, `${process.env.USER}-state`));
+    // homebrew's watchman:
+    fs.removeSync(`/usr/local/var/run/watchman/${process.env.USER}-state`);
+  }
+  if (process.platform === 'darwin') {
+    await spawnAsync('launchctl', [
+      'unload',
+      '-F',
+      '~/Library/LaunchAgents/com.github.facebook.watchman.plist',
+    ]);
+  }
+  if (projectRoot) {
+    await spawnAsync('watchman', ['watch-del', projectRoot]);
+    await spawnAsync('watchman', ['watch-project', projectRoot]);
+  }
+}
+
+async function _versionAsync() {
+  return await spawnAsync('watchman', ['version']);
+}
