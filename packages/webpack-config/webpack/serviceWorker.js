2711bf15140cda5016f17324172097c7adf59be6
Yifei He <yifei@hesyifei.com>
2019-07-12 18:09:03 -0700

    feat: [web] Add basic support for `notificationclick` and `postMessage`

--- packages/webpack-config/webpack/serviceWorker.js
+++ packages/webpack-config/webpack/serviceWorker.js
@@ -22,4 +22,40 @@ self.addEventListener('push', event => {
   event.waitUntil(self.registration.showNotification(title, options));
 });
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Clients
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+
+  event.waitUntil(
+    (async function() {
+      const allClients = await self.clients.matchAll({
+        includeUncontrolled: true,
+      });
+
+      let appClient;
+
+      // Let's see if we already have a window open:
+      for (const client of allClients) {
+        const url = new URL(client.url);
+
+        if (url.pathname === '/') {
+          // Excellent, let's use it!
+          client.focus();
+          appClient = client;
+          break;
+        }
+      }
+
+      // If we didn't find an existing window,
+      // open a new one:
+      if (!appClient) {
+        appClient = await self.clients.openWindow('/');
+      }
+
+      // Message the client:
+      appClient.postMessage('New chat messages!');
+    })()
+  );
+});
+
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
