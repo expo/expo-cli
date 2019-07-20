e3cac90b58ca05d8f7cd41595a8d182c3b51aa6b
Evan Bacon <baconbrix@gmail.com>
2019-07-19 18:32:44 -0700

    Fix webpack warning

--- packages/webpack-config/tests/basic/index.js
+++ packages/webpack-config/tests/basic/index.js
@@ -1,9 +1,9 @@
-/* global __DEV__ */
 import registerRootComponent from 'expo/build/launch/registerRootComponent';
 import { activateKeepAwake } from 'expo-keep-awake';
 
 import App from './App';
 
+// eslint-disable-next-line
 if (__DEV__) {
   activateKeepAwake();
 }
