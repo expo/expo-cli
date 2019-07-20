abbb8ec5f424cf6296e36c3fa220f43179312cc0
Evan Bacon <baconbrix@gmail.com>
2019-07-19 17:03:29 -0700

    Update webpack.config.js

--- packages/webpack-config/tests/basic/webpack.config.js
+++ packages/webpack-config/tests/basic/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = async function(env, argv) {
   const config = await createExpoWebpackConfigAsync(
     {
       ...env,
-      report: true,
+      report: !process.env.CI,
     },
     argv
   );
