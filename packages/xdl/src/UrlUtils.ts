777f37e5c0fe73102a32c6785cd1d50de0477282
Ville Immonen <ville.immonen@iki.fi>
2019-07-30 19:46:39 +0300

    Make 'https' option optional

--- packages/xdl/src/UrlUtils.ts
+++ packages/xdl/src/UrlUtils.ts
@@ -178,7 +178,7 @@ export async function constructUrlAsync(
       dev: joi.boolean(),
       strict: joi.boolean(),
       minify: joi.boolean(),
-      https: joi.boolean(),
+      https: joi.boolean().optional(),
       urlRandomness: joi
         .string()
         .optional()
