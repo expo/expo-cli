575f5d3104e4ce4d1bcb18e76e6a788776dfccd2
He Yifei 何一非 <yifei@hesyifei.com>
2019-08-05 10:16:58 -0700

    [xdl] Add support for `ios.requireFullScreen` option in `app.json` (#908)
    * [xdl] Add support for option `ios.requireFullScreen`
    
    * [xdl] Use `== null` instead of `=== null || === undefined`
    
    https://github.com/expo/expo-cli/pull/908#discussion_r309939894
    
    * [xdl] Cast to make sure that `UIRequiresFullScreen` is a boolean
    
    https://github.com/expo/expo-cli/pull/908#discussion_r309940084

--- packages/xdl/src/detach/IosNSBundle.js
+++ packages/xdl/src/detach/IosNSBundle.js
@@ -366,6 +366,15 @@ async function _configureInfoPlistAsync(context: StandaloneContext) {
       infoPlist.UIDeviceFamily = [2];
     }
 
+    // Whether requires full screen on iPad
+    infoPlist.UIRequiresFullScreen = config.ios && config.ios.requireFullScreen;
+    if (infoPlist.UIRequiresFullScreen == null) {
+      // NOTES: This is defaulted to `true` for now to match the behavior prior to SDK 34, but will change to `false` in a future SDK version.
+      infoPlist.UIRequiresFullScreen = true;
+    }
+    // Cast to make sure that it is a boolean.
+    infoPlist.UIRequiresFullScreen = Boolean(infoPlist.UIRequiresFullScreen);
+
     // context-specific plist changes
     if (context.type === 'user') {
       infoPlist = _configureInfoPlistForLocalDevelopment(infoPlist, context.data.exp);
