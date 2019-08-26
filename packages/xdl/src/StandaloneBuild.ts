fe719fa28646e34c9594617ecbd27f9020dfd8d5
Ville Immonen <ville.immonen@iki.fi>
2019-08-26 12:46:36 +0300

    refactor(xdl): convert StandaloneBuild to TS

--- packages/xdl/src/StandaloneBuild.ts
+++ packages/xdl/src/StandaloneBuild.ts
@@ -2,8 +2,19 @@ import _ from 'lodash';
 
 import ApiV2Client from './ApiV2';
 import UserManager from './User';
+import { Platform } from '@expo/config';
 
-export async function getStandaloneBuilds({ id, platform, limit, slug }) {
+export async function getStandaloneBuilds({
+  id,
+  platform,
+  limit,
+  slug,
+}: {
+  id?: string;
+  platform: Platform;
+  limit: number;
+  slug: string;
+}) {
   const user = await UserManager.ensureLoggedInAsync();
   const api = ApiV2Client.clientForUser(user);
   const params = { id, slug, platform, limit, status: 'finished' };
