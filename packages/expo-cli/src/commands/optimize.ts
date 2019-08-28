c7c0eddd26118136efc24b4d0092482875dc0a6e
Ville Immonen <ville.immonen@iki.fi>
2019-08-28 13:33:21 +0300

    refactor(expo-cli): convert bundle-assets, doctor, optimize and url to TypeScript (#966)

--- packages/expo-cli/src/commands/optimize.ts
+++ packages/expo-cli/src/commands/optimize.ts
@@ -1,33 +1,50 @@
+import { Command } from 'commander';
 import { Project, ProjectUtils, AssetUtils } from '@expo/xdl';
+
 import log from '../log';
 
-export async function action(projectDir = './', options = {}) {
+type Options = {
+  save?: boolean;
+  quality?: string;
+  include?: string;
+  exclude?: string;
+};
+
+export async function action(projectDir = './', options: Options = {}) {
   const { exp } = await ProjectUtils.readConfigJsonAsync(projectDir);
   if (exp === null) {
     log.warn('No Expo configuration found. Are you sure this is a project directory?');
     process.exit(1);
   }
 
-  const hasUnoptimizedAssets = await AssetUtils.hasUnoptimizedAssetsAsync(projectDir, options);
+  const optimizationOptions = {
+    ...options,
+    quality: parseQuality(options),
+  };
+
+  const hasUnoptimizedAssets = await AssetUtils.hasUnoptimizedAssetsAsync(
+    projectDir,
+    optimizationOptions
+  );
   if (!options.save && hasUnoptimizedAssets) {
     log.warn('This will overwrite the original assets.');
   }
-
-  // Validate custom quality
-  const defaultQuality = 80;
-  const { quality: strQuality } = options;
-
-  const quality = Number(strQuality);
-  const validQuality = Number.isInteger(quality) && quality > 0 && quality <= 100;
-  if (strQuality !== undefined && !validQuality) {
-    throw new Error('Invalid value for --quality flag. Must be an integer between 1 and 100.');
-  }
-  const outputQuality = validQuality ? quality : defaultQuality;
-  options.quality = outputQuality;
-  await Project.optimizeAsync(projectDir, options);
+  const optimizeOptions = await Project.optimizeAsync(projectDir, optimizationOptions);
 }
 
-export default program => {
+function parseQuality(options: Options): number {
+  const defaultQuality = 80;
+  if (options.quality == null) {
+    return defaultQuality;
+  }
+  const quality = Number(options.quality);
+  if (!(Number.isInteger(quality) && quality > 0 && quality <= 100)) {
+    throw new Error('Invalid value for --quality flag. Must be an integer between 1 and 100.');
+  }
+  return quality;
+}
+
+export default (program: Command) => {
   program
     .command('optimize [project-dir]')
     .alias('o')
