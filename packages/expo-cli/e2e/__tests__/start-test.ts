55e19495fb2a39cd31b072d4881dfbe559420f04
Ville Immonen <ville.immonen@iki.fi>
2019-09-03 17:24:54 +0300

    test(expo-cli): add e2e (end-to-end) tests for init and start commands

---
+++ packages/expo-cli/e2e/__tests__/start-test.ts
@@ -0,0 +1,26 @@
+import path from 'path';
+
+import spawnAsync from '@expo/spawn-async';
+import temporary from 'tempy';
+
+import { runAsync, EXPO_CLI } from '../TestUtils';
+
+const projectDir = path.join(temporary.directory(), 'my-app');
+
+beforeAll(async () => {
+  jest.setTimeout(60000);
+  await runAsync(['init', projectDir, '--template', 'blank', '--name', 'My App', '--yarn']);
+});
+
+test('start --offline', async () => {
+  const promise = spawnAsync(EXPO_CLI, ['start', '--offline'], {
+    cwd: projectDir,
+  });
+  const cli = promise.child;
+  cli.stdout.pipe(process.stdout).on('data', data => {
+    if (/Your native app is running/.test(data.toString())) {
+      cli.kill('SIGINT');
+    }
+  });
+  cli.stderr.pipe(process.stderr);
+});
