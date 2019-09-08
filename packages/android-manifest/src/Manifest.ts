f3d55b801ea8f1ac0dcf0c7d04ae1a5d4f39d2cf
Evan Bacon <baconbrix@gmail.com>
2019-09-07 18:05:32 -0700

    Added E2E methods

--- packages/android-manifest/src/Manifest.ts
+++ packages/android-manifest/src/Manifest.ts
@@ -27,12 +27,16 @@ export function createPermission(doc: Document, name: string): Element {
   return permission;
 }
 
-export function removePermissions(doc: Document, permissionNames: string[]) {
-  const targetNames = permissionNames.map(ensurePermissionNameFormat);
+export function removePermissions(doc: Document, permissionNames?: string[]) {
+  const targetNames = permissionNames ? permissionNames.map(ensurePermissionNameFormat) : null;
 
   for (const attribute of getPermissionAttributes(doc)) {
-    if (targetNames.includes(attribute.value())) {
-      // continue to iterate in case there are duplicates
+    if (targetNames) {
+      if (targetNames.includes(attribute.value())) {
+        // continue to iterate in case there are duplicates
+        attribute.remove();
+      }
+    } else {
       attribute.remove();
     }
   }
@@ -47,6 +51,26 @@ export function addPermission(doc: Document, permission: Element) {
 
   getRoot(doc).addChild(permission);
   return;
+}
+
+export function ensurePermissions(
+  doc: Document,
+  permissionNames: string[]
+): { [permission: string]: boolean } {
+  const permissions = getPermissions(doc);
+
+  const results: { [permission: string]: boolean } = {};
+  for (const permissionName of permissionNames) {
+    const targetName = ensurePermissionNameFormat(permissionName);
+    if (!permissions.includes(targetName)) {
+      const permissionElement = createPermission(doc, targetName);
+      addPermission(doc, permissionElement);
+      results[permissionName] = true;
+    } else {
+      results[permissionName] = false;
+    }
+  }
+  return results;
 }
 
 export function ensurePermission(doc: Document, permissionName: string): boolean {
@@ -79,7 +103,7 @@ export function findAttributeInTag(doc: Document, attribute: string, tag: string
   return values.filter(value => value !== null) as Attribute[];
 }
 
-function ensurePermissionNameFormat(permissionName: string): string {
+export function ensurePermissionNameFormat(permissionName: string): string {
   if (permissionName.includes('.')) {
     const com = permissionName.split('.');
     const name = com.pop() as string;
@@ -111,18 +135,23 @@ export async function writeAndroidManifestAsync(
   manifestPath: string,
   manifest: any
 ): Promise<void> {
-  await fs.ensureDir(manifestPath);
+  await fs.ensureDir(path.dirname(manifestPath));
   await fs.writeFile(manifestPath, manifest);
 }
 
-export function getProjectAndroidManifestPath(projectDir: string): string | null {
-  const shellPath = path.join(projectDir, 'android');
-  if (fs.existsSync(shellPath)) {
-    const manifestPath = path.join(shellPath, 'app/src/main/AndroidManifest.xml');
-    if (fs.existsSync(manifestPath)) {
-      return manifestPath;
+export async function getProjectAndroidManifestPathAsync(
+  projectDir: string
+): Promise<string | null> {
+  try {
+    const shellPath = path.join(projectDir, 'android');
+    if ((await fs.stat(shellPath)).isDirectory()) {
+      const manifestPath = path.join(shellPath, 'app/src/main/AndroidManifest.xml');
+      if ((await fs.stat(manifestPath)).isFile()) {
+        return manifestPath;
+      }
     }
-  }
+  } catch (error) {}
+
   return null;
 }
 
@@ -131,3 +160,62 @@ export async function readAsync(manifestPath: string): Promise<Document> {
   const manifest: Document = parseXml(contents);
   return manifest;
 }
+
+export async function persistAndroidPermissionsAsync(
+  projectDir: string,
+  permissions: string[]
+): Promise<boolean> {
+  const manifestPath = await getProjectAndroidManifestPathAsync(projectDir);
+  // The Android Manifest takes priority over the app.json
+  if (!manifestPath) {
+    return false;
+  }
+  const manifest = await readAsync(manifestPath);
+  removePermissions(manifest);
+  const results = ensurePermissions(manifest, permissions);
+  if (Object.values(results).reduce((prev, current) => prev && current, true) === false) {
+    const failed = Object.keys(results).filter(key => !results[key]);
+    throw new Error(
+      `Failed to write the following permissions to the native AndroidManifest.xml: ${failed.join(
+        ', '
+      )}`
+    );
+  }
+  await writeAndroidManifestAsync(manifestPath, manifest);
+  return true;
+}
+
+// TODO(Bacon): link to resources about required permission prompts
+export const UnimodulePermissions: { [key: string]: string } = {
+  'android.permission.ACCESS_COARSE_LOCATION': 'ACCESS_COARSE_LOCATION',
+  'android.permission.ACCESS_FINE_LOCATION': 'ACCESS_FINE_LOCATION',
+  'android.permission.CAMERA': 'CAMERA',
+  'android.permission.MANAGE_DOCUMENTS': 'MANAGE_DOCUMENTS',
+  'android.permission.READ_CONTACTS': 'READ_CONTACTS',
+  'android.permission.READ_CALENDAR': 'READ_CALENDAR',
+  'android.permission.WRITE_CALENDAR': 'WRITE_CALENDAR',
+  'android.permission.READ_EXTERNAL_STORAGE': 'READ_EXTERNAL_STORAGE',
+  'android.permission.READ_PHONE_STATE': 'READ_PHONE_STATE',
+  'android.permission.RECORD_AUDIO': 'RECORD_AUDIO',
+  'android.permission.USE_FINGERPRINT': 'USE_FINGERPRINT',
+  'android.permission.VIBRATE': 'VIBRATE',
+  'android.permission.WAKE_LOCK': 'WAKE_LOCK',
+  'android.permission.WRITE_EXTERNAL_STORAGE': 'WRITE_EXTERNAL_STORAGE',
+  'com.anddoes.launcher.permission.UPDATE_COUNT': 'com.anddoes.launcher.permission.UPDATE_COUNT',
+  'com.android.launcher.permission.INSTALL_SHORTCUT':
+    'com.android.launcher.permission.INSTALL_SHORTCUT',
+  'com.google.android.c2dm.permission.RECEIVE': 'com.google.android.c2dm.permission.RECEIVE',
+  'com.google.android.gms.permission.ACTIVITY_RECOGNITION':
+    'com.google.android.gms.permission.ACTIVITY_RECOGNITION',
+  'com.google.android.providers.gsf.permission.READ_GSERVICES':
+    'com.google.android.providers.gsf.permission.READ_GSERVICES',
+  'com.htc.launcher.permission.READ_SETTINGS': 'com.htc.launcher.permission.READ_SETTINGS',
+  'com.htc.launcher.permission.UPDATE_SHORTCUT': 'com.htc.launcher.permission.UPDATE_SHORTCUT',
+  'com.majeur.launcher.permission.UPDATE_BADGE': 'com.majeur.launcher.permission.UPDATE_BADGE',
+  'com.sec.android.provider.badge.permission.READ':
+    'com.sec.android.provider.badge.permission.READ',
+  'com.sec.android.provider.badge.permission.WRITE':
+    'com.sec.android.provider.badge.permission.WRITE',
+  'com.sonyericsson.home.permission.BROADCAST_BADGE':
+    'com.sonyericsson.home.permission.BROADCAST_BADGE',
+};
--- packages/android-manifest/src/__tests__/Manifest-test.js
+++ packages/android-manifest/src/__tests__/Manifest-test.js
@@ -1,6 +1,6 @@
-import { resolve } from 'path';
+import { resolve, dirname } from 'path';
+import fs from 'fs-extra';
 import * as Manifest from '../Manifest';
-
 const fixturesPath = resolve(__dirname, 'fixtures');
 const manifestPath = resolve(fixturesPath, 'react-native-AndroidManifest.xml');
 
@@ -23,6 +23,16 @@ describe('Permissions', () => {
     expect(permissions.length).toBe(1);
   });
 
+  it(`ensures multiple permissions`, async () => {
+    const manifest = await Manifest.readAsync(manifestPath);
+    const permissionsToAdd = ['VALUE_1', 'VALUE_2'];
+    const results = Manifest.ensurePermissions(manifest, permissionsToAdd);
+    expect(results).toMatchSnapshot();
+    expect(Object.values(results)).toStrictEqual([true, true]);
+
+    expect(Manifest.getPermissions(manifest).length).toBe(3);
+  });
+
   it(`removes permissions by name`, async () => {
     const manifest = await Manifest.readAsync(manifestPath);
     expect(Manifest.ensurePermission(manifest, 'VALUE_TO_REMOVE_1')).toBe(true);
@@ -30,8 +40,18 @@ describe('Permissions', () => {
     expect(Manifest.getPermissions(manifest).length).toBe(3);
 
     Manifest.removePermissions(manifest, ['VALUE_TO_REMOVE_1', 'VALUE_TO_REMOVE_2']);
-    Manifest.logManifest(manifest);
     expect(Manifest.getPermissions(manifest).length).toBe(1);
+  });
+
+  it(`removes all permissions`, async () => {
+    const manifest = await Manifest.readAsync(manifestPath);
+    expect(Manifest.ensurePermission(manifest, 'VALUE_TO_REMOVE_1')).toBe(true);
+    expect(Manifest.ensurePermission(manifest, 'VALUE_TO_REMOVE_2')).toBe(true);
+    expect(Manifest.getPermissions(manifest).length).toBe(3);
+
+    Manifest.removePermissions(manifest);
+
+    expect(Manifest.getPermissions(manifest).length).toBe(0);
   });
 
   it(`can write with a pretty format`, async () => {
@@ -47,5 +67,40 @@ describe('Permissions', () => {
         selfCloseEmpty: true,
       })
     ).toMatchSnapshot();
+  });
+
+  describe('E2E', () => {
+    beforeEach(async () => {
+      await fs.remove(resolve(fixturesPath, 'android'));
+    });
+    afterAll(async () => {
+      await fs.remove(resolve(fixturesPath, 'android'));
+    });
+
+    const appManifestPath = resolve(fixturesPath, 'android/app/src/main/AndroidManifest.xml');
+
+    it(`replaces and persists permissions`, async () => {
+      await fs.ensureDir(dirname(appManifestPath));
+      await fs.copyFile(manifestPath, appManifestPath);
+
+      const permissionsToPersist = [
+        'CAMERA',
+        'android.permission.WRITE_CALENDAR',
+        'com.anddoes.launcher.permission.UPDATE_COUNT',
+        'com.sonyericsson.home.permission.BROADCAST_BADGE',
+      ];
+      expect(
+        await Manifest.persistAndroidPermissionsAsync(fixturesPath, permissionsToPersist)
+      ).toBe(true);
+
+      const manifest = await Manifest.readAsync(appManifestPath);
+      expect(Manifest.getPermissions(manifest)).toStrictEqual(
+        permissionsToPersist.map(Manifest.ensurePermissionNameFormat)
+      );
+    });
+
+    it(`returns false when the native project cannot be found`, async () => {
+      expect(await Manifest.persistAndroidPermissionsAsync(fixturesPath, [])).toBe(false);
+    });
   });
 });
