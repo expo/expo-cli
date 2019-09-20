21838b412627ef7cad3065c220b7e10b87fb6323
Yifei He <yifei@hesyifei.com>
2019-09-20 14:30:05 -0700

    Add `expo-constants-manifest` for basic test

--- packages/webpack-config/tests/basic/App.js
+++ packages/webpack-config/tests/basic/App.js
@@ -1,12 +1,14 @@
 import { LinearGradient } from 'expo-linear-gradient';
 import React from 'react';
 import { StyleSheet, Text } from 'react-native';
+import Constants from 'expo-constants';
 import getenv from 'getenv';
 
 export default function App() {
   return (
     <LinearGradient colors={['orange', 'blue']} style={styles.container}>
       <Text testID="basic-text">Open up App.js to start working on your app!</Text>
+      <Text testID="expo-constants-manifest">{JSON.stringify(Constants.manifest)}</Text>
       {getenv.boolish('CI', false) && <Text testID="has-ci-text">Has CI env</Text>}
     </LinearGradient>
   );
