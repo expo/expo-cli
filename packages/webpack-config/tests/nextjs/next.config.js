03dbb44faf872cf3ade757dad93bef6f36bd8eef
Yifei He <yifei@hesyifei.com>
2019-09-20 12:09:30 -0700

    Do not hardcode `projectRoot`

--- packages/webpack-config/tests/nextjs/next.config.js
+++ packages/webpack-config/tests/nextjs/next.config.js
@@ -1,11 +1,11 @@
 const withUnimodules = require('../../withUnimodules');
+const projectRoot = process.cwd();
 
 module.exports = {
   webpack(config, options) {
     // Further custom configuration here
     return withUnimodules(config, {
-      projectRoot:
-        '/Users/eflyjason/Documents/Github/expo/expo-cli/packages/webpack-config/tests/nextjs/',
+      projectRoot,
     });
   },
 };
