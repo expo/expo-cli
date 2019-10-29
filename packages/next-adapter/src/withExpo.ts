fb4eb73c8d4c4363476ff0451c69dea390da2477
Evan Bacon <baconbrix@gmail.com>
2019-10-29 10:36:35 +0100

    strip down unimodules

--- packages/next-adapter/src/withExpo.ts
+++ packages/next-adapter/src/withExpo.ts
@@ -7,16 +7,19 @@ export default (nextConfig: any = {}): any => ({
   ...nextConfig,
   pageExtensions: getModuleFileExtensionsWithoutDotPrefix('web'),
   webpack(config: any, options: any): any {
-    config = withUnimodules(config, {
+    const expoConfig = withUnimodules(config, {
       projectRoot: nextConfig.projectRoot || process.cwd(),
     });
+
+    // Use original public path
+    expoConfig.output.publicPath = (config.output || {}).publicPath;
 
     // TODO: Bacon: use commonjs for RNW babel maybe...
 
     if (typeof nextConfig.webpack === 'function') {
-      return nextConfig.webpack(config, options);
+      return nextConfig.webpack(expoConfig, options);
     }
 
-    return config;
+    return expoConfig;
   },
 });
