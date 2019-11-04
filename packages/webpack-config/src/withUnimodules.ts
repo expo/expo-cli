caba9d3b1619ffbe825ad9086d0f5336e9b52f17
Evan Bacon <baconbrix@gmail.com>
2019-11-04 12:57:59 -0800

    Update withUnimodules.ts

--- packages/webpack-config/src/withUnimodules.ts
+++ packages/webpack-config/src/withUnimodules.ts
@@ -150,7 +150,6 @@ export default function withUnimodules(
   // Reference:
   // https://github.com/martpie/next-transpile-modules/blob/77450a0c0307e4b650d7acfbc18641ef9465f0da/index.js#L48-L62
   // https://github.com/zeit/next.js/blob/0b496a45e85f3c9aa3cf2e77eef10888be5884fc/packages/next/build/webpack-config.ts#L185-L258
-  // const babelLoader = _findBabelLoader(config.module!.rules);
   // `include` function is from https://github.com/expo/expo-cli/blob/3933f3d6ba65bffec2738ece71b62f2c284bd6e4/packages/webpack-config/webpack/loaders/createBabelLoaderAsync.js#L76-L96
   const includeFunc = babelLoader.include as ((path: string) => boolean);
   if (inputWebpackConfig.externals) {
