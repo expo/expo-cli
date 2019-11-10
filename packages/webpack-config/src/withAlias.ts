45d73aee3ce5ff1104cec0b15b5c7f0cca283400
Evan Bacon <baconbrix@gmail.com>
2019-11-10 11:54:17 -0800

    use withAlias in withUnimodules

--- packages/webpack-config/src/withAlias.ts
+++ packages/webpack-config/src/withAlias.ts
@@ -1,12 +1,16 @@
 import { AnyConfiguration } from './types';
 import { DEFAULT_ALIAS } from './utils/config';
 
-export default function withAlias(webpackConfig: AnyConfiguration): AnyConfiguration {
+export default function withAlias(
+  webpackConfig: AnyConfiguration,
+  alias: { [key: string]: string } = {}
+): AnyConfiguration {
   // Mix in aliases
   if (!webpackConfig.resolve) webpackConfig.resolve = {};
   webpackConfig.resolve.alias = {
     ...DEFAULT_ALIAS,
     ...(webpackConfig.resolve.alias || {}),
+    ...alias,
   };
 
   return webpackConfig;
