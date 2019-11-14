8dba66de9f5e309d99d6fb390fc91d2cbd501419
Evan Bacon <baconbrix@gmail.com>
2019-11-14 14:30:37 -0800

    fix customize output path

--- packages/electron-adapter/src/Config.ts
+++ packages/electron-adapter/src/Config.ts
@@ -9,6 +9,7 @@ const PACKAGE_MAIN_PROCESS_PATH = '@expo/electron-adapter/template/main';
 const PACKAGE_TEMPLATE_PATH = '@expo/webpack-config/web-default/index.html';
 const PACKAGE_RENDER_WEBPACK_CONFIG_PATH = '@expo/electron-adapter/template/webpack.config';
 
+const LOCAL_ELECTRON_PATH = './electron';
 const LOCAL_MAIN_PROCESS_PATH = './electron/main';
 const LOCAL_TEMPLATE_PATH = './electron/template/index.html';
 const LOCAL_RENDER_WEBPACK_CONFIG_PATH = './electron/webpack.config';
@@ -76,7 +77,7 @@ export function withExpoAdapter({
 }
 
 export function copyTemplateToProject(projectPath: string) {
-  const outputPath = path.resolve(projectPath, LOCAL_MAIN_PROCESS_PATH);
+  const outputPath = path.resolve(projectPath, LOCAL_ELECTRON_PATH);
   if (!fs.pathExistsSync(outputPath)) {
     fs.ensureDirSync(path.dirname(outputPath));
     fs.copy(path.resolve(__dirname, '../template'), outputPath);
