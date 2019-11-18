9cac56506c22be6676d5742b1180a6665dbd26fb
Evan Bacon <baconbrix@gmail.com>
2019-11-18 12:05:20 -0800

    [electron-adapter] fix everything (#1230)
    * Remove unused dependencies
    
    * fix customize output path
    
    * move ensureEntryAsync to webpack-config/utils
    
    - rename ensureEntryAsync -> resolveEntryAsync
    
    * remove webpack-cli dependency
    
    * bump webpack
    
    * cache the results of include method in babel loader
    
    * Remove expo module externs
    
    * Fix index.html resolution
    
    * Added webpack-config as a devDep
    
    * Update yarn.lock
    
    * Update createBabelLoader.ts

--- packages/electron-adapter/src/Webpack.ts
+++ packages/electron-adapter/src/Webpack.ts
@@ -2,12 +2,20 @@ import { withAlias } from '@expo/webpack-config/addons';
 import { createBabelLoaderFromEnvironment } from '@expo/webpack-config/loaders';
 import { ExpoDefinePlugin, ExpoInterpolateHtmlPlugin } from '@expo/webpack-config/plugins';
 import { getPaths, getConfig, getModuleFileExtensions } from '@expo/webpack-config/env';
-import { getPluginsByName, getRulesByMatchingFiles } from '@expo/webpack-config/utils';
+import {
+  getPluginsByName,
+  getRulesByMatchingFiles,
+  resolveEntryAsync,
+} from '@expo/webpack-config/utils';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import * as path from 'path';
 import { Configuration } from 'webpack';
 
-export function withExpoWebpack(config: Configuration, options: { projectRoot?: string } = {}) {
+// skipEntry defaults to false
+export function withExpoWebpack(
+  config: Configuration,
+  options: { projectRoot?: string; skipEntry?: boolean } = {}
+) {
   // Support React Native aliases
   config = withAlias(config);
 
@@ -44,12 +52,77 @@ export function withExpoWebpack(config: Configuration, options: { projectRoot?: 
   config.resolve.extensions = getModuleFileExtensions('electron', 'web');
   config.resolve.extensions.push('.node');
 
+  if (!options.skipEntry) {
+    if (!env.locations.appMain) {
+      throw new Error(
+        `The entry point for your project couldn't be found. Please install \`expo\`, or define it in the package.json main field`
+      );
+    }
+
+    const electronWebpackDefaultEntryPoints = [
+      path.resolve(env.projectRoot, 'index'),
+      path.resolve(env.projectRoot, 'app'),
+    ];
+    const expoEntry = config.entry;
+    config.entry = async () => {
+      const entries = await resolveEntryAsync(expoEntry);
+
+      const expoEntryPointPath = env.locations.appMain;
+
+      if (entries.renderer && !entries.renderer.includes(expoEntryPointPath)) {
+        if (!Array.isArray(entries.renderer)) {
+          entries.renderer = [entries.renderer];
+        }
+
+        entries.renderer = entries.renderer.filter(
+          inputPath =>
+            !electronWebpackDefaultEntryPoints.some(possibleEntryPoint =>
+              inputPath.includes(possibleEntryPoint)
+            )
+        );
+
+        entries.renderer.push(expoEntryPointPath);
+
+        if (entries.renderer.length > 2) {
+          throw new Error(
+            `electron-adapter app entry hack doesn't work with this version of electron-webpack. The expected entry length should be 2, instead got: [${entries.renderer.join(
+              ', '
+            )}]`
+          );
+        } else {
+          console.log(` Using custom entry point > ${expoEntryPointPath}`);
+        }
+      }
+      return entries;
+    };
+  }
+
+  const babelConfig = createBabelLoaderFromEnvironment(env);
+
+  // Modify externals https://github.com/electron-userland/electron-webpack/issues/81
+  const includeFunc = babelConfig.include as ((path: string) => boolean);
+  if (config.externals) {
+    config.externals = (config.externals as any)
+      .map((external: any) => {
+        if (typeof external !== 'function') {
+          const relPath = path.join('node_modules', external);
+          if (!includeFunc(relPath)) return external;
+          return null;
+        }
+        return (ctx: any, req: any, cb: any) => {
+          const relPath = path.join('node_modules', req);
+          return includeFunc(relPath) ? cb() : external(ctx, req, cb);
+        };
+      })
+      .filter(Boolean);
+  }
+
   // Replace JS babel loaders with Expo loaders that can handle RN libraries
-  const rules = getRulesByMatchingFiles(config, [path.resolve(env.projectRoot, 'foo.js')]);
+  const rules = getRulesByMatchingFiles(config, [env.locations.appMain]);
+
   for (const filename of Object.keys(rules)) {
     for (const loaderItem of rules[filename]) {
-      const babelConfig = createBabelLoaderFromEnvironment(env);
-      (config.module || { rules: [] }).rules.splice(loaderItem.index, 1, babelConfig);
+      (config.module || { rules: [] }).rules.splice(loaderItem.index, 0, babelConfig);
       return config;
     }
   }
