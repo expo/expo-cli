cfeb0890bf19d4475bd551797e3cf987ef9e537a
Evan Bacon <baconbrix@gmail.com>
2019-11-22 13:53:00 -0800

    create a custom babel preset for next-expo projects

---
+++ packages/next-adapter/src/babel.ts
@@ -0,0 +1,43 @@
+// Learn more: https://github.com/expo/expo/blob/master/docs/pages/versions/unversioned/guides/using-nextjs.md#shared-steps
+
+let hasCheckedModules = false;
+
+module.exports = function(api: any) {
+  // Detect web usage (this may change in the future if Next.js changes the loader to `next-babel-loader`)
+  const isWeb = api.caller((caller?: { name: string }) => caller && caller.name === 'babel-loader');
+
+  // Check peer dependencies
+  if (!hasCheckedModules) {
+    hasCheckedModules = true;
+    // Only check for next support in the browser...
+    const missingPackages = ['babel-preset-expo', isWeb && 'next/babel'].filter(
+      packageName => packageName && !hasModule(packageName)
+    );
+    // Throw an error if anything is missing
+    if (missingPackages.length)
+      throw new Error(
+        `[BABEL]: preset \`@expo/next-adapter/babel\` is missing peer dependencies: ${missingPackages.join(
+          ', '
+        )}`
+      );
+  }
+
+  return {
+    presets: [
+      require('babel-preset-expo'),
+      // Only use next in the browser, it'll break your native project/
+      isWeb && require('next/babel'),
+    ].filter(Boolean),
+  };
+};
+
+function hasModule(name: string): boolean {
+  try {
+    return !!require.resolve(name);
+  } catch (error) {
+    if (error.code === 'MODULE_NOT_FOUND' && error.message.includes(name)) {
+      return false;
+    }
+    throw error;
+  }
+}
