2fa5d0be87e40abf4682c49b1a9a83bde5e6d859
Evan Bacon <baconbrix@gmail.com>
2019-11-25 15:39:48 -0800

    [next][web] Disable unimodule font loading in next  (#1242)
    * [WIP]
    
    * Add image and font support by default
    
    * revert
    
    * disable default font loading
    
    * Remove image and fonts by default
    
    * fix loaders export
    
    * 🧹 cleanup

--- packages/next-adapter/src/withExpo.ts
+++ packages/next-adapter/src/withExpo.ts
@@ -1,22 +1,29 @@
 import { getManagedExtensions } from '@expo/config/paths';
 import { withUnimodules } from '@expo/webpack-config/addons';
+import { AnyConfiguration } from '@expo/webpack-config/webpack/types';
+import { NextConfig } from 'next';
 
-export default (nextConfig: any = {}): any => ({
-  ...nextConfig,
-  pageExtensions: getManagedExtensions(['web']),
-  webpack(config: any, options: any): any {
-    const expoConfig = withUnimodules(config, {
-      projectRoot: nextConfig.projectRoot || process.cwd(),
-    });
-    // Use original public path
-    (expoConfig.output || {}).publicPath = (config.output || {}).publicPath;
+export default function withExpo(nextConfig: NextConfig = {}): NextConfig {
+  return {
+    ...nextConfig,
+    pageExtensions: getManagedExtensions(['web']),
+    webpack(config: AnyConfiguration, options: any): AnyConfiguration {
+      const expoConfig = withUnimodules(
+        config,
+        {
+          projectRoot: nextConfig.projectRoot || process.cwd(),
+        },
+        { supportsFontLoading: false }
+      );
+      // Use original public path
+      (expoConfig.output || {}).publicPath = (config.output || {}).publicPath;
 
-    // TODO: Bacon: use commonjs for RNW babel maybe...
+      // TODO: Bacon: use commonjs for RNW babel maybe...
+      if (typeof nextConfig.webpack === 'function') {
+        return nextConfig.webpack(expoConfig, options);
+      }
 
-    if (typeof nextConfig.webpack === 'function') {
-      return nextConfig.webpack(expoConfig, options);
-    }
-
-    return expoConfig;
-  },
-});
+      return expoConfig;
+    },
+  };
+}
--- packages/webpack-config/src/loaders/index.ts
+++ packages/webpack-config/src/loaders/index.ts
@@ -1,4 +1,13 @@
-export { default as createAllLoaders } from './createAllLoaders';
+export {
+  imageLoaderRule,
+  fallbackLoaderRule,
+  styleLoaderRule,
+  getBabelLoaderRuleFromEnv,
+  getBabelLoaderRule,
+  getHtmlLoaderRule,
+  getAllLoaderRules,
+  default as createAllLoaders,
+} from './createAllLoaders';
 export { default as createBabelLoader } from './createBabelLoader';
 export * from './createBabelLoader';
 export { default as createFontLoader } from './createFontLoader';
