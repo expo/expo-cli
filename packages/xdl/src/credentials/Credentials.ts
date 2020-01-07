42f1ebf90e1022870a852ff8c17d12fcd27c02a6
Jonathan 小林 Hales <jonathan@expo.io>
2020-01-07 12:45:06 -0800

    add apiv2 option for android getCredentials

--- packages/xdl/src/credentials/Credentials.ts
+++ packages/xdl/src/credentials/Credentials.ts
@@ -1,4 +1,5 @@
 import { Platform, readConfigJsonAsync } from '@expo/config';
+import { ApiV2 } from '../xdl';
 
 import Api from '../Api';
 import UserManager from '../User';
@@ -46,33 +47,49 @@ export async function credentialsExistForPlatformAsync(
 
 export async function getEncryptedCredentialsForPlatformAsync(
   metadata: CredentialMetadata
-): Promise<Credentials | undefined> {
+): Promise<Credentials | undefined | null> {
   return fetchCredentials(metadata, false);
 }
 
 export async function getCredentialsForPlatform(
   metadata: CredentialMetadata
-): Promise<Credentials | undefined> {
+): Promise<Credentials | undefined | null> {
   return fetchCredentials(metadata, true);
 }
 
 async function fetchCredentials(
   { username, experienceName, bundleIdentifier, platform }: CredentialMetadata,
   decrypt: boolean
-): Promise<Credentials | undefined> {
+): Promise<Credentials | undefined | null> {
   // this doesn't hit our mac rpc channel, so it needs significantly less debugging
-  const { err, credentials } = await Api.callMethodAsync('getCredentials', [], 'post', {
-    username,
-    experienceName,
-    bundleIdentifier,
-    platform,
-    decrypt,
-  });
+  console.log('fetchCredentials', { username, experienceName, bundleIdentifier, platform });
+  let credentials;
+  if (process.env.EXPO_NEXT_API) {
+    //TODO-JJ make this work for ios as well
+    const user = await UserManager.ensureLoggedInAsync();
+    const api = ApiV2.clientForUser(user);
+    credentials = await api.getAsync(`credentials/${platform}/keystore/${experienceName}`);
+    console.log({ credentials });
+    if (credentials['keystore']) {
+      credentials['keystore']['keystoreAlias'] = credentials['keystore']['keyAlias'];
+      delete credentials['keystore']['keyAlias'];
+    } else {
+      return null;
+    }
+  } else {
+    const response = await Api.callMethodAsync('getCredentials', [], 'post', {
+      username,
+      experienceName,
+      bundleIdentifier,
+      platform,
+      decrypt,
+    });
 
-  if (err) {
-    throw new Error('Error fetching credentials.');
+    if (response.err) {
+      throw new Error('Error fetching credentials.');
+    }
+    credentials = response.credentials;
   }
-
   return credentials;
 }
 
@@ -82,15 +99,41 @@ export async function updateCredentialsForPlatform(
   userCredentialsIds: Array<number>,
   metadata: CredentialMetadata
 ): Promise<void> {
-  const { err, credentials } = await Api.callMethodAsync('updateCredentials', [], 'post', {
-    credentials: newCredentials,
-    userCredentialsIds,
+  console.log('updateCredentialsForPlatform', {
     platform,
-    ...metadata,
+    newCredentials,
+    userCredentialsIds,
+    metadata,
   });
 
-  if (err || !credentials) {
-    throw new Error('Error updating credentials.');
+  if (process.env.EXPO_NEXT_API) {
+    //TODO-JJ make this work for ios as well
+    console.log('API V2 updateCredentialsForPlatform');
+    const user = await UserManager.ensureLoggedInAsync();
+    const api = ApiV2.clientForUser(user);
+    const result = await api.putAsync(
+      `credentials/${platform}/keystore/${metadata.experienceName}`,
+      {
+        credentials: newCredentials,
+      }
+    );
+
+    console.log('API V2 creds result:', result);
+    if (!result) {
+      throw new Error('Error updating credentials.');
+    }
+  } else {
+    const { err, credentials } = await Api.callMethodAsync('updateCredentials', [], 'post', {
+      credentials: newCredentials,
+      userCredentialsIds,
+      platform,
+      ...metadata,
+    });
+
+    console.log({ err, credentials });
+    if (err || !credentials) {
+      throw new Error('Error updating credentials.');
+    }
   }
 }
 
@@ -99,12 +142,31 @@ export async function removeCredentialsForPlatform(
   metadata: CredentialMetadata
 ): Promise<void> {
   // doesn't go through mac rpc, no request id needed
-  const { err } = await Api.callMethodAsync('deleteCredentials', [], 'post', {
-    platform,
-    ...metadata,
-  });
+  console.log('deleteCredentials');
+  console.log({ platform, metadata });
 
-  if (err) {
-    throw new Error('Error deleting credentials.');
+  if (process.env.EXPO_NEXT_API) {
+    //TODO-JJ make this work for ios as well
+    console.log('API V2 Delete CredentialsForPlatform');
+    const user = await UserManager.ensureLoggedInAsync();
+    const api = ApiV2.clientForUser(user);
+    const result = await api.deleteAsync(
+      `credentials/${platform}/keystore/${metadata.experienceName}`
+    );
+
+    console.log('API V2 creds result:', result);
+    if (!result) {
+      throw new Error('Error deleting credentials.');
+    }
+  } else {
+    const { err } = await Api.callMethodAsync('deleteCredentials', [], 'post', {
+      platform,
+      ...metadata,
+    });
+
+    console.log({ err });
+    if (err) {
+      throw new Error('Error deleting credentials.');
+    }
   }
 }
