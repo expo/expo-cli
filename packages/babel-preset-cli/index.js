8f6ea55ab43647f796ab67fb59b55a645a78506a
jkh <jonathan@expo.io>
2020-01-07 12:40:31 -0800

    @jkhales/add nullish coalescing operator (#1415)

--- packages/babel-preset-cli/index.js
+++ packages/babel-preset-cli/index.js
@@ -15,5 +15,6 @@ module.exports = () => ({
     '@babel/plugin-proposal-class-properties',
     ['@babel/plugin-transform-modules-commonjs', { lazy: source => true }],
     '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
   ],
 });
