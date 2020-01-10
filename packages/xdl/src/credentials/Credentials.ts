3d538a409f4ed3d2212d7768dd1decb94c473142
Jonathan 小林 Hales <jonathan@expo.io>
2020-01-10 11:41:49 -0800

    fix typo

--- packages/xdl/src/credentials/Credentials.ts
+++ packages/xdl/src/credentials/Credentials.ts
@@ -189,7 +189,9 @@ export async function updateCredentialsForPlatform(
 
       // reused credentials
       for (const id of userCredentialsIdOverride) {
-        const record = await api.get(`credentials/ios/userCredentials/${id}`, { decrypt: false });
+        const record = await api.getAsync(`credentials/ios/userCredentials/${id}`, {
+          decrypt: false,
+        });
         if (record && record.type === 'push-key') {
           await api.postAsync(`credentials/ios/use/push`, {
             experienceName,
