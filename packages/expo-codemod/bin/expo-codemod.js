a6ad3fd617e9ddcf42f45d57575e8cee198e6926
Ville Immonen <ville.immonen@iki.fi>
2020-01-22 15:07:55 +0200

    BREAKING CHANGE: drop Node.js 8 support (#1463)
    Expo CLI supports Node.js current and LTS versions.
    Node 8 reached end of life on December 31st 2019, so it is no longer supported.

--- packages/expo-codemod/bin/expo-codemod.js
+++ packages/expo-codemod/bin/expo-codemod.js
@@ -2,7 +2,7 @@
 var semver = require('semver');
 var version = process.versions.node;
 
-if (semver.satisfies(version, '8.x.x || >=10.0.0')) {
+if (semver.satisfies(version, '^10.13.0 || ^12.0.0 || ^13.0.0')) {
   require('../build/cli.js')
     .runAsync(process.argv)
     .catch(error => {
@@ -10,5 +10,5 @@ if (semver.satisfies(version, '8.x.x || >=10.0.0')) {
       process.exit(1);
     });
 } else {
-  throw new Error('expo-codemod supports Node versions 8.x.x, 10.x.x and newer.');
+  throw new Error('expo-codemod supports Node versions ^10.13.0, ^12.0.0 and ^13.0.0.');
 }
