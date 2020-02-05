df4c5c73e124d1c96da5926fb97da63f56983eb7
Evan Bacon <baconbrix@gmail.com>
2020-02-05 14:22:28 -0800

    Improve babel tests and error messages

--- packages/webpack-config/src/loaders/__tests__/createBabelLoader-test.js
+++ packages/webpack-config/src/loaders/__tests__/createBabelLoader-test.js
@@ -6,53 +6,89 @@ import { getBabelLoaderRule } from '../createAllLoaders';
 
 const projectRoot = path.resolve(__dirname, '../../../tests/basic');
 
-const env = {
-  projectRoot,
-  mode: 'development',
-  config: { web: { build: { babel: { include: ['custom-lib'] } } } },
-};
+describe('preset', () => {
+  for (const platform of ['ios', 'web']) {
+    it(`Uses a known cache directory for platform ${platform}`, () => {
+      const env = {
+        projectRoot,
+        platform,
+        mode: 'development',
+        config: { web: { build: { babel: { include: ['custom-lib'] } } } },
+      };
 
-const rules = getRules({
-  module: {
-    rules: [getBabelLoaderRule(env)],
-  },
+      const babelLoader = getBabelLoaderRule(env);
+      expect(babelLoader.use.options.cacheDirectory).toMatch(
+        `.expo/${platform}/cache/development/babel-loader`
+      );
+    });
+  }
+
+  it(`uses an optimized loader in production`, () => {
+    const env = {
+      projectRoot,
+      mode: 'production',
+    };
+    const babelLoader = getBabelLoaderRule(env);
+    expect(babelLoader.use.options.compact).toBe(true);
+    expect(babelLoader.use.options.cacheCompression).toBe(false);
+    expect(babelLoader.use.options.caller.mode).toBe('production');
+  });
 });
-for (const library of [
-  'react-native-foo',
-  'custom-lib',
-  'expo-foo',
-  '@expo/foo',
-  'react-navigation-foo',
-]) {
-  it(`matches against libraries ${library}`, () => {
-    const matches = rules.filter(({ rule }) =>
-      conditionMatchesFile(rule, path.join(projectRoot, 'node_modules', library, `foo.js`))
-    );
-    expect(matches.length).toBeGreaterThanOrEqual(1);
-  });
-}
 
-for (const library of ['vue-foo', 'angular-foo', 'not-expo-foo', 'lodash']) {
-  it(`doesn't match against libraries ${library}`, () => {
-    const matches = rules.filter(({ rule }) =>
-      conditionMatchesFile(rule, path.join(projectRoot, 'node_modules', library, `foo.js`))
-    );
-    expect(matches.length).toBe(0);
+describe('includes', () => {
+  let rules;
+
+  beforeAll(() => {
+    const env = {
+      projectRoot,
+      mode: 'development',
+      config: { web: { build: { babel: { include: ['custom-lib'] } } } },
+    };
+
+    rules = getRules({
+      module: {
+        rules: [getBabelLoaderRule(env)],
+      },
+    });
   });
-}
-for (const file of ['js', 'jsx', 'tsx', 'ts', 'mjs']) {
-  it(`matches against ${file}`, () => {
-    const matches = rules.filter(({ rule }) =>
-      conditionMatchesFile(rule, path.join(projectRoot, `foo.${file}`))
-    );
-    expect(matches.length).toBeGreaterThanOrEqual(1);
-  });
-}
-for (const file of ['xml', 'json', 'foo', 'css', 'png']) {
-  it(`doesn't match against ${file}`, () => {
-    const matches = rules.filter(({ rule }) =>
-      conditionMatchesFile(rule, path.join(projectRoot, `foo.${file}`))
-    );
-    expect(matches.length).toBe(0);
-  });
-}
+
+  for (const library of [
+    'react-native-foo',
+    'custom-lib',
+    'expo-foo',
+    '@expo/foo',
+    'react-navigation-foo',
+  ]) {
+    it(`matches against libraries ${library}`, () => {
+      const matches = rules.filter(({ rule }) =>
+        conditionMatchesFile(rule, path.join(projectRoot, 'node_modules', library, `foo.js`))
+      );
+      expect(matches.length).toBeGreaterThanOrEqual(1);
+    });
+  }
+
+  for (const library of ['vue-foo', 'angular-foo', 'not-expo-foo', 'lodash']) {
+    it(`doesn't match against libraries ${library}`, () => {
+      const matches = rules.filter(({ rule }) =>
+        conditionMatchesFile(rule, path.join(projectRoot, 'node_modules', library, `foo.js`))
+      );
+      expect(matches.length).toBe(0);
+    });
+  }
+  for (const file of ['js', 'jsx', 'tsx', 'ts', 'mjs']) {
+    it(`matches against ${file}`, () => {
+      const matches = rules.filter(({ rule }) =>
+        conditionMatchesFile(rule, path.join(projectRoot, `foo.${file}`))
+      );
+      expect(matches.length).toBeGreaterThanOrEqual(1);
+    });
+  }
+  for (const file of ['xml', 'json', 'foo', 'css', 'png']) {
+    it(`doesn't match against ${file}`, () => {
+      const matches = rules.filter(({ rule }) =>
+        conditionMatchesFile(rule, path.join(projectRoot, `foo.${file}`))
+      );
+      expect(matches.length).toBe(0);
+    });
+  }
+});
