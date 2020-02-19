fffdb58b006a40fdd3f8ec57cbaab3a9243766a2
Evan Bacon <baconbrix@gmail.com>
2020-02-19 15:22:11 -0800

    change format to lcov

--- packages/android-manifest/Dangerfile.js
+++ packages/android-manifest/Dangerfile.js
@@ -3,7 +3,7 @@ const { istanbulCoverage } = require('danger-plugin-istanbul-coverage');
 
 schedule(
   istanbulCoverage({
-    coveragePaths: ['./packages/android-manifest/coverage/coverage-final.json'],
+    coveragePaths: ['./packages/android-manifest/coverage/lcov.info'],
     // What to do when the PR doesn't meet the minimum code coverage threshold
     reportMode: 'message', // || "warn" || "fail"
   })
--- packages/android-manifest/jest.config.js
+++ packages/android-manifest/jest.config.js
@@ -8,5 +8,4 @@ module.exports = {
   },
   testEnvironment: 'node',
   resetModules: false,
-  coverageReporters: ['json'],
 };
