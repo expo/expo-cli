e7152a1651923d766988dd5da87bffb4bb364c0c
Evan Bacon <baconbrix@gmail.com>
2020-02-18 17:59:22 -0800

    Migrate prepare-detached-build to TypeScript

--- packages/expo-cli/src/commands/prepare-detached-build.ts
+++ packages/expo-cli/src/commands/prepare-detached-build.ts
@@ -1,14 +1,20 @@
 import { Detach } from '@expo/xdl';
+import { Command } from 'commander';
 
-async function action(projectDir, options) {
+type Options = {
+  platform?: string;
+  skipXcodeConfig: boolean;
+};
+
+async function action(projectDir: string, options: Options) {
   await Detach.prepareDetachedBuildAsync(projectDir, options);
 }
 
-export default program => {
+export default function(program: Command) {
   program
     .command('prepare-detached-build [project-dir]')
     .option('--platform [platform]', 'detached project platform')
     .option('--skipXcodeConfig [bool]', '[iOS only] if true, do not configure Xcode project')
     .description('Prepares a detached project for building')
     .asyncActionProjectDir(action, true);
-};
+}
