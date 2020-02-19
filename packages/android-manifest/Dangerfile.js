81f21c0b1957d6d637d82276cb05fc8cceeab6b5
Evan Bacon <baconbrix@gmail.com>
2020-02-19 15:02:13 -0800

    Update Dangerfile.js

--- packages/android-manifest/Dangerfile.js
+++ packages/android-manifest/Dangerfile.js
@@ -1,4 +1,10 @@
 const { schedule } = require('danger');
 const { istanbulCoverage } = require('danger-plugin-istanbul-coverage');
 
-schedule(istanbulCoverage());
+schedule(
+  istanbulCoverage({
+    coveragePaths: ['./packages/android-manifest/coverage/coverage-final.json'],
+    // What to do when the PR doesn't meet the minimum code coverage threshold
+    reportMode: 'message', // || "warn" || "fail"
+  })
+);
