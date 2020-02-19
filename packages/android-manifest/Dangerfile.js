ff357d9468b4fe2e324f22fa2a1cd7832218d82b
Evan Bacon <baconbrix@gmail.com>
2020-02-19 14:54:17 -0800

    Update Dangerfile.js

--- packages/android-manifest/Dangerfile.js
+++ packages/android-manifest/Dangerfile.js
@@ -1,4 +1,4 @@
-import { schedule } from 'danger';
-import { istanbulCoverage } from 'danger-plugin-istanbul-coverage';
+const { schedule } = require('danger');
+const { istanbulCoverage } = require('danger-plugin-istanbul-coverage');
 
 schedule(istanbulCoverage());
