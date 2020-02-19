d1088a2d310def82f1d0bc718271fe045f849803
Evan Bacon <baconbrix@gmail.com>
2020-02-18 18:03:42 -0800

    Migrate publish-modify to TypeScript

--- packages/expo-cli/src/commands/publish-modify.ts
+++ packages/expo-cli/src/commands/publish-modify.ts
@@ -1,12 +1,9 @@
-/**
- * @flow
- */
-
 import { ApiV2, Project, UserManager } from '@expo/xdl';
+import { Command } from 'commander';
 import log from '../log';
 import * as table from '../commands/utils/cli-table';
 
-export default (program: any) => {
+export default function(program: Command) {
   program
     .command('publish:set [project-dir]')
     .alias('ps')
@@ -19,37 +16,44 @@ export default (program: any) => {
       '-p, --publish-id <publish-id>',
       'The id of the published release to serve from the channel. (Required)'
     )
-    .asyncActionProjectDir(async (projectDir, options) => {
-      if (!options.releaseChannel) {
-        throw new Error('You must specify a release channel.');
+    .asyncActionProjectDir(
+      async (
+        projectDir: string,
+        options: { releaseChannel?: string; publishId?: string }
+      ): Promise<void> => {
+        if (!options.releaseChannel) {
+          throw new Error('You must specify a release channel.');
+        }
+        if (!options.publishId) {
+          throw new Error('You must specify a publish id. You can find ids using publish:history.');
+        }
+        const user = await UserManager.ensureLoggedInAsync();
+        const api = ApiV2.clientForUser(user);
+        try {
+          let result = await api.postAsync('publish/set', {
+            releaseChannel: options.releaseChannel,
+            publishId: options.publishId,
+            slug: await Project.getSlugAsync(projectDir, options),
+          });
+          let tableString = table.printTableJson(
+            result.queryResult,
+            'Channel Set Status ',
+            'SUCCESS'
+          );
+          console.log(tableString);
+        } catch (e) {
+          log.error(e);
+        }
       }
-      if (!options.publishId) {
-        throw new Error('You must specify a publish id. You can find ids using publish:history.');
-      }
-      const user = await UserManager.ensureLoggedInAsync();
-      const api = ApiV2.clientForUser(user);
-      try {
-        let result = await api.postAsync('publish/set', {
-          releaseChannel: options.releaseChannel,
-          publishId: options.publishId,
-          slug: await Project.getSlugAsync(projectDir, options),
-        });
-        let tableString = table.printTableJson(
-          result.queryResult,
-          'Channel Set Status ',
-          'SUCCESS'
-        );
-        console.log(tableString);
-      } catch (e) {
-        log.error(e);
-      }
-    });
+    );
   program
     .command('publish:rollback [project-dir]')
     .alias('pr')
     .description('Rollback an update to a channel.')
     .option('--channel-id <channel-id>', 'The channel id to rollback in the channel. (Required)')
-    .asyncActionProjectDir(async (projectDir, options) => {
+    .asyncActionProjectDir(async (projectDir: string, options: { channelId?: string }): Promise<
+      void
+    > => {
       if (!options.channelId) {
         throw new Error('You must specify a channel id. You can find ids using publish:history.');
       }
@@ -70,4 +74,4 @@ export default (program: any) => {
         log.error(e);
       }
     });
-};
+}
