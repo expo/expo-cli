7bfdb24ff085e42af8c94e74ef0d9467d4f02d7f
Evan Bacon <baconbrix@gmail.com>
2020-02-19 16:51:20 -0800

    Add config to coverage test

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   projects: [
     require('./packages/android-manifest/jest.config'),
+    require('./packages/config/jest.config'),
     require('./packages/schemer/jest.config'),
   ],
   testPathIgnorePatterns: ['.*'],
