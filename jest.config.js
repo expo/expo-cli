70494610cad73167e4e5e9ca48c17a1e68afd665
Evan Bacon <baconbrix@gmail.com>
2020-02-19 16:48:27 -0800

    Add schemer to coverage test

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  projects: [require('./packages/android-manifest/jest.config')],
+  projects: [
+    require('./packages/android-manifest/jest.config'),
+    require('./packages/schemer/jest.config'),
+  ],
   testPathIgnorePatterns: ['.*'],
 };
