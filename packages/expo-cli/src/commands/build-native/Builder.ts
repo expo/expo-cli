e0479ad9c524bea524f3dfbf36fc72ec6b7997a9
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2020-03-10 13:32:19 +0100

    [turtle-v2] update build-tools 0.1.4 (#1639)

--- packages/expo-cli/src/commands/build-native/Builder.ts
+++ packages/expo-cli/src/commands/build-native/Builder.ts
@@ -1,18 +1,21 @@
 import os from 'os';
 import path from 'path';
 
-import { Platform } from '@expo/config';
+import { Platform, prepareJob } from '@expo/build-tools';
 import { TurtleApi, User } from '@expo/xdl';
 import fs from 'fs-extra';
 import ora from 'ora';
 import uuidv4 from 'uuid/v4';
 
-import { Options, prepareJob } from './prepare';
 import { getLogsUrl, makeProjectTarball, waitForBuildEnd } from './utils';
 import log from '../../log';
 
 export interface StatusResult {
   builds: BuildInfo[];
+}
+
+export interface Options {
+  platform: Platform;
 }
 
 export interface BuildInfo {
@@ -38,12 +41,16 @@ export default class Builder {
     const tarPath = path.join(os.tmpdir(), `${uuidv4()}.tar.gz`);
     try {
       await makeProjectTarball(tarPath);
+
       const spinner = ora('Uploading project to server.').start();
       const { s3Url } = await this.client.uploadFile(tarPath);
       spinner.succeed('Project uploaded.');
-      const job = await prepareJob(options, s3Url, projectDir);
+
+      const job = await prepareJob(options.platform, s3Url, projectDir);
       const { buildId } = await this.client.postAsync('builds', job);
+
       log(`Build logs: ${getLogsUrl(buildId)}`);
+
       return await waitForBuildEnd(this.client, buildId);
     } finally {
       await fs.remove(tarPath);
