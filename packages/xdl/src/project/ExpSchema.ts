241dbb7803f2e25cd5794316a92de18a76e84c3c
Evan Bacon <baconbrix@gmail.com>
2020-03-12 13:45:42 -0700

    Update ExpSchema.ts

--- packages/xdl/src/project/ExpSchema.ts
+++ packages/xdl/src/project/ExpSchema.ts
@@ -1,9 +1,9 @@
+import { getConfig } from '@expo/config';
 import Schemer from '@expo/schemer';
 import fs from 'fs';
 import path from 'path';
 
 import Api from '../Api';
-import { getProjectConfigAsync } from '../Config';
 
 export type Schema = any;
 export type AssetSchema = { schema: Schema; fieldPath: string };
@@ -11,7 +11,7 @@ export type AssetSchema = { schema: Schema; fieldPath: string };
 let _xdlSchemaJson: { [sdkVersion: string]: Schema } = {};
 
 export async function validatorFromProjectRoot(projectRoot: string): Promise<Schemer> {
-  const { exp } = await getProjectConfigAsync(projectRoot, { skipSDKVersionRequirement: false });
+  const { exp } = getConfig(projectRoot);
   if (!exp.sdkVersion) throw new Error(`Couldn't read local manifest`);
   const schema = await getSchemaAsync(exp.sdkVersion);
   const validator = new Schemer(schema);
