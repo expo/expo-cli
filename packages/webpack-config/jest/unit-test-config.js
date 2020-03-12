1f649ea6108e436dc076d98b759aaa1cd0c29847
Evan Bacon <baconbrix@gmail.com>
2020-03-12 13:43:54 -0700

    Update unit-test-config.js

--- packages/webpack-config/jest/unit-test-config.js
+++ packages/webpack-config/jest/unit-test-config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   rootDir: path.resolve(__dirname, '..'),
   roots: ['src'],
-  displayName: require('../package.json').name,
+  displayName: require('@expo/webpack-config/package.json').name,
   testRegex: '/__(tests|testfixtures)__/.*(test|spec)\\.(j|t)sx?$',
   moduleNameMapper: {
     '^jest/(.*)': path.join(__dirname, '../../../jest/$1'),
