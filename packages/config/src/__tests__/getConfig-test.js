25d8613cd0e070f7f42e9b34b7555badeb5c0672
Evan Bacon <baconbrix@gmail.com>
2020-03-13 19:43:10 -0700

    [config] Spawn process to read config new each time (#1689)
    * Spawn a sync process for reevaluating the project config
    
    * Migrate script to TypeScript
    
    * updated tests
    
    * Fix babel register
    
    * Update read-config.ts

--- packages/config/src/__tests__/getConfig-test.js
+++ packages/config/src/__tests__/getConfig-test.js
@@ -1,24 +1,6 @@
 import { join } from 'path';
 
-import { findAndEvalConfig, serializeAndEvaluate } from '../getConfig';
-
-describe('serializeAndEvaluate', () => {
-  it(`serializes item`, () => {
-    expect(
-      serializeAndEvaluate({
-        foo: 'bar',
-        boo: true,
-        inn: 200,
-        then: [true, { foo: 'bar' }],
-        alpha: () => ({ beta: ['val'] }),
-        last: {
-          bar: 'foo',
-          charlie: [2, 'delta'],
-        },
-      })
-    ).toMatchSnapshot();
-  });
-});
+import { findAndEvalConfig } from '../getConfig';
 
 describe('findAndEvalConfig', () => {
   it(`throws a useful error for JS configs with a syntax error`, () => {
