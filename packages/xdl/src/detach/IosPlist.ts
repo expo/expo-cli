cdda03c53371fbe5d90a7f2708aa5d37ddb3fd29
Brent Vatne <brentvatne@gmail.com>
2020-03-16 20:21:05 -0700

    Use fs-extra instead of spawning cp/rm

--- packages/xdl/src/detach/IosPlist.ts
+++ packages/xdl/src/detach/IosPlist.ts
@@ -50,7 +50,7 @@ async function modifyAsync(plistPath: string, plistName: string, transform: (con
   config = transform(config);
 
   // back up old plist and swap in modified one
-  await spawnAsyncThrowError('/bin/cp', [configPlistName, `${configPlistName}.bak`]);
+  await fs.copy(configPlistName, `${configPlistName}.bak`);
   await fs.writeFile(configFilename, JSON.stringify(config));
   if (process.platform === 'darwin') {
     await spawnAsyncThrowError('plutil', [
@@ -89,7 +89,7 @@ async function createBlankAsync(plistPath: string, plistName: string) {
   }
 
   // remove tmp json file
-  await spawnAsyncThrowError('/bin/rm', [tmpConfigFile]);
+  await fs.remove(tmpConfigFile);
 }
 
 async function cleanBackupAsync(plistPath: string, plistName: string, restoreOriginal = true) {
