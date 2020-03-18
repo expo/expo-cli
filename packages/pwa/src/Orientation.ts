3c4c4a735a1425cf835535963b8749af5cf8bf57
Evan Bacon <baconbrix@gmail.com>
2020-03-18 15:10:58 -0700

    Added basic tests

--- packages/pwa/src/Orientation.ts
+++ packages/pwa/src/Orientation.ts
@@ -10,44 +10,24 @@ export type Orientation =
   | 'omit';
 
 // https://developer.mozilla.org/en-US/docs/Web/Manifest#orientation
-const VALID_ORIENTATIONS = [
-  'any',
-  'natural',
-  'landscape',
-  'landscape-primary',
-  'landscape-secondary',
-  'portrait',
-  'portrait-primary',
-  'portrait-secondary',
-  'omit',
-];
+const ANY_ORIENTATIONS = ['any', 'natural', 'omit'];
 
-const PORTRAIT_ORIENTATIONS = [
-  'any',
-  'natural',
-  'portrait',
-  'portrait-primary',
-  'portrait-secondary',
-  'omit',
-];
+const PORTRAIT_ORIENTATIONS = ['portrait', 'portrait-primary', 'portrait-secondary'];
 
-const LANDSCAPE_ORIENTATIONS = [
-  'any',
-  'natural',
-  'landscape',
-  'landscape-primary',
-  'landscape-secondary',
-  'omit',
-];
+const LANDSCAPE_ORIENTATIONS = ['landscape', 'landscape-primary', 'landscape-secondary'];
 
 export function isValid(orientation: string): orientation is Orientation {
-  return VALID_ORIENTATIONS.includes(orientation);
+  return (
+    ANY_ORIENTATIONS.includes(orientation) ||
+    LANDSCAPE_ORIENTATIONS.includes(orientation) ||
+    PORTRAIT_ORIENTATIONS.includes(orientation)
+  );
 }
 
 export function isLandscape(orientation: string): boolean {
-  return LANDSCAPE_ORIENTATIONS.includes(orientation);
+  return ANY_ORIENTATIONS.includes(orientation) || LANDSCAPE_ORIENTATIONS.includes(orientation);
 }
 
 export function isPortrait(orientation: string): boolean {
-  return PORTRAIT_ORIENTATIONS.includes(orientation);
+  return ANY_ORIENTATIONS.includes(orientation) || PORTRAIT_ORIENTATIONS.includes(orientation);
 }
