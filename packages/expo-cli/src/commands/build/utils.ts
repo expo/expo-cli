700d036cbe6b8eeff46d2cfc65834af186491ceb
cruzach <charliecruzan@gmail.com>
2020-04-06 10:32:42 -0400

    add latest SDK version to err msg

--- packages/expo-cli/src/commands/build/utils.ts
+++ packages/expo-cli/src/commands/build/utils.ts
@@ -10,14 +10,15 @@ export async function checkIfSdkIsSupported(
   const isSupported = await Versions.canTurtleBuildSdkVersion(sdkVersion, platform);
   const minimumSdkVersionSupported = await Versions.oldestSupportedMajorVersionAsync();
   const majorSdkVersion = Number(sdkVersion.split('.')[0]);
+  const { version: latestSDKVersion } = await Versions.newestReleasedSdkVersionAsync();
 
   if (!isSupported) {
     log.error(
       chalk.red(
         'Unsupported SDK version: our app builders ' +
           (majorSdkVersion < minimumSdkVersionSupported
-            ? `no longer support SDK version ${majorSdkVersion}. Please upgrade to at least SDK ${minimumSdkVersionSupported}.`
-            : `do not support SDK version ${majorSdkVersion}, yet.`)
+            ? `no longer support SDK version ${majorSdkVersion}. Please upgrade to at least SDK ${minimumSdkVersionSupported}, or to the latest SDK version (${latestSDKVersion}).`
+            : `do not support SDK version ${majorSdkVersion}, yet. The latest SDK version is ${latestSDKVersion}.`)
       )
     );
     throw new Error('Unsupported SDK version');
