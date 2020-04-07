275b1010778c52a1569fa440e8aedde6f79cc032
Juwan Wheatley <datwheat@gmail.com>
2020-04-07 16:24:09 -0700

    [dev-tools] add emotion babel plugin dep

--- packages/dev-tools/client/babel.config.js
+++ packages/dev-tools/client/babel.config.js
@@ -3,7 +3,7 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      ['emotion'],
+      'emotion',
       [
         'module-resolver',
         {
