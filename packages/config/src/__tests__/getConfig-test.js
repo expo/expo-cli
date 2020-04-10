a710a45d3112e11fc2a3d2b268c3e6e6abf8921a
Evan Bacon <baconbrix@gmail.com>
2020-04-10 13:22:32 -0700

    Add app.config.js to error test

--- packages/config/src/__tests__/getConfig-test.js
+++ packages/config/src/__tests__/getConfig-test.js
@@ -4,7 +4,9 @@ import { getConfigFilePaths } from '../Config';
 import { getDynamicConfig } from '../getConfig';
 
 describe('getDynamicConfig', () => {
-  it(`throws a useful error for JS configs with a syntax error`, () => {
+  // This tests error are thrown properly and ensures that a more specific
+  // config is used instead of defaulting to a valid substitution.
+  it(`throws a useful error for dynamic configs with a syntax error`, () => {
     const paths = getConfigFilePaths(join(__dirname, 'fixtures/behavior/syntax-error'));
     expect(() => getDynamicConfig(paths.dynamicConfigPath, {})).toThrowError(
       'Unexpected token (3:4)'
