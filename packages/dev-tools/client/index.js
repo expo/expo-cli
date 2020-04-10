a68647bd5a85d53458fb440e79e6af0aa23e9146
Juwan Wheatley <datwheat@gmail.com>
2020-04-09 18:53:55 -0700

    [dev-tools] import name properly

--- packages/dev-tools/client/index.js
+++ packages/dev-tools/client/index.js
@@ -1,10 +1,10 @@
 import { AppRegistry } from 'react-native';
 
 import App from './App';
-import { name as appName } from './app.json';
+import { expo } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(expo.name, () => App);
 
 const rootTag = document.getElementById('root');
 
-AppRegistry.runApplication(appName, { rootTag });
+AppRegistry.runApplication(expo.name, { rootTag });
