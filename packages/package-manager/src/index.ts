a9b7fe2ae8eb3a7fd856548be99f47818b397b1b
Evan Bacon <baconbrix@gmail.com>
2020-04-15 15:49:21 -0700

    Added offline support to Yarn package-manager

--- packages/package-manager/src/index.ts
+++ packages/package-manager/src/index.ts
@@ -1,3 +1,5 @@
 export * from './PackageManager';
 export * from './NodePackageManagers';
 export * from './CocoaPodsPackageManager';
+export { default as shouldUseYarn } from './utils/shouldUseYarn';
+export { default as isYarnOfflineAsync } from './utils/isYarnOfflineAsync';
---
+++ packages/package-manager/src/utils/shouldUseYarn.ts
@@ -0,0 +1,13 @@
+import { execSync } from 'child_process';
+
+export default function shouldUseYarn(): boolean {
+  try {
+    if (process.env.npm_config_user_agent?.startsWith('yarn')) {
+      return true;
+    }
+    execSync('yarnpkg --version', { stdio: 'ignore' });
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
