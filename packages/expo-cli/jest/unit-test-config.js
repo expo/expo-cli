fc7a81578af6c14f63828dc7d4bcdbad741e3982
Quinlan Jung <quin@expo.io>
2020-04-22 11:28:59 -0700

    Apply suggestions from code review
    Co-Authored-By: Ville Immonen <ville.immonen@iki.fi>

--- packages/expo-cli/jest/unit-test-config.js
+++ packages/expo-cli/jest/unit-test-config.js
@@ -4,13 +4,11 @@ module.exports = {
   rootDir: path.resolve(__dirname, '..'),
   roots: ['__mocks__', 'src'],
   displayName: require('../package.json').name,
-  testRegex: '/__(tests|testfixtures)__/.*(test|spec)\\.(j|t)sx?$',
   moduleNameMapper: {
     '^jest/(.*)': path.join(__dirname, '../../../jest/$1'),
   },
   transform: {
-    '^.+\\.[jt]sx?$': ['babel-jest', { configFile: require.resolve('../babel.config.js') }],
+    '^.+\\.[jt]sx?$': ['babel-jest', { configFile: require.resolve(__dirname, '../babel.config.js') }],
   },
   testEnvironment: 'node',
-  resetModules: false,
 };
