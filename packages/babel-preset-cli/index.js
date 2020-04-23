2662d696fdf07e422f01cbbef764dfd40916c54d
Evan Bacon <baconbrix@gmail.com>
2020-04-22 17:33:12 -0700

    Update index.js

--- packages/babel-preset-cli/index.js
+++ packages/babel-preset-cli/index.js
@@ -13,7 +13,7 @@ module.exports = () => ({
   ],
   plugins: [
     require.resolve('@babel/plugin-proposal-class-properties'),
-    [require.resolve('@babel/plugin-transform-modules-commonjs'), { lazy: true }],
+    [require.resolve('@babel/plugin-transform-modules-commonjs'), { lazy: source => true }],
     require.resolve('@babel/plugin-proposal-optional-chaining'),
     require.resolve('@babel/plugin-proposal-nullish-coalescing-operator'),
   ],
