6e3faae6497d05f6718451b6c210bc3d6dc7b5c9
Cedric van Putten <me@bycedric.com>
2020-04-27 11:58:44 +0200

    fix(expo-cli): use proper path in jest e2e config

--- packages/expo-cli/jest/e2e-test-config.js
+++ packages/expo-cli/jest/e2e-test-config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   preset: '@expo/jest-preset-cli',
-  displayName: require('./package').name,
+  displayName: require('../package').name,
   rootDir: path.resolve(__dirname, '..'),
   roots: ['e2e'],
 };
