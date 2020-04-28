675380d06de3179fcd1dcfbd7a3f0c593dc55dca
Evan Bacon <baconbrix@gmail.com>
2020-04-27 23:07:01 -0700

    Update whoami.ts

--- packages/expo-cli/src/commands/whoami.ts
+++ packages/expo-cli/src/commands/whoami.ts
@@ -7,10 +7,13 @@ import log from '../log';
 async function action() {
   const user = await UserManager.getCurrentUserAsync({ silent: true });
   if (user) {
-    log(`Logged in as ${chalk.green(user.username)}`);
+    if (process.stdout.isTTY) {
+      log(`Logged in as ${chalk.cyan(user.username)}`);
+    }
     log.raw(user.username);
   } else {
     log(`\u203A Not logged in, run ${chalk.cyan`expo login`} to authenticate`);
+    process.exit(1);
   }
 }
 
