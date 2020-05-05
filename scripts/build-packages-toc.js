a84d10f43acee35cad181b02e641a4ed5a21e179
Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>
2020-05-05 16:40:37 +0200

    [scripts] improvement: added auto-generating padding in cells in build-packages-toc script

--- scripts/build-packages-toc.js
+++ scripts/build-packages-toc.js
@@ -1,19 +1,58 @@
 #!/usr/bin/env node
-let path = require('path');
+const path = require('path');
 const fs = require('fs-extra');
 
 function toCamel(s) {
   return s.replace(/(-\w)/g, m => m[1].toUpperCase());
 }
 
+/**
+ * @param {string} cell
+ * @param {number} width
+ * @returns {string}
+ */
+function padCell(cell, width) {
+  const padsCount = width - cell.length;
+  return cell + ' '.repeat(padsCount);
+}
+
+/**
+ * @param {string[]} elements
+ */
+function getMaxWidth(elements) {
+  return elements.reduce((acc, el) => Math.max(acc, el.length), 0);
+}
+
+let firstCellMaxWidth = null;
+let secondCellMaxWidth = null;
+
+/**
+ * @param {[string, string]} entries - one row cells contents
+ * @param {number} idx
+ * @param {[string, string][]} entriesArray
+ * @returns {string} resulting markdown table row with cells with padded content
+ */
+function convertToCells([firstCell, secondCell], idx, entriesArray) {
+  firstCellMaxWidth =
+    firstCellMaxWidth !== null ? firstCellMaxWidth : getMaxWidth(entriesArray.map(el => el[0]));
+  secondCellMaxWidth =
+    secondCellMaxWidth !== null ? secondCellMaxWidth : getMaxWidth(entriesArray.map(el => el[1]));
+
+  const result = `| ${padCell(firstCell, firstCellMaxWidth)} | ${padCell(
+    secondCell,
+    secondCellMaxWidth
+  )} |`;
+  return result;
+}
+
 async function run() {
   const pkgsPath = path.join(__dirname, '..', 'packages');
   const contents = await fs.readdir(pkgsPath);
 
-  let entries = [
-    `\n<!-- Begin auto-generation -->\n`,
-    '| Package | Coverage |',
-    '|----------|---------|',
+  const lines = [`\n<!-- Begin auto-generation -->\n`];
+  const entries = [
+    ['Package', 'Coverage'],
+    ['---', '---'],
   ];
   for (const pkg of contents) {
     try {
@@ -34,18 +73,22 @@ async function run() {
 
       // Maybe do package size too https://flat.badgen.net/packagephobia/publish/pod-install
       // Currently doesn't work with xdl or expo-cli
-      entries.push(
-        `| [**\`${pkgJson.name}\`**](${repoLink}) | [![badges](${codecovIcon})](${codecovLink}) |`
-      );
+      entries.push([
+        `[**\`${pkgJson.name}\`**](${repoLink})`,
+        `[![badges](${codecovIcon})](${codecovLink})`,
+      ]);
     } catch (e) {
       console.log(`Skipping ${pkg}`);
     }
   }
 
-  entries.push(`\n<!-- Generated with $ node scripts/build-packages-toc.js -->\n`);
+  const paddedCells = entries.map(convertToCells);
+
+  lines.push(...paddedCells);
+  lines.push(`\n<!-- Generated with $ node scripts/build-packages-toc.js -->\n`);
 
   console.log(`\n\n\n\n`);
-  console.log(entries.join('\n'));
+  console.log(lines.join('\n'));
 }
 
 run().catch(error => {
