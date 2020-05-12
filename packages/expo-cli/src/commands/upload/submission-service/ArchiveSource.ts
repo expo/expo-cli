86f684e80ba5bc361e4af950a4ac51f1697deb0e
Dominik Sokal <dominik.sokal@swmansion.com>
2020-05-12 12:11:47 +0200

    [expo-cli] refactor ArchiveSource module

--- packages/expo-cli/src/commands/upload/submission-service/ArchiveSource.ts
+++ packages/expo-cli/src/commands/upload/submission-service/ArchiveSource.ts
@@ -59,97 +59,105 @@ export type ArchiveSource =
   | ArchivePromptSource;
 
 async function getArchiveUrlAsync(source: ArchiveSource): Promise<string> {
-  if (source.sourceType === ArchiveSourceType.url) {
-    return source.url;
-  } else if (source.sourceType === ArchiveSourceType.latest) {
-    const builds = await StandaloneBuild.getStandaloneBuilds(
-      {
-        platform: source.platform,
-        owner: source.owner,
-        slug: source.slug,
-      },
-      1
-    );
-    if (builds.length === 0) {
-      throw new Error("Couldn't find any builds for this project.");
-    }
-    return builds[0].artifacts.url;
-  } else if (source.sourceType === ArchiveSourceType.path) {
-    if (!(await existingFile(source.path))) {
-      throw new Error(`${source.path} doesn't exist`);
-    }
-    const spinner = ora(`Uploading ${source.path}`).start();
-    try {
-      const archiveUrl = await uploadAsync(UploadType.SUBMISSION_APP_ARCHIVE, source.path);
-      spinner.succeed();
-      return archiveUrl;
-    } catch (err) {
-      spinner.fail();
-      throw err;
-    }
-  } else if (source.sourceType === ArchiveSourceType.buildId) {
-    const build = await StandaloneBuild.getStandaloneBuildById({
+  switch (source.sourceType) {
+    case ArchiveSourceType.url:
+      return await handleUrlSourceAsync(source);
+    case ArchiveSourceType.latest:
+      return await handleLatestsSourceAsync(source);
+    case ArchiveSourceType.path:
+      return await handlePathSourceAsync(source);
+    case ArchiveSourceType.buildId:
+      return await handleBuildIdSourceAsync(source);
+    case ArchiveSourceType.prompt:
+      return await handlePromptSourceAsync(source);
+  }
+}
+
+async function handleUrlSourceAsync(source: ArchiveUrlSource): Promise<string> {
+  return source.url;
+}
+
+async function handleLatestsSourceAsync(source: ArchiveLatestSource): Promise<string> {
+  const builds = await StandaloneBuild.getStandaloneBuilds(
+    {
       platform: source.platform,
-      id: source.id,
       owner: source.owner,
       slug: source.slug,
-    });
-    if (!build) {
-      throw new Error(`Couldn't find build with id: ${source.id}`);
-    }
-    return build.artifacts.url;
-  } else if (source.sourceType === ArchiveSourceType.prompt) {
-    const { sourceType: sourceTypeRaw } = await prompt({
-      name: 'sourceType',
-      type: 'list',
-      message: 'What would you like to submit?',
-      choices: [
-        { name: 'I have a url to the app archive', value: ArchiveSourceType.url },
-        {
-          name: "I'd like to upload the app archive from my computer",
-          value: ArchiveSourceType.path,
-        },
-        {
-          name: 'The lastest build from Expo Servers',
-          value: ArchiveSourceType.latest,
-        },
-        {
-          name: 'A build identified by a build id',
-          value: ArchiveSourceType.buildId,
-        },
-      ],
-    });
-    const sourceType = sourceTypeRaw as ArchiveSourceType;
-    if (sourceType === ArchiveSourceType.url) {
-      const { url } = await prompt({
-        name: 'url',
-        message: 'URL:',
-        type: 'input',
-        validate: (url: string): string | boolean =>
-          validateUrl(url) || `${url} does not conform to HTTP format`,
-      });
+    },
+    1
+  );
+  if (builds.length === 0) {
+    throw new Error("Couldn't find any builds for this project.");
+  }
+  return builds[0].artifacts.url;
+}
+
+async function handlePathSourceAsync(source: ArchivePathSource): Promise<string> {
+  if (!(await existingFile(source.path))) {
+    throw new Error(`${source.path} doesn't exist`);
+  }
+  const spinner = ora(`Uploading ${source.path}`).start();
+  try {
+    const archiveUrl = await uploadAsync(UploadType.SUBMISSION_APP_ARCHIVE, source.path);
+    spinner.succeed();
+    return archiveUrl;
+  } catch (err) {
+    spinner.fail();
+    throw err;
+  }
+}
+
+async function handleBuildIdSourceAsync(source: ArchiveBuildIdSource): Promise<string> {
+  const build = await StandaloneBuild.getStandaloneBuildById({
+    platform: source.platform,
+    id: source.id,
+    owner: source.owner,
+    slug: source.slug,
+  });
+  if (!build) {
+    throw new Error(`Couldn't find build with id: ${source.id}`);
+  }
+  return build.artifacts.url;
+}
+
+async function handlePromptSourceAsync(source: ArchivePromptSource): Promise<string> {
+  const { sourceType: sourceTypeRaw } = await prompt({
+    name: 'sourceType',
+    type: 'list',
+    message: 'What would you like to submit?',
+    choices: [
+      { name: 'I have a url to the app archive', value: ArchiveSourceType.url },
+      {
+        name: "I'd like to upload the app archive from my computer",
+        value: ArchiveSourceType.path,
+      },
+      {
+        name: 'The lastest build from Expo Servers',
+        value: ArchiveSourceType.latest,
+      },
+      {
+        name: 'A build identified by a build id',
+        value: ArchiveSourceType.buildId,
+      },
+    ],
+  });
+  const sourceType = sourceTypeRaw as ArchiveSourceType;
+  switch (sourceType) {
+    case ArchiveSourceType.url: {
+      const url = await askForArchiveUrlAsync();
       return getArchiveUrlAsync({
         sourceType: ArchiveSourceType.url,
         url,
       });
-    } else if (sourceType === ArchiveSourceType.path) {
-      const { path } = await prompt({
-        name: 'path',
-        message: 'Path to the app archive file:',
-        type: 'input',
-        validate: async (path: string): Promise<boolean | string> => {
-          if (!(await existingFile(path, false))) {
-            return `File ${path} doesn't exist.`;
-          } else {
-            return true;
-          }
-        },
-      });
+    }
+    case ArchiveSourceType.path: {
+      const path = await askForArchivePathAsync();
       return getArchiveUrlAsync({
         sourceType: ArchiveSourceType.path,
         path,
       });
-    } else if (sourceType === ArchiveSourceType.latest) {
+    }
+    case ArchiveSourceType.latest: {
       const { owner, slug } = getAppConfig(source.projectDir);
       return getArchiveUrlAsync({
         sourceType: ArchiveSourceType.latest,
@@ -157,13 +165,9 @@ async function getArchiveUrlAsync(source: ArchiveSource): Promise<string> {
         owner,
         slug,
       });
-    } else if (sourceType === ArchiveSourceType.buildId) {
-      const { id } = await prompt({
-        name: 'id',
-        message: 'Build ID:',
-        type: 'input',
-        validate: (val: string): boolean => val !== '',
-      });
+    }
+    case ArchiveSourceType.buildId: {
+      const id = await askForBuildIdAsync();
       const { owner, slug } = getAppConfig(source.projectDir);
       return getArchiveUrlAsync({
         sourceType: ArchiveSourceType.buildId,
@@ -172,12 +176,47 @@ async function getArchiveUrlAsync(source: ArchiveSource): Promise<string> {
         slug,
         id,
       });
-    } else {
-      throw new Error('This should never happen');
     }
-  } else {
-    throw new Error('This should never happen');
+    case ArchiveSourceType.prompt:
+      throw new Error('This should never happen');
   }
+}
+
+async function askForArchiveUrlAsync(): Promise<string> {
+  const { url } = await prompt({
+    name: 'url',
+    message: 'URL:',
+    type: 'input',
+    validate: (url: string): string | boolean =>
+      validateUrl(url) || `${url} does not conform to HTTP format`,
+  });
+  return url;
+}
+
+async function askForArchivePathAsync(): Promise<string> {
+  const { path } = await prompt({
+    name: 'path',
+    message: 'Path to the app archive file:',
+    type: 'input',
+    validate: async (path: string): Promise<boolean | string> => {
+      if (!(await existingFile(path, false))) {
+        return `File ${path} doesn't exist.`;
+      } else {
+        return true;
+      }
+    },
+  });
+  return path;
+}
+
+async function askForBuildIdAsync(): Promise<string> {
+  const { id } = await prompt({
+    name: 'id',
+    message: 'Build ID:',
+    type: 'input',
+    validate: (val: string): boolean => val !== '',
+  });
+  return id;
 }
 
 function validateUrl(url: string): boolean {
