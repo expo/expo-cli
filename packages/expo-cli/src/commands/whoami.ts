6fd6c6adc526dc8b279d1113a0ff1770b6f30070
Ville Immonen <ville.immonen@iki.fi>
2020-05-13 11:17:18 +0300

    fix: always print whoami output to stdout (#2101)

--- packages/expo-cli/src/commands/whoami.ts
+++ packages/expo-cli/src/commands/whoami.ts
@@ -4,20 +4,21 @@ import { Command } from 'commander';
 
 import log from '../log';
 
-async function action() {
+async function action(command: Command) {
   const user = await UserManager.getCurrentUserAsync({ silent: true });
   if (user) {
-    if (process.stdout.isTTY) {
+    if (command.parent?.nonInteractive) {
+      log.nested(user.username);
+    } else {
       log(`Logged in as ${chalk.cyan(user.username)}`);
     }
-    log.raw(user.username);
   } else {
     log(`\u203A Not logged in, run ${chalk.cyan`expo login`} to authenticate`);
     process.exit(1);
   }
 }
 
-export default function(program: Command) {
+export default function (program: Command) {
   program
     .command('whoami')
     .alias('w')
