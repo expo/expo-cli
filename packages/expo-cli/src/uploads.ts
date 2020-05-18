d70bcce76be4ae955c74b77aaddc1a462c4e261d
Dominik Sokal <dominik.sokal@swmansion.com>
2020-05-18 16:44:05 +0200

    [expo-cli] add progress bar for uploads

--- packages/expo-cli/src/uploads.ts
+++ packages/expo-cli/src/uploads.ts
@@ -2,8 +2,7 @@ import fs from 'fs';
 import { Readable } from 'stream';
 
 import { ApiV2, FormData, UserManager } from '@expo/xdl';
-import axios from 'axios';
-import concat from 'concat-stream';
+import got, { Progress } from 'got';
 import md5File from 'md5-file/promise';
 
 enum UploadType {
@@ -11,14 +10,24 @@ enum UploadType {
   SUBMISSION_APP_ARCHIVE = 'submission-app-archive',
 }
 
+type ProgressHandler = (progress: Progress) => void;
+
+async function uploadAsync(
+  uploadType: UploadType,
+  path: string,
+  handleProgressEvent?: ProgressHandler
+): Promise<string> {
+  const presignedPost = await obtainS3PresignedPostAsync(uploadType, path);
+  return await uploadWithPresignedPostAsync(
+    fs.createReadStream(path),
+    presignedPost,
+    handleProgressEvent
+  );
+}
+
 interface S3PresignedPost {
   url: string;
   fields: Record<string, string>;
-}
-
-async function uploadAsync(uploadType: UploadType, filePath: string): Promise<string> {
-  const presignedPost = await obtainS3PresignedPostAsync(uploadType, filePath);
-  return await uploadWithPresignedPostAsync(fs.createReadStream(filePath), presignedPost);
 }
 
 async function obtainS3PresignedPostAsync(
@@ -34,25 +43,23 @@ async function obtainS3PresignedPostAsync(
   return presignedUrl;
 }
 
-async function uploadWithPresignedPostAsync(src: Readable, presignedPost: S3PresignedPost) {
+async function uploadWithPresignedPostAsync(
+  stream: Readable,
+  presignedPost: S3PresignedPost,
+  handleProgressEvent?: ProgressHandler
+) {
   const form = new FormData();
   for (const [fieldKey, fieldValue] of Object.entries(presignedPost.fields)) {
     form.append(fieldKey, fieldValue);
   }
-  form.append('file', src);
-  const formBuffer = await convertFormDataToBuffer(form);
-  // add progress bar
-  const result = await axios.post(presignedPost.url, formBuffer, {
-    headers: form.getHeaders(),
-    maxContentLength: formBuffer.byteLength,
-  });
-  return String(result.headers.location);
-}
-
-async function convertFormDataToBuffer(formData: FormData): Promise<Buffer> {
-  return new Promise(resolve => {
-    formData.pipe(concat({ encoding: 'buffer' }, data => resolve(data)));
-  });
+  form.append('file', stream);
+  const formHeaders = form.getHeaders();
+  let uploadPromise = got.post(presignedPost.url, { body: form, headers: { ...formHeaders } });
+  if (handleProgressEvent) {
+    uploadPromise = uploadPromise.on('uploadProgress', handleProgressEvent);
+  }
+  const response = await uploadPromise;
+  return String(response.headers.location);
 }
 
 async function getApiClientForUser(): Promise<ApiV2> {
