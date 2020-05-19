6ce9d4e1a85d8100a318b6dea789761ddcbc3ab4
Evan Bacon <baconbrix@gmail.com>
2020-05-19 11:42:14 -0700

    Normalize config

--- packages/webpack-config/src/__tests__/webpack.config-test.js
+++ packages/webpack-config/src/__tests__/webpack.config-test.js
@@ -5,19 +5,33 @@ import createConfig from '..';
 
 const projectRoot = dirname(require.resolve('@expo/webpack-config/e2e/basic'));
 
+function normalizeConfig(config) {
+  // Replace plugins with their names
+  // in the future we may want to selectively test certain plugin shapes
+  const plugins = config.plugins;
+  config.plugins = [];
+  // Just use plugin names in order
+  for (const plugin of plugins) {
+    config.plugins.push(plugin.constructor.name);
+  }
+
+  // Make the paths be relative to the project
+  const normalized = normalizePaths(config, value => value.split('expo-cli/').pop());
+
+  // performance is disabled in CI
+  delete normalized.performance;
+  return normalized;
+}
+
 describe(`ios`, () => {
   it('ios development', async () => {
     const config = await createConfig({ mode: 'development', platform: 'ios', projectRoot });
-    const normalized = normalizePaths(config, value => value.split('expo-cli/').pop());
-
-    // performance is disabled in CI
-    delete normalized.performance;
-
+    const normalized = normalizeConfig(config);
     expect(normalized).toMatchSnapshot();
   });
   it('ios production', async () => {
     const config = await createConfig({ mode: 'production', platform: 'ios', projectRoot });
-    const normalized = normalizePaths(config, value => value.split('expo-cli/').pop());
+    const normalized = normalizeConfig(config);
 
     expect(normalized).toMatchSnapshot();
   });
@@ -25,23 +39,15 @@ describe(`ios`, () => {
 describe(`web`, () => {
   it('web development', async () => {
     const config = await createConfig({ mode: 'development', platform: 'web', projectRoot });
-    const normalized = normalizePaths(config, value => value.split('expo-cli/').pop());
-
-    // performance is disabled in CI
-    delete normalized.performance;
+    const normalized = normalizeConfig(config);
 
     expect(normalized).toMatchSnapshot();
   });
 
   it('web production', async () => {
     const config = await createConfig({ mode: 'production', platform: 'web', projectRoot });
-    const plugins = config.plugins;
-    config.plugins = [];
-    // Just use plugin names in order
-    for (const plugin of plugins) {
-      config.plugins.push(plugin.constructor.name);
-    }
-    const normalized = normalizePaths(config, value => value.split('expo-cli/').pop());
+
+    const normalized = normalizeConfig(config);
 
     expect(normalized).toMatchSnapshot();
   });
