5eec01ba7485374cd69480b9bdc16c2ab7f48e10
Evan Bacon <baconbrix@gmail.com>
2020-05-25 18:26:55 -0700

    [webpack-config] Delete Next.js E2E tests (#2174)
    * Delete Next.js E2E tests
    
    - Next.js tests were written to test when Expo CLI would start and build Next apps.
    - Next.js apps are now run and built by Next's scripts instead.
    - Next tests constantly break and are too hard to maintain.
    - SSR code is mixed in with the other workflows and make them hard to update and read.

--- packages/webpack-config/e2e/__tests__/basic-test.js
+++ packages/webpack-config/e2e/__tests__/basic-test.js
@@ -6,7 +6,7 @@ import config from '../../jest-puppeteer.config';
 const isInCI = getenv.boolish('CI', false);
 const type = getenv.string('EXPO_E2E_COMMAND');
 
-const isProduction = ['buildNextJsFromNextCLI', 'buildNextJsFromExpoCLI', 'build'].includes(type);
+const isProduction = ['build'].includes(type);
 
 let response;
 beforeEach(async () => {
@@ -19,39 +19,24 @@ it(`should match a text element`, async () => {
   });
 });
 
-if (config.hasServerSideRendering) {
-  it(`should match a text element server-side`, async () => {
-    const sourceCode = await response.text();
-    expect(sourceCode).toEqual(
-      expect.stringContaining('Open up App.js to start working on your app!')
-    );
-  });
-} else {
-  if (isProduction) {
-    it(`should register expo service worker`, async () => {
-      const swID = 'div[data-testid="has-sw-text"]';
+if (isProduction) {
+  it(`should register expo service worker`, async () => {
+    const swID = 'div[data-testid="has-sw-text"]';
 
-      await expect(page).toMatchElement(swID, {
-        text: 'Has SW installed',
-        timeout: 2000,
-      });
-    }, 2500);
-  }
+    await expect(page).toMatchElement(swID, {
+      text: 'Has SW installed',
+      timeout: 2000,
+    });
+  }, 2500);
 }
 
 describe('Optional polyfills', () => {
-  if (!config.hasServerSideRendering) {
-    it(`should have resize-observer polyfill added`, async () => {
-      const ciID = 'div[data-testid="has-resize-observer"]';
-      if (isInCI) {
-        await expect(page).toMatchElement(ciID, {
-          text: 'Has ResizeObserver polyfill',
-        });
-      } else {
-        await expect(page).not.toMatchElement(ciID);
-      }
+  it(`should have resize-observer polyfill added`, async () => {
+    const elementId = 'div[data-testid="has-resize-observer"]';
+    await expect(page).toMatchElement(elementId, {
+      text: 'Has ResizeObserver polyfill',
     });
-  }
+  });
 });
 
 describe('DefinePlugin', () => {
@@ -70,19 +55,4 @@ describe('DefinePlugin', () => {
       text: `A Neat Expo App`,
     });
   });
-
-  if (config.hasServerSideRendering) {
-    it(`should be aware of process.env.CI server-side`, async () => {
-      const sourceCode = await response.text();
-      if (isInCI) {
-        expect(sourceCode).toEqual(expect.stringContaining('Has CI env'));
-      } else {
-        expect(sourceCode).not.toEqual(expect.stringContaining('Has CI env'));
-      }
-    });
-    it(`should have manifest from expo-constants server-side`, async () => {
-      const sourceCode = await response.text();
-      expect(sourceCode).toEqual(expect.stringContaining(`A Neat Expo App`));
-    });
-  }
 });
--- packages/webpack-config/e2e/nextjs/server.js
+++
@@ -1,5 +0,0 @@
-const { startServerAsync } = require('../../../next-adapter/build');
-
-startServerAsync(/* port: 3000 */).then(({ app, handle, server }) => {
-  // started
-});
