e466a2d087a2b5c451d0d49f6cccac7174fdef00
Evan Bacon <baconbrix@gmail.com>
2020-06-02 14:37:03 -0700

    Updated syntax for send command

--- packages/expo-cli/src/askUser.ts
+++ packages/expo-cli/src/askUser.ts
@@ -2,21 +2,20 @@ import { UserSettings } from '@expo/xdl';
 import prompt from './prompt';
 
 async function askForSendToAsync(): Promise<string> {
-  var sendToFromSettings = await UserSettings.getAsync('sendTo', null);
+  const cachedValue = await UserSettings.getAsync('sendTo', null);
   console.log("Enter an email address and we'll send a link to your phone.");
-  var answers = await prompt(
+  const answers = await prompt(
     [
       {
         type: 'input',
         name: 'sendTo',
-        message:
-          'Your email address' + (sendToFromSettings ? ' (space to not send anything)' : '') + ':',
-        default: sendToFromSettings || undefined,
+        message: `Your email address ${cachedValue ? ' (space to not send anything)' : ''}:`,
+        default: cachedValue ?? undefined,
       },
     ],
     { nonInteractiveHelp: 'Please specify email address with --send-to.' }
   );
-  let recipient = answers.sendTo.trim();
+  const recipient = answers.sendTo.trim();
   await UserSettings.mergeAsync({ sendTo: recipient });
   return recipient;
 }
@@ -26,7 +25,7 @@ export default {
 };
 
 if (require.main === module) {
-  askForSendToAsync().then(function (sendTo) {
+  askForSendToAsync().then(function(sendTo) {
     console.log('Your email address is', sendTo);
   });
 }
