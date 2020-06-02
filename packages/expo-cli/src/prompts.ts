80017d7e2b5a55fd63ca77fce9797c7dca3bcf36
Cedric van Putten <me@bycedric.com>
2020-06-02 13:07:36 +0200

    refactor(expo-cli): add prompts implementation to replace inquirer

---
+++ packages/expo-cli/src/prompts.ts
@@ -0,0 +1,34 @@
+import program from 'commander';
+import prompts, { Choice, PromptObject, PromptType } from 'prompts';
+
+import CommandError from './CommandError';
+
+export { PromptType, PromptObject };
+
+type CliPrompts = PromptObject | PromptObject[];
+
+export default function prompt(
+  cliPrompts: CliPrompts,
+  { nonInteractiveHelp }: { nonInteractiveHelp?: string } = {}
+) {
+  const nPrompts = Array.isArray(cliPrompts) ? cliPrompts.length : 1;
+  if (program.nonInteractive && nPrompts !== 0) {
+    let message = `Input is required, but Expo CLI is in non-interactive mode.\n`;
+    if (nonInteractiveHelp) {
+      message += nonInteractiveHelp;
+    } else {
+      const question: any = Array.isArray(cliPrompts) ? cliPrompts[0] : cliPrompts;
+      message += `Required input:\n${(question.message || '').trim().replace(/^/gm, '> ')}`;
+    }
+    throw new CommandError('NON_INTERACTIVE', message);
+  }
+  return prompts(cliPrompts, {
+    onCancel() {
+      throw new CommandError('ABORTED');
+    },
+  });
+}
+
+// todo: replace this workaround, its still selectable by the cursor
+// see: https://github.com/terkelg/prompts/issues/254
+prompt.separator = (title: string): Choice => ({ title, disable: true, value: undefined });
