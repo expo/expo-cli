6efc52ae3604f12268c997d8f3ede4e4ad956a1c
Evan Bacon <baconbrix@gmail.com>
2020-06-02 23:33:23 -0700

    [WIP]

---
+++ packages/expo-cli/src/commands/esbuild/service.ts
@@ -0,0 +1,44 @@
+import { build } from 'esbuild';
+import fs from 'fs-extra';
+import path from 'path';
+
+// import { resolveModule } from '@expo/config';
+// import { getBareExtensions } from '@expo/config/paths';
+
+export async function buildAsync(
+  projectRoot: string,
+  mainFile: string,
+  outputPath: string
+): Promise<void> {
+  await fs.ensureDir(path.dirname(outputPath));
+  console.log('Building: ', mainFile);
+  console.log('output: ', outputPath);
+  try {
+    const { stderr, warnings } = await build({
+      entryPoints: [
+        // resolveModule('react-native/build/polyfills/console.js', projectRoot, {}),
+        // resolveModule('react-native/build/polyfills/error-guard.js', projectRoot, {}),
+        // resolveModule('react-native/build/polyfills/Object.es7.js', projectRoot, {}),
+        mainFile,
+      ],
+      // resolveExtensions: getBareExtensions(['ios', 'native'], { isReact: true, isTS: true, isModern: false }).map(
+      //   value => `.${value}`
+      // ),
+      minify: true,
+      bundle: true,
+      sourcemap: true,
+      target: 'esnext',
+      outfile: outputPath,
+      loader: { '.js': 'jsx' },
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        __DEV__: 'false',
+      },
+    });
+    const output = fs.readFileSync(outputPath, 'utf8');
+    await fs.writeFile(outputPath, 'var global = global || this;\n' + output);
+    console.log('success', { output, stderr, warnings });
+  } catch ({ stderr, errors, warnings }) {
+    console.error('failure', { stderr, errors, warnings });
+  }
+}
--- packages/expo-cli/src/commands/ios.ts
+++ packages/expo-cli/src/commands/ios.ts
@@ -2,24 +2,27 @@ import boxen from 'boxen';
 import chalk from 'chalk';
 import { Command } from 'commander';
 
-export default function (program: Command) {
+import { Exp } from '@expo/xdl';
+import { readFile } from 'fs-extra';
+import path from 'path';
+import { buildAsync } from './esbuild/service';
+
+export default function(program: Command) {
   program
     .command('ios [project-dir]')
     .description(
       chalk.yellow`Deprecated: Opens your app in Expo in an iOS simulator on your computer`
     )
     .allowOffline()
-    .asyncActionProjectDir(() => {
-      // Deprecate after July 24, 2020 (3 months)
-      console.log(
-        boxen(
-          chalk.yellow(
-            `${chalk.bold(
-              `expo ios`
-            )} is deprecated. You can open your project by:\n- Pressing ${chalk.bold`i`} in the ${chalk.bold`expo start`} terminal UI\n- Or by running ${chalk.bold`expo start --ios`}`
-          ),
-          { borderColor: 'yellow', padding: 1 }
-        )
+    .asyncActionProjectDir(async (projectRoot: string) => {
+      const filename = Exp.determineEntryPoint(projectRoot, 'ios');
+      // const contents = await readFile(filename, 'utf8');
+      // console.log(filename, contents);
+      await buildAsync(
+        projectRoot,
+        path.resolve(filename),
+        path.join(projectRoot, 'public/index.js')
       );
+      // console.log(data);
     });
 }
