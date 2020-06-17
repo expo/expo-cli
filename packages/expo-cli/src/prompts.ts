490595dc90bf3af69b4c5e49cdf8061e7e9d3b6b
Cedric van Putten <me@bycedric.com>
2020-06-17 14:45:54 +0200

    [cli] Fix type errors in new prompts implementation

--- packages/expo-cli/src/prompts.ts
+++ packages/expo-cli/src/prompts.ts
@@ -1,28 +1,31 @@
 import program from 'commander';
-import prompts, { Choice, PromptObject, PromptType } from 'prompts';
+import prompts, { Choice, PromptType, PromptObject as Question } from 'prompts';
 
 import CommandError from './CommandError';
 
-export { PromptType, PromptObject as Question };
-
-type CliPrompts = PromptObject | PromptObject[];
+export { PromptType, Question };
 
 export default function prompt(
   questions: Question | Question[],
   { nonInteractiveHelp }: { nonInteractiveHelp?: string } = {}
 ) {
   questions = Array.isArray(questions) ? questions : [questions];
-  if (program.nonInteractive && nPrompts !== 0) {
+  if (program.nonInteractive && questions.length !== 0) {
     let message = `Input is required, but Expo CLI is in non-interactive mode.\n`;
     if (nonInteractiveHelp) {
       message += nonInteractiveHelp;
     } else {
       const question = questions[0];
-      message += `Required input:\n${(question.message || '').trim().replace(/^/gm, '> ')}`;
+      const questionMessage =
+        typeof question.message === 'function'
+          ? question.message(undefined, {}, question)
+          : question.message;
+
+      message += `Required input:\n${(questionMessage || '').trim().replace(/^/gm, '> ')}`;
     }
     throw new CommandError('NON_INTERACTIVE', message);
   }
-  return prompts(cliPrompts, {
+  return prompts(questions, {
     onCancel() {
       throw new CommandError('ABORTED');
     },
