d4c610708a51fbacddd3595bd141a270de574e1d
Dominik Sokal <dominik.sokal@swmansion.com>
2020-08-04 10:36:07 +0200

    [xdl] require.requireActual -> jest.requireActual

--- packages/xdl/src/__tests__/Project-publishAsync-test.ts
+++ packages/xdl/src/__tests__/Project-publishAsync-test.ts
@@ -14,7 +14,7 @@ jest.dontMock('resolve-from');
 
 jest.mock('../project/Doctor', () => ({
   async validateWithNetworkAsync() {
-    const Doctor = require.requireActual('../project/Doctor');
+    const Doctor = jest.requireActual('../project/Doctor');
     return Doctor.NO_ISSUES;
   },
 }));
