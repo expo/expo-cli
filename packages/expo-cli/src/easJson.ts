5f5d9e1f48a38f7e8a4ff07faf759d9fc9561c59
Evan Bacon <baconbrix@gmail.com>
2020-08-30 16:55:59 -0700

    Update easJson.ts

--- packages/expo-cli/src/easJson.ts
+++ packages/expo-cli/src/easJson.ts
@@ -71,64 +71,42 @@ const EasJsonSchema = Joi.object({
     android: Joi.object().pattern(
       Joi.string(),
       Joi.object({
-        workflow: Joi.string()
-          .valid('generic', 'managed')
-          .required(),
+        workflow: Joi.string().valid('generic', 'managed').required(),
       }).unknown(true) // profile is validated further only if build is for that platform
     ),
     ios: Joi.object().pattern(
       Joi.string(),
       Joi.object({
-        workflow: Joi.string()
-          .valid('generic', 'managed')
-          .required(),
+        workflow: Joi.string().valid('generic', 'managed').required(),
       }).unknown(true) // profile is validated further only if build is for that platform
     ),
   }),
 });
 
 const AndroidGenericSchema = Joi.object({
-  workflow: Joi.string()
-    .valid('generic')
-    .required(),
-  credentialsSource: Joi.string()
-    .valid('local', 'remote', 'auto')
-    .default('auto'),
+  workflow: Joi.string().valid('generic').required(),
+  credentialsSource: Joi.string().valid('local', 'remote', 'auto').default('auto'),
   gradleCommand: Joi.string(),
   artifactPath: Joi.string(),
   withoutCredentials: Joi.boolean(),
 });
 
 const AndroidManagedSchema = Joi.object({
-  workflow: Joi.string()
-    .valid('managed')
-    .required(),
-  credentialsSource: Joi.string()
-    .valid('local', 'remote', 'auto')
-    .default('auto'),
-  buildType: Joi.string()
-    .valid('apk', 'app-bundle')
-    .default('app-bundle'),
+  workflow: Joi.string().valid('managed').required(),
+  credentialsSource: Joi.string().valid('local', 'remote', 'auto').default('auto'),
+  buildType: Joi.string().valid('apk', 'app-bundle').default('app-bundle'),
 });
 
 const iOSGenericSchema = Joi.object({
-  workflow: Joi.string()
-    .valid('generic')
-    .required(),
-  credentialsSource: Joi.string()
-    .valid('local', 'remote', 'auto')
-    .default('auto'),
+  workflow: Joi.string().valid('generic').required(),
+  credentialsSource: Joi.string().valid('local', 'remote', 'auto').default('auto'),
   scheme: Joi.string(),
   artifactPath: Joi.string(),
 });
 
 const iOSManagedSchema = Joi.object({
-  workflow: Joi.string()
-    .valid('managed')
-    .required(),
-  credentialsSource: Joi.string()
-    .valid('local', 'remote', 'auto')
-    .default('auto'),
+  workflow: Joi.string().valid('managed').required(),
+  credentialsSource: Joi.string().valid('local', 'remote', 'auto').default('auto'),
   buildType: Joi.string().valid('archive', 'simulator'),
 });
 
