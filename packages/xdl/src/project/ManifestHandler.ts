145b97f9a5adfa6ec10b9c2f9315997719f92e6e
Evan Bacon <baconbrix@gmail.com>
2020-09-04 14:43:19 +0200

    Added basic test for manifest handler

--- packages/xdl/src/project/ManifestHandler.ts
+++ packages/xdl/src/project/ManifestHandler.ts
@@ -16,6 +16,17 @@ import UserSettings from '../UserSettings';
 import * as Doctor from './Doctor';
 import * as ProjectUtils from './ProjectUtils';
 
+interface HostInfo {
+  host: string;
+  server: 'xdl';
+  serverVersion: string;
+  serverDriver: string | null;
+  serverOS: NodeJS.Platform;
+  serverOSVersion: string;
+}
+
+type PackagerOptions = ProjectSettings.ProjectSettings;
+
 type CachedSignedManifest =
   | {
       manifestString: null;
@@ -54,6 +65,49 @@ function stripPort(host: string | undefined): string | undefined {
   return new URL('/', `http://${host}`).hostname;
 }
 
+async function getPackagerOptionsAsync(
+  projectRoot: string
+): Promise<[PackagerOptions, PackagerOptions]> {
+  // Get packager opts and then copy into bundleUrlPackagerOpts
+  const packagerOpts = await ProjectSettings.readAsync(projectRoot);
+  const bundleUrlPackagerOpts = JSON.parse(JSON.stringify(packagerOpts));
+  bundleUrlPackagerOpts.urlType = 'http';
+  if (bundleUrlPackagerOpts.hostType === 'redirect') {
+    bundleUrlPackagerOpts.hostType = 'tunnel';
+  }
+  return [packagerOpts, bundleUrlPackagerOpts];
+}
+
+async function getBundleUrlAsync({
+  projectRoot,
+  platform,
+  packagerOpts,
+  bundleUrlPackagerOpts,
+  mainModuleName,
+  hostname,
+}: {
+  platform: string;
+  hostname?: string;
+  mainModuleName: string;
+  projectRoot: string;
+  packagerOpts: PackagerOptions;
+  bundleUrlPackagerOpts: PackagerOptions;
+}): Promise<string> {
+  const queryParams = await UrlUtils.constructBundleQueryParamsAsync(projectRoot, packagerOpts);
+
+  const path = `/${encodeURI(mainModuleName)}.bundle?platform=${encodeURIComponent(
+    platform
+  )}&${queryParams}`;
+
+  return (
+    (await UrlUtils.constructBundleUrlAsync(projectRoot, bundleUrlPackagerOpts, hostname)) + path
+  );
+}
+
+function getPlatformFromRequest(headers: http.IncomingHttpHeaders): string {
+  return (headers['exponent-platform'] || 'ios').toString();
+}
+
 export function getManifestHandler(projectRoot: string) {
   return async (
     req: express.Request | http.IncomingMessage,
@@ -64,77 +118,23 @@ export function getManifestHandler(projectRoot: string) {
       // if there is a potential error in the package.json and don't want to slow
       // down the request
       Doctor.validateWithNetworkAsync(projectRoot);
-      // Get packager opts and then copy into bundleUrlPackagerOpts
-      const packagerOpts = await ProjectSettings.readAsync(projectRoot);
-      const { exp: manifest } = getConfig(projectRoot);
-      const bundleUrlPackagerOpts = JSON.parse(JSON.stringify(packagerOpts));
-      bundleUrlPackagerOpts.urlType = 'http';
-      if (bundleUrlPackagerOpts.hostType === 'redirect') {
-        bundleUrlPackagerOpts.hostType = 'tunnel';
-      }
-      manifest.xde = true; // deprecated
-      manifest.developer = {
-        tool: Config.developerTool,
+
+      const { manifestString, exp, hostInfo } = await getManifestResponseFromHeadersAsync({
         projectRoot,
-      };
-      manifest.packagerOpts = packagerOpts;
-      manifest.env = {};
-      for (const key of Object.keys(process.env)) {
-        if (shouldExposeEnvironmentVariableInManifest(key)) {
-          manifest.env[key] = process.env[key];
-        }
-      }
-      const platform = (req.headers['exponent-platform'] || 'ios').toString();
-      const entryPoint = Exp.determineEntryPoint(projectRoot, platform);
-      const mainModuleName = UrlUtils.guessMainModulePath(entryPoint);
-      const queryParams = await UrlUtils.constructBundleQueryParamsAsync(projectRoot, packagerOpts);
-      const path = `/${encodeURI(mainModuleName)}.bundle?platform=${encodeURIComponent(
-        platform
-      )}&${queryParams}`;
-      const hostname = stripPort(req.headers.host);
-      manifest.bundleUrl =
-        (await UrlUtils.constructBundleUrlAsync(projectRoot, bundleUrlPackagerOpts, hostname)) +
-        path;
-      manifest.debuggerHost = await UrlUtils.constructDebuggerHostAsync(projectRoot, hostname);
-      manifest.mainModuleName = mainModuleName;
-      manifest.logUrl = await UrlUtils.constructLogUrlAsync(projectRoot, hostname);
-      manifest.hostUri = await UrlUtils.constructHostUriAsync(projectRoot, hostname);
-      await resolveManifestAssets({
-        projectRoot,
-        manifest: manifest as PublicConfig,
-        async resolver(path) {
-          return manifest.bundleUrl.match(/^https?:\/\/.*?\//)[0] + 'assets/' + path;
-        },
-      }); // the server normally inserts this but if we're offline we'll do it here
-      await resolveGoogleServicesFile(projectRoot, manifest);
-      const hostUUID = await UserSettings.anonymousIdentifier();
-      const currentSession = await UserManager.getSessionAsync();
-      if (!currentSession || Config.offline) {
-        manifest.id = `@${ANONYMOUS_USERNAME}/${manifest.slug}-${hostUUID}`;
-      }
-      let manifestString;
-      if (req.headers['exponent-accept-signature']) {
-        manifestString =
-          !currentSession || Config.offline
-            ? getUnsignedManifestString(manifest)
-            : await getSignedManifestStringAsync(manifest, currentSession);
-      } else {
-        manifestString = JSON.stringify(manifest);
-      }
-      const hostInfo = {
-        host: hostUUID,
-        server: 'xdl',
-        serverVersion: require('@expo/xdl/package.json').version,
-        serverDriver: Config.developerTool,
-        serverOS: os.platform(),
-        serverOSVersion: os.release(),
-      };
+        headers: req.headers,
+      });
+      const sdkVersion = exp.sdkVersion ?? null;
+
+      // Send the response
       res.setHeader('Exponent-Server', JSON.stringify(hostInfo));
+      // End the request
       res.end(manifestString);
+
+      // Log analytics
       Analytics.logEvent('Serve Manifest', {
         projectRoot,
         developerTool: Config.developerTool,
-        sdkVersion: manifest.sdkVersion ?? null,
+        sdkVersion,
       });
     } catch (e) {
       ProjectUtils.logError(projectRoot, 'expo', e.stack);
@@ -146,6 +146,128 @@ export function getManifestHandler(projectRoot: string) {
         })
       );
     }
+  };
+}
+
+async function getManifestResponseFromHeadersAsync({
+  projectRoot,
+  headers,
+}: {
+  projectRoot: string;
+  headers: http.IncomingHttpHeaders;
+}): Promise<{ exp: ExpoConfig; manifestString: string; hostInfo: HostInfo }> {
+  // Read from headers
+  const platform = getPlatformFromRequest(headers);
+  const acceptSignature = headers['exponent-accept-signature'];
+  return getManifestResponseAsync({ projectRoot, host: headers.host, platform, acceptSignature });
+}
+
+export async function getManifestResponseAsync({
+  projectRoot,
+  host,
+  platform,
+  acceptSignature,
+}: {
+  projectRoot: string;
+  platform: string;
+  host?: string;
+  acceptSignature?: string | string[];
+}): Promise<{ exp: ExpoConfig; manifestString: string; hostInfo: HostInfo }> {
+  // Read from headers
+  const hostname = stripPort(host);
+
+  // Get project entry point and initial module
+  const entryPoint = Exp.determineEntryPoint(projectRoot, platform);
+  const mainModuleName = UrlUtils.guessMainModulePath(entryPoint);
+
+  // Gather packager and host info
+  const hostInfo = await createHostInfoAsync();
+  const [packagerOpts, bundleUrlPackagerOpts] = await getPackagerOptionsAsync(projectRoot);
+
+  // Read the config
+  const { exp: manifest } = getConfig(projectRoot);
+
+  // Mutate the manifest
+  manifest.xde = true; // deprecated
+  manifest.developer = {
+    tool: Config.developerTool,
+    projectRoot,
+  };
+  manifest.packagerOpts = packagerOpts;
+  manifest.mainModuleName = mainModuleName;
+  manifest.env = getManifestEnvironment();
+  // Add URLs to the manifest
+  manifest.bundleUrl = await getBundleUrlAsync({
+    projectRoot,
+    platform,
+    packagerOpts,
+    bundleUrlPackagerOpts,
+    mainModuleName,
+    hostname,
+  });
+  manifest.debuggerHost = await UrlUtils.constructDebuggerHostAsync(projectRoot, hostname);
+  manifest.logUrl = await UrlUtils.constructLogUrlAsync(projectRoot, hostname);
+  manifest.hostUri = await UrlUtils.constructHostUriAsync(projectRoot, hostname);
+
+  // Resolve all assets and set them on the manifest as URLs
+  await resolveManifestAssets({
+    projectRoot,
+    manifest: manifest as PublicConfig,
+    async resolver(path) {
+      return manifest.bundleUrl.match(/^https?:\/\/.*?\//)[0] + 'assets/' + path;
+    },
+  });
+
+  // The server normally inserts this but if we're offline we'll do it here
+  await resolveGoogleServicesFile(projectRoot, manifest);
+
+  // Create the final string
+  const manifestString = await getManifestStringAsync(manifest, hostInfo.host, acceptSignature);
+
+  return {
+    manifestString,
+    exp: manifest,
+    hostInfo,
+  };
+}
+
+function getManifestEnvironment(): Record<string, any> {
+  return Object.keys(process.env).reduce<Record<string, any>>((prev, key) => {
+    if (shouldExposeEnvironmentVariableInManifest(key)) {
+      prev[key] = process.env[key];
+    }
+    return prev;
+  }, {});
+}
+
+async function getManifestStringAsync(
+  manifest: ExpoConfig,
+  hostUUID: string,
+  acceptSignature?: string | string[]
+): Promise<string> {
+  const currentSession = await UserManager.getSessionAsync();
+  if (!currentSession || Config.offline) {
+    manifest.id = `@${ANONYMOUS_USERNAME}/${manifest.slug}-${hostUUID}`;
+  }
+  if (acceptSignature) {
+    return !currentSession || Config.offline
+      ? getUnsignedManifestString(manifest)
+      : await getSignedManifestStringAsync(manifest, currentSession);
+  } else {
+    return JSON.stringify(manifest);
+  }
+}
+
+async function createHostInfoAsync(): Promise<HostInfo> {
+  const host = await UserSettings.anonymousIdentifier();
+
+  return {
+    host,
+    server: 'xdl',
+    serverVersion: require('@expo/xdl/package.json').version,
+    serverDriver: Config.developerTool,
+    serverOS: os.platform(),
+    serverOSVersion: os.release(),
   };
 }
 
--- packages/xdl/src/project/__tests__/ManifestHandler-test.ts
+++ packages/xdl/src/project/__tests__/ManifestHandler-test.ts
@@ -1,15 +1,33 @@
 import { ExpoConfig } from '@expo/config';
 import axios from 'axios';
+import fs from 'fs-extra';
+import { vol } from 'memfs';
+import os from 'os';
+import path from 'path';
+import uuid from 'uuid';
 
-import { getSignedManifestStringAsync, getUnsignedManifestString } from '../ManifestHandler';
+import {
+  getManifestResponseAsync,
+  getSignedManifestStringAsync,
+  getUnsignedManifestString,
+} from '../ManifestHandler';
 
+jest.mock('fs');
 jest.mock('axios');
 
-jest.mock('../../User', () => ({
-  ensureLoggedInAsync: () => ({
-    sessionSecret: 'SECRET',
-  }),
-}));
+jest.mock('../../User', () => {
+  // const user = jest.requireActual('../../User');
+  return {
+    // ...user,
+    ANONYMOUS_USERNAME: 'anonymous',
+    getSessionAsync() {
+      return null;
+    },
+    ensureLoggedInAsync: () => ({
+      sessionSecret: 'SECRET',
+    }),
+  };
+});
 
 const mockManifest: ExpoConfig = {
   name: 'Hello',
@@ -70,5 +88,93 @@ describe('getSignedManifestStringAsync', () => {
 describe('getUnsignedManifestString', () => {
   it('returns a stringified manifest with the same shape a server-signed manifest', () => {
     expect(getUnsignedManifestString(mockManifest)).toMatchSnapshot();
+  });
+});
+
+describe('getManifestResponseAsync', () => {
+  // for some reason, tempy fails with memfs in XDL
+  const expoDir = path.join(os.tmpdir(), `.expo-${uuid.v4()}`);
+
+  beforeAll(() => {
+    process.env.__UNSAFE_EXPO_HOME_DIRECTORY = expoDir;
+    fs.mkdirpSync(expoDir);
+  });
+
+  afterAll(() => {
+    process.env.__UNSAFE_EXPO_HOME_DIRECTORY = '';
+    fs.removeSync(expoDir);
+  });
+
+  beforeEach(() => {
+    const packageJson = JSON.stringify(
+      {
+        name: 'testing123',
+        version: '0.1.0',
+        main: 'index.js',
+      },
+      null,
+      2
+    );
+
+    const appJson = JSON.stringify(
+      {
+        expo: {
+          name: 'testing 123',
+          version: '0.1.0',
+          sdkVersion: '38.0.0',
+          slug: 'testing-123',
+          extras: { myExtra: '123' },
+        },
+      },
+      null,
+      2
+    );
+
+    vol.fromJSON({
+      '/alpha/package.json': packageJson,
+      '/alpha/app.json': appJson,
+      '/alpha/index.js': 'console.log("lol")',
+    });
+  });
+
+  afterEach(() => {
+    vol.reset();
+    delete process.env.EXPO_SOME_TEST_VALUE;
+    delete process.env.REACT_NATIVE_TEST_VALUE;
+    delete process.env.EXPO_APPLE_PASSWORD;
+  });
+
+  it(`returns a minimal expected manifest`, async () => {
+    process.env.EXPO_SOME_TEST_VALUE = 'true';
+    process.env.REACT_NATIVE_TEST_VALUE = 'true';
+    process.env.EXPO_APPLE_PASSWORD = 'my-password';
+
+    const res = await getManifestResponseAsync({
+      projectRoot: '/alpha',
+      host: '127.0.0.1:19000',
+      platform: 'ios',
+      acceptSignature: 'true',
+    });
+
+    // Values starting with EXPO_ or REACT_NATIVE_ get added to the env and exposed to expo-constants
+    expect(res.exp.env.EXPO_SOME_TEST_VALUE).toBe('true');
+    expect(res.exp.env.REACT_NATIVE_TEST_VALUE).toBe('true');
+    // This value is blacklisted
+    expect(res.exp.env.EXPO_APPLE_PASSWORD).not.toBeDefined();
+    // Users should use app.config.js + extras now so test that it always works
+    expect(res.exp.extras.myExtra).toBe('123');
+
+    // Ensure the bundle URL is built correctly
+    expect(res.exp.bundleUrl).toBe(
+      'http://127.0.0.1:80/index.bundle?platform=ios&dev=true&minify=false&hot=false'
+    );
+    expect(res.exp.debuggerHost).toBe('127.0.0.1:80');
+    expect(res.exp.logUrl).toBe('http://127.0.0.1:80/logs');
+    expect(res.exp.hostUri).toBe('127.0.0.1:80');
+
+    expect(res.exp.mainModuleName).toBe('index');
+    expect(res.exp.packagerOpts).toBeDefined();
+    // Required for various tools
+    expect(res.exp.developer.projectRoot).toBe('/alpha');
   });
 });
