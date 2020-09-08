3a735548312ad604ea2fe44a7adc42a0c9e57d1c
Evan Bacon <baconbrix@gmail.com>
2020-09-08 20:39:48 +0200

    Update packages/expo-cli/src/commands/utils/CreateApp.ts
    Co-authored-by: Brent Vatne <brentvatne@gmail.com>

--- packages/expo-cli/src/commands/utils/CreateApp.ts
+++ packages/expo-cli/src/commands/utils/CreateApp.ts
@@ -14,7 +14,7 @@ export function validateName(name?: string): string | true {
     return 'The project name can not be empty.';
   }
   if (!/^[a-z0-9@.\-_]+$/i.test(name)) {
-    return 'The project name can only contain URL-friendly characters.';
+    return 'The project name can only contain URL-friendly characters (alphanumeric and @ . -  _)';
   }
   return true;
 }
