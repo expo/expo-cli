4fd692a772a0790e884e101ddf67f918647d4806
Evan Bacon <baconbrix@gmail.com>
2020-09-13 20:28:42 +0200

    disable test

--- packages/config/jest.config.js
+++ packages/config/jest.config.js
@@ -2,8 +2,6 @@ const path = require('path');
 
 module.exports = {
   preset: '../../jest/unit-test-config',
-  transformIgnorePatterns: ['node_modules/@babel/core'],
-  verbose: true,
   rootDir: path.resolve(__dirname),
   displayName: require('./package').name,
   roots: ['__mocks__', 'src'],
