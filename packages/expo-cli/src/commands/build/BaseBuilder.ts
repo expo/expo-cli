2124e84f519721573f9908c1a42464e66a40c4da
Evan Bacon <baconbrix@gmail.com>
2020-09-16 13:29:05 +0200

    Update BaseBuilder.ts

--- packages/expo-cli/src/commands/build/BaseBuilder.ts
+++ packages/expo-cli/src/commands/build/BaseBuilder.ts
@@ -382,8 +382,10 @@ ${job.id}
         : completedJob.artifacts.url;
       log.addNewLineIfNone();
       log(`${chalk.green('Successfully built standalone app:')} ${chalk.underline(artifactUrl)}`);
-      log.newLine();
-      log(`You can publish to the App Store with ${chalk.bold('expo upload:ios')}`);
+      if (process.platform === 'darwin') {
+        log.newLine();
+        log(`You can publish to the App Store with ${chalk.bold('expo upload:ios')}`);
+      }
       log.newLine();
     } else {
       log('Alternatively, run `expo build:status` to monitor it from the command line.');
