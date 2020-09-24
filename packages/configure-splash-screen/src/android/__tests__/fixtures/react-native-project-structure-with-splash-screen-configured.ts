969c59384230bb9c3567aae78c70a6098df2d802
Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>
2020-09-24 15:56:46 +0200

    [configure-splash-screen]<feat>: Make Android configuration conform to the new native API

--- packages/configure-splash-screen/src/android/__tests__/fixtures/react-native-project-structure-with-splash-screen-configured.ts
+++ packages/configure-splash-screen/src/android/__tests__/fixtures/react-native-project-structure-with-splash-screen-configured.ts
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
 
+import com.facebook.react.ReactRootView;
+
 import expo.modules.splashscreen.SplashScreen;
 import expo.modules.splashscreen.SplashScreenImageResizeMode;
 
@@ -14,7 +16,7 @@ public class MainActivity extends ReactActivity {
     super.onCreate(savedInstanceState);
     // SplashScreen.show(...) has to be called after super.onCreate(...)
     // Below line is handled by '@expo/configure-splash-screen' command and it's discouraged to modify it manually
-    SplashScreen.show(this, SplashScreenImageResizeMode.CONTAIN, false);
+    SplashScreen.show(this, SplashScreenImageResizeMode.CONTAIN, ReactRootView.class, false);
   }
 
   /**
