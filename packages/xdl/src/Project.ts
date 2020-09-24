261e20c33a649a9f9629df29d7a37c8d808c0278
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-09-23 23:24:56 -0400

    feat: use `process.env.METRO_NODE_OPTIONS` when starting Metro (#2401)
    Instead of using NODE_OPTIONS from the expo-cli process, the Metro process overrides it with METRO_NODE_OPTIONS.
    
    This has two effects: (1) the expo-cli process can be debugged with --inspect-brk without causing an "address already in use" error in the Metro process,
    and (2) the Metro process can be debugged with --inspect-brk.

--- packages/xdl/src/Project.ts
+++ packages/xdl/src/Project.ts
@@ -1531,9 +1531,12 @@ export async function startReactNativeServerAsync({
 
   if (options.reset) {
     cliOpts.push('--reset-cache');
-  } // Get custom CLI path from project package.json, but fall back to node_module path
+  }
+
+  // Get custom CLI path from project package.json, but fall back to node_module path
   const defaultCliPath = resolveModule('react-native/local-cli/cli.js', projectRoot, exp);
   const cliPath = exp.rnCliPath || defaultCliPath;
+
   let nodePath;
   // When using a custom path for the RN CLI, we want it to use the project
   // root to look up config files and Node modules
@@ -1542,6 +1545,7 @@ export async function startReactNativeServerAsync({
   } else {
     nodePath = null;
   }
+
   // Run the copy of Node that's embedded in Electron by setting the
   // ELECTRON_RUN_AS_NODE environment variable
   // Note: the CLI script sets up graceful-fs and sets ulimit to 4096 in the
@@ -1551,6 +1555,7 @@ export async function startReactNativeServerAsync({
     cwd: projectRoot,
     env: {
       ...process.env,
+      NODE_OPTIONS: process.env.METRO_NODE_OPTIONS,
       REACT_NATIVE_APP_ROOT: projectRoot,
       ELECTRON_RUN_AS_NODE: '1',
       ...nodePathEnv,
