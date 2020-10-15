62bbe490e9f0de056cd8f03cca5a7cbe94201be7
Evan Bacon <baconbrix@gmail.com>
2020-10-15 16:23:40 +0200

    Update configureIOSProjectAsync.ts

--- packages/expo-cli/src/commands/apply/configureIOSProjectAsync.ts
+++ packages/expo-cli/src/commands/apply/configureIOSProjectAsync.ts
@@ -1,4 +1,4 @@
-import { ExportedConfig, getConfig, IOSConfig } from '@expo/config/build';
+import { ExportedConfig, getConfig } from '@expo/config/build';
 import { withExpoIOSPlugins } from '@expo/config/build/plugins/expo-plugins';
 import { compilePluginsAsync } from '@expo/config/build/plugins/plugin-compiler';
 import { UserManager } from '@expo/xdl';
@@ -8,10 +8,8 @@ import { getOrPromptForBundleIdentifier } from '../eject/ConfigValidation';
 export default async function configureIOSProjectAsync(projectRoot: string) {
   // Check bundle ID before reading the config because it may mutate the config if the user is prompted to define it.
   const bundleIdentifier = await getOrPromptForBundleIdentifier(projectRoot);
-  IOSConfig.BundleIdenitifer.setBundleIdentifierForPbxproj(projectRoot, bundleIdentifier);
   const expoUsername = await UserManager.getCurrentUsernameAsync();
 
-  //// New System
   let config = getExportedConfig(projectRoot);
 
   // Add all built-in plugins
