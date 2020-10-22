ba3dd09ce638db15df28a2137c390a3587719613
Brent Vatne <brentvatne@gmail.com>
2020-10-22 16:17:57 -0700

    [cli] Add TODO to remove legacy eject and --force flag with SDK 40

--- packages/expo-cli/src/commands/eject.ts
+++ packages/expo-cli/src/commands/eject.ts
@@ -35,6 +35,7 @@ async function action(
   }
 
   // Set EXPO_VIEW_DIR to universe/exponent to pull expo view code locally instead of from S3 for ExpoKit
+  // TODO: remove LegacyEject when SDK 36 is no longer supported: after SDK 40 is released.
   if (Versions.lteSdkVersion(exp, '36.0.0')) {
     if (options.force || (await userWantsToEjectWithoutUpgradingAsync())) {
       await LegacyEject.ejectAsync(projectDir, options as LegacyEject.EjectAsyncOptions);
@@ -55,7 +56,7 @@ export default function (program: Command) {
       'Create Xcode and Android Studio projects for your app. Use this if you need to add custom native functionality.'
     )
     .helpGroup('eject')
-    .option('--force', 'Skip legacy eject warnings.')
+    .option('--force', 'Skip legacy eject warnings.') // TODO: remove the force flag when SDK 36 is no longer supported: after SDK 40 is released.
     .option('--no-install', 'Skip installing npm packages and CocoaPods.')
     .option('--npm', 'Use npm to install dependencies. (default when Yarn is not installed)')
     .asyncActionProjectDir(action);
