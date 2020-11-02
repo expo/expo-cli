d734733b0a59cd3d0a75e97e5b3d34a669b644d1
Evan Bacon <baconbrix@gmail.com>
2020-11-02 13:29:30 +0100

    Updated android branch support (#2814)

--- packages/config/src/android/Manifest.ts
+++ packages/config/src/android/Manifest.ts
@@ -198,16 +198,22 @@ export function addMetaDataItemToMainApplication(
 }
 
 export function removeMetaDataItemFromMainApplication(mainApplication: any, itemName: string) {
+  const index = findMetaDataItem(mainApplication, itemName);
+  if (mainApplication?.['meta-data'] && index > -1) {
+    mainApplication['meta-data'].splice(index, 1);
+  }
+  return mainApplication;
+}
+
+export function findMetaDataItem(mainApplication: any, itemName: string): number {
   if (mainApplication.hasOwnProperty('meta-data')) {
     const index = mainApplication['meta-data'].findIndex(
       (e: any) => e['$']['android:name'] === itemName
     );
 
-    if (index > -1) {
-      mainApplication['meta-data'].splice(index, 1);
-    }
+    return index;
   }
-  return mainApplication;
+  return -1;
 }
 
 export function getMainApplicationMetaDataValue(
