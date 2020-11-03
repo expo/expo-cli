b8e6d6b62af406d6a011a23018014f072fb54a15
Evan Bacon <baconbrix@gmail.com>
2020-11-03 22:29:50 +0100

    convert to TS

--- packages/config/src/android/UserInterfaceStyle.ts
+++ packages/config/src/android/UserInterfaceStyle.ts
@@ -16,44 +16,46 @@ public class MainActivity extends ReactActivity {
     }
 `;
 
-export function getUserInterfaceStyle(config: ExpoConfig): string | null {
+export function getUserInterfaceStyle(
+  config: Pick<ExpoConfig, 'android' | 'userInterfaceStyle'>
+): string | null {
   return config.android?.userInterfaceStyle ?? config.userInterfaceStyle ?? null;
 }
 
 export async function setUiModeAndroidManifest(
-  config: ExpoConfig,
-  manifestDocument: AndroidManifest
+  config: Pick<ExpoConfig, 'android' | 'userInterfaceStyle'>,
+  manifest: AndroidManifest
 ) {
   const userInterfaceStyle = getUserInterfaceStyle(config);
   if (!userInterfaceStyle) {
-    return manifestDocument;
+    return manifest;
   }
 
-  let mainActivity = getMainActivity(manifestDocument);
+  let mainActivity = getMainActivity(manifest);
   if (!mainActivity) {
     mainActivity = { $: { 'android:name': '.MainActivity' } };
   }
   mainActivity.$[CONFIG_CHANGES_ATTRIBUTE] =
     'keyboard|keyboardHidden|orientation|screenSize|uiMode';
 
-  return manifestDocument;
+  return manifest;
 }
 
 export function addOnConfigurationChangedMainActivity(
-  config: ExpoConfig,
-  MainActivity: string
+  config: Pick<ExpoConfig, 'android' | 'userInterfaceStyle'>,
+  mainActivity: string
 ): string {
   const userInterfaceStyle = getUserInterfaceStyle(config);
   if (!userInterfaceStyle) {
-    return MainActivity;
+    return mainActivity;
   }
 
   // Cruzan: this is not ideal, but I'm not sure of a better way to handle writing to MainActivity.java
-  if (MainActivity.match(`onConfigurationChanged`)?.length) {
-    return MainActivity;
+  if (mainActivity.match(`onConfigurationChanged`)?.length) {
+    return mainActivity;
   }
 
-  const MainActivityWithImports = addJavaImports(MainActivity, [
+  const MainActivityWithImports = addJavaImports(mainActivity, [
     'android.content.Intent',
     'android.content.res.Configuration',
   ]);
