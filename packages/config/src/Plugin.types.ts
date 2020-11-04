b1556d7f51443aa0001a49da331709bdf8a5731c
Evan Bacon <baconbrix@gmail.com>
2020-11-04 19:16:35 +0100

    Update Plugin.types.ts

--- packages/config/src/Plugin.types.ts
+++ packages/config/src/Plugin.types.ts
@@ -54,6 +54,7 @@ export interface ExportedConfigWithProps<Data = any> extends ExpoConfig {
   modRequest: ModProps<Data>;
 }
 
+// TODO: Change any to void
 export type ConfigPlugin<Props = any> = (config: ExpoConfig, props: Props) => ExpoConfig;
 
 export type Mod<Props = any> = (
