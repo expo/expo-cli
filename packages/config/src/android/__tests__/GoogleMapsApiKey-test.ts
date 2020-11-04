0f9ff1e787f4ff76cc51a9e2c8ba12dcb4c45c04
Evan Bacon <baconbrix@gmail.com>
2020-11-04 16:02:55 +0100

    Update GoogleMapsApiKey-test.ts

--- packages/config/src/android/__tests__/GoogleMapsApiKey-test.ts
+++ packages/config/src/android/__tests__/GoogleMapsApiKey-test.ts
@@ -1,41 +1,75 @@
 import { resolve } from 'path';
 
 import { getGoogleMapsApiKey, setGoogleMapsApiKey } from '../GoogleMapsApiKey';
-import { getMainApplicationOrThrow, readAndroidManifestAsync } from '../Manifest';
+import { AndroidManifest, getMainApplicationOrThrow, readAndroidManifestAsync } from '../Manifest';
 
 const fixturesPath = resolve(__dirname, 'fixtures');
 const sampleManifestPath = resolve(fixturesPath, 'react-native-AndroidManifest.xml');
 
-describe('Android google maps api key', () => {
-  it(`returns null if no android google maps api key is provided`, () => {
+describe(getGoogleMapsApiKey, () => {
+  it(`returns null if no android google maps API key is provided`, () => {
     expect(getGoogleMapsApiKey({ android: { config: { googleMaps: {} } } })).toBe(null);
   });
 
-  it(`returns apikey if android google maps api key is provided`, () => {
+  it(`returns API key if android google maps api key is provided`, () => {
     expect(
       getGoogleMapsApiKey({ android: { config: { googleMaps: { apiKey: 'MY-API-KEY' } } } })
     ).toBe('MY-API-KEY');
   });
+});
 
-  it('add google maps key to androidmanifest.xml', async () => {
+describe(setGoogleMapsApiKey, () => {
+  it('adds and removes google maps key', async () => {
+    function hasSingleEntry(androidManifestJson: AndroidManifest) {
+      const mainApplication = getMainApplicationOrThrow(androidManifestJson);
+
+      const apiKeyItem = mainApplication['meta-data'].filter(
+        e => e.$['android:name'] === 'com.google.android.geo.API_KEY'
+      );
+      expect(apiKeyItem).toHaveLength(1);
+      expect(apiKeyItem[0].$['android:value']).toMatch('MY-API-KEY');
+
+      const usesLibraryItem = mainApplication['uses-library'].filter(
+        e => e.$['android:name'] === 'org.apache.http.legacy'
+      );
+      expect(usesLibraryItem).toHaveLength(1);
+      expect(usesLibraryItem[0].$['android:required']).toBe(false);
+    }
+    function isRemoved(androidManifestJson: AndroidManifest) {
+      const mainApplication = getMainApplicationOrThrow(androidManifestJson);
+
+      const apiKeyItem = mainApplication['meta-data'].filter(
+        e => e.$['android:name'] === 'com.google.android.geo.API_KEY'
+      );
+      expect(apiKeyItem).toHaveLength(0);
+
+      const usesLibraryItem = mainApplication['uses-library'].filter(
+        e => e.$['android:name'] === 'org.apache.http.legacy'
+      );
+      expect(usesLibraryItem).toHaveLength(0);
+    }
+
     let androidManifestJson = await readAndroidManifestAsync(sampleManifestPath);
+
+    // Add the key once
     androidManifestJson = setGoogleMapsApiKey(
       { android: { config: { googleMaps: { apiKey: 'MY-API-KEY' } } } },
       androidManifestJson
     );
 
-    const mainApplication = getMainApplicationOrThrow(androidManifestJson);
+    hasSingleEntry(androidManifestJson);
 
-    const apiKeyItem = mainApplication['meta-data'].filter(
-      e => e.$['android:name'] === 'com.google.android.geo.API_KEY'
+    // Test that adding it twice doesn't cause duplicate entries
+    androidManifestJson = setGoogleMapsApiKey(
+      { android: { config: { googleMaps: { apiKey: 'MY-API-KEY' } } } },
+      androidManifestJson
     );
-    expect(apiKeyItem).toHaveLength(1);
-    expect(apiKeyItem[0].$['android:value']).toMatch('MY-API-KEY');
 
-    const usesLibraryItem = mainApplication['uses-library'].filter(
-      e => e.$['android:name'] === 'org.apache.http.legacy'
-    );
-    expect(usesLibraryItem).toHaveLength(1);
-    expect(usesLibraryItem[0].$['android:required']).toBe(false);
+    hasSingleEntry(androidManifestJson);
+
+    // Remove meta
+    androidManifestJson = setGoogleMapsApiKey({}, androidManifestJson);
+
+    isRemoved(androidManifestJson);
   });
 });
