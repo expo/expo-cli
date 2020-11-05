e7f2d829f283aea10fbb485f2ea2657a92922ff9
Brent Vatne <brentvatne@gmail.com>
2020-11-04 17:17:54 -0800

    Update packages/xdl/src/ProjectSettings.ts

--- packages/xdl/src/ProjectSettings.ts
+++ packages/xdl/src/ProjectSettings.ts
@@ -152,7 +152,8 @@ The "settings.json" file contains the server configuration that is used to serve
 > Should I commit the ".expo" folder?
 
 No, you should not share the ".expo" folder. It does not contain any information that is relevant for other developers working on the project, it is specific to your machine.
-Upon project creation, ".expo" folder is already added to your ".gitignore" file.
+
+Upon project creation, the ".expo" folder is already added to your ".gitignore" file.
 `
     );
   }
