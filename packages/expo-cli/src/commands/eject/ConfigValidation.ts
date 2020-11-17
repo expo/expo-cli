8aa3f12876376b973d34ec9962116b0dc09cde94
Evan Bacon <baconbrix@gmail.com>
2020-11-17 18:22:35 +0100

    Update ConfigValidation.ts

--- packages/expo-cli/src/commands/eject/ConfigValidation.ts
+++ packages/expo-cli/src/commands/eject/ConfigValidation.ts
@@ -2,6 +2,7 @@ import { ExpoConfig, getConfig, modifyConfigAsync } from '@expo/config';
 import { UserManager } from '@expo/xdl';
 import got from 'got';
 
+import CommandError, { SilentError } from '../../CommandError';
 import log from '../../log';
 import prompt, { confirmAsync } from '../../prompts';
 import { learnMore } from '../utils/TerminalLink';
@@ -97,14 +98,9 @@ export async function getOrPromptForBundleIdentifier(projectRoot: string): Promi
     if (validateBundleId(currentBundleId)) {
       return currentBundleId;
     }
-
-    log(
-      log.chalk.red(
-        `The ios.bundleIdentifier defined in your Expo config is not formatted properly. Only alphanumeric characters, '.', '-', and '_' are allowed, and each '.' must be followed by a letter.`
-      )
+    throw new CommandError(
+      `The ios.bundleIdentifier defined in your Expo config is not formatted properly. Only alphanumeric characters, '.', '-', and '_' are allowed, and each '.' must be followed by a letter.`
     );
-
-    process.exit(1);
   }
 
   // Recommend a bundle ID based on the username and project slug.
@@ -182,13 +178,9 @@ export async function getOrPromptForPackage(projectRoot: string): Promise<string
     if (validatePackage(currentPackage)) {
       return currentPackage;
     }
-    log(
-      log.chalk.red(
-        `Invalid format of Android package name. Only alphanumeric characters, '.' and '_' are allowed, and each '.' must be followed by a letter.`
-      )
+    throw new CommandError(
+      `Invalid format of Android package name. Only alphanumeric characters, '.' and '_' are allowed, and each '.' must be followed by a letter.`
     );
-
-    process.exit(1);
   }
 
   // Recommend a package name based on the username and project slug.
@@ -272,7 +264,7 @@ async function attemptModification(
   if (modification.type === 'success') {
     log.newLine();
   } else {
-    warnAboutConfigAndExit(modification.type, modification.message!, exactEdits);
+    warnAboutConfigAndThrow(modification.type, modification.message!, exactEdits);
   }
 }
 
@@ -284,7 +276,7 @@ function logNoConfig() {
   );
 }
 
-function warnAboutConfigAndExit(type: string, message: string, edits: Partial<ExpoConfig>) {
+function warnAboutConfigAndThrow(type: string, message: string, edits: Partial<ExpoConfig>) {
   log.addNewLineIfNone();
   if (type === 'warn') {
     // The project is using a dynamic config, give the user a helpful log and bail out.
@@ -294,7 +286,7 @@ function warnAboutConfigAndExit(type: string, message: string, edits: Partial<Ex
   }
 
   notifyAboutManualConfigEdits(edits);
-  process.exit(1);
+  throw new SilentError();
 }
 
 function notifyAboutManualConfigEdits(edits: Partial<ExpoConfig>) {
