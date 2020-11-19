5c3cff3b81d3c839aa1e730e4866d080a53a5af4
Evan Bacon <baconbrix@gmail.com>
2020-11-19 23:48:08 +0100

    Update diagnostics.ts

--- packages/expo-cli/src/commands/diagnostics.ts
+++ packages/expo-cli/src/commands/diagnostics.ts
@@ -6,8 +6,8 @@ import log from '../log';
 
 const packageJSON = require('../../package.json');
 
-async function action(projectRoot: string): Promise<void> {
-  const info = await envinfo.run(
+function getEnvironmentInfoAsync(): Promise<string> {
+  return envinfo.run(
     {
       System: ['OS', 'Shell'],
       Binaries: ['Node', 'Yarn', 'npm', 'Watchman'],
@@ -30,7 +30,10 @@ async function action(projectRoot: string): Promise<void> {
       title: `Expo CLI ${packageJSON.version} environment info`,
     }
   );
+}
 
+async function actionAsync(projectRoot: string): Promise<void> {
+  const info = await getEnvironmentInfoAsync();
   const workflow = getDefaultTarget(projectRoot ?? process.cwd());
   const lines = info.split('\n');
   lines.pop();
@@ -43,5 +46,5 @@ export default function (program: Command) {
     .command('diagnostics [path]')
     .description('Log environment info to the console')
     .helpGroup('info')
-    .asyncAction(action);
+    .asyncAction(actionAsync);
 }
