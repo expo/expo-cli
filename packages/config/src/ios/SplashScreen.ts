24171cd09437361ff87bd4d3eee1a5af1f775ed7
Evan Bacon <baconbrix@gmail.com>
2020-11-20 16:50:47 +0100

    Update SplashScreen.ts

--- packages/config/src/ios/SplashScreen.ts
+++ packages/config/src/ios/SplashScreen.ts
@@ -32,38 +32,6 @@ type ExpoConfigIosSplash = NonNullable<NonNullable<ExpoConfig['ios']>['splash']>
 const defaultResizeMode = 'contain';
 const defaultBackgroundColor = '#ffffff';
 const defaultUserInterfaceStyle = 'automatic';
-
-type StatusBarStyle = 'auto' | 'light' | 'dark'; // | 'inverted'
-
-interface StatusBarConfig {
-  /**
-   * Sets the color of the status bar text. Default value is "auto" which
-   * picks the appropriate value according to the active color scheme, eg:
-   * if your app is dark mode, the style will be "light".
-   */
-  style?: StatusBarStyle;
-
-  /**
-   * If the status bar is hidden.
-   */
-  hidden?: boolean;
-
-  /**
-   * The background color of the status bar.
-   *
-   * @platform android
-   */
-  backgroundColor?: string;
-
-  /**
-   * If the status bar is translucent. When translucent is set to true,
-   * the app will draw under the status bar. This is the default in
-   * projects created with Expo tools because it is consistent with iOS.
-   *
-   * @platform android
-   */
-  translucent?: boolean;
-}
 export interface IOSSplashConfig {
   image: string;
   // tabletImage: string | null;
