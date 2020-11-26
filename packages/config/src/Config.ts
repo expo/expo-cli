855df0a916b03a5b8eda243050fb579b137dba78
Evan Bacon <baconbrix@gmail.com>
2020-11-26 19:34:37 +0100

    delete unused mock

--- packages/config/src/Config.ts
+++ packages/config/src/Config.ts
@@ -20,7 +20,6 @@ import {
 } from './Config.types';
 import { ConfigError } from './Errors';
 import { getRootPackageJsonPath, projectHasModule } from './Modules';
-// import { ModConfig } from './Plugin.types';
 import { getExpoSDKVersion } from './Project';
 import { getDynamicConfig, getStaticConfig } from './getConfig';
 
