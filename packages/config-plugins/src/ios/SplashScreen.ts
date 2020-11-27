942bccee78163d010d1b142f9f0c869f30c28a6f
Evan Bacon <baconbrix@gmail.com>
2020-11-27 17:37:45 +0100

    Update SplashScreen.ts

--- packages/config-plugins/src/ios/SplashScreen.ts
+++ packages/config-plugins/src/ios/SplashScreen.ts
@@ -1,6 +1,6 @@
 import { ExpoConfig } from '@expo/config-types';
 import fs from 'fs-extra';
-import Jimp from 'jimp';
+import Jimp from 'jimp/es';
 import * as path from 'path';
 import { XcodeProject } from 'xcode';
 
