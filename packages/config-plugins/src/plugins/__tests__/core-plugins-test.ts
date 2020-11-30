5a8eeab8dec841e7881d9730dcb448ba64a842b2
Evan Bacon <baconbrix@gmail.com>
2020-11-30 14:06:39 +0100

    Update core-plugins-test.ts

--- packages/config-plugins/src/plugins/__tests__/core-plugins-test.ts
+++ packages/config-plugins/src/plugins/__tests__/core-plugins-test.ts
@@ -3,40 +3,55 @@ import { createRunOncePlugin, withExtendedMod, withPlugins, withRunOnce } from '
 import { evalModsAsync } from '../mod-compiler';
 
 describe(withRunOnce, () => {
-  it('prevents running duplicates', () => {
+  it('runs plugins multiple times without withRunOnce', () => {
+    const pluginA: ConfigPlugin = jest.fn(config => config);
+
+    withPlugins({ extra: [] } as any, [
+      // Prove unsafe runs as many times as it was added
+      pluginA,
+      pluginA,
+    ]);
+
+    // Unsafe runs multiple times
+    expect(pluginA).toBeCalledTimes(2);
+  });
+
+  it('prevents running different plugins with same id', () => {
     const pluginA: ConfigPlugin = jest.fn(config => config);
     const pluginB: ConfigPlugin = jest.fn(config => config);
-    const pluginC: ConfigPlugin = jest.fn(config => config);
-    const pluginD: ConfigPlugin = jest.fn(config => config);
 
     const pluginId = 'foo';
 
     const safePluginA = createRunOncePlugin(pluginA, pluginId);
-    const safePluginB = createRunOncePlugin(pluginB, 'bar');
     // A different plugin with the same ID as (A), this proves
     // that different plugins can be prevented when using the same ID.
-    const safePluginC = createRunOncePlugin(pluginB, pluginId);
+    const safePluginB = createRunOncePlugin(pluginB, pluginId);
+
+    withPlugins({ extra: [] } as any, [
+      // Run plugin twice
+      safePluginA,
+      safePluginB,
+    ]);
+
+    // Prove that each plugin is only run once
+    expect(pluginA).toBeCalledTimes(1);
+    expect(pluginB).toBeCalledTimes(0);
+  });
+
+  it('prevents running the same plugin twice', () => {
+    const pluginA: ConfigPlugin = jest.fn(config => config);
+    const pluginId = 'foo';
+
+    const safePluginA = createRunOncePlugin(pluginA, pluginId);
 
     withPlugins({ extra: [] } as any, [
       // Run plugin twice
       safePluginA,
       safePluginA,
-      // Run plugin
-      safePluginB,
-      safePluginB,
-      //
-      safePluginC,
-      // Prove unsafe runs as many times as it was added
-      pluginD,
-      pluginD,
     ]);
 
     // Prove that each plugin is only run once
     expect(pluginA).toBeCalledTimes(1);
-    expect(pluginB).toBeCalledTimes(1);
-    expect(pluginC).toBeCalledTimes(0);
-    // Unsafe runs multiple times
-    expect(pluginD).toBeCalledTimes(2);
   });
 });
 
