18a04582f040e232143a8d9c8a1c6d3e8b518f60
Evan Bacon <baconbrix@gmail.com>
2020-12-09 14:22:35 -0800

    Created ModError

--- packages/config-plugins/src/utils/errors.ts
+++ packages/config-plugins/src/utils/errors.ts
@@ -6,6 +6,22 @@ export class UnexpectedError extends Error {
   }
 }
 
+export class ModError extends Error {
+  constructor(messageOrError?: string | Error) {
+    const message =
+      (typeof messageOrError === 'string' ? messageOrError : messageOrError?.message) ??
+      'This error should fail silently in the CLI';
+    super(message);
+    if (typeof messageOrError !== 'string') {
+      // forward the props of the incoming error for tests or processes outside of expo-cli that use expo cli internals.
+      this.stack = messageOrError?.stack ?? this.stack;
+      this.name = messageOrError?.name ?? this.name;
+    } else {
+      this.name = 'ModError';
+    }
+  }
+}
+
 export function assert(value: any, message?: string | Error): asserts value {
   // TODO: Upgrade node? TypeScript isn't properly asserting values without this wrapper.
   return nodeAssert(value, message);
