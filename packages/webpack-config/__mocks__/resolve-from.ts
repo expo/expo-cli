f5a005bf1900235ece66754dac914c342f7276b5
Evan Bacon <baconbrix@gmail.com>
2021-01-28 16:35:32 -0800

    fix tests

---
+++ packages/webpack-config/__mocks__/resolve-from.ts
@@ -0,0 +1,24 @@
+module.exports = require(require.resolve('resolve-from'));
+
+module.exports.silent = (fromDirectory, request) => {
+  try {
+    const fs = require('fs');
+    const path = require('path');
+    try {
+      fromDirectory = fs.realpathSync(fromDirectory);
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        fromDirectory = path.resolve(fromDirectory);
+      } else {
+        return;
+      }
+    }
+
+    const outputPath = path.join(fromDirectory, 'node_modules', request);
+    if (fs.existsSync(outputPath)) {
+      return outputPath;
+    }
+  } catch {
+    return null;
+  }
+};
