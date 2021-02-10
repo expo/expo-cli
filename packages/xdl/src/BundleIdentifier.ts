fdd5c660273f2018e0795e1aa8e70f8744f28647
Ville Immonen <ville.immonen@iki.fi>
2021-02-10 18:07:18 +0200

    Add a doc block

--- packages/xdl/src/BundleIdentifier.ts
+++ packages/xdl/src/BundleIdentifier.ts
@@ -1,3 +1,15 @@
+/**
+ * BundleIdentifier.ts
+ *
+ * NOTE:
+ * The code in this module originates from eas-cli and the canonical version of
+ * it is in
+ * https://github.com/expo/eas-cli/blob/6a0a9bbaaad03b053b5930f7d14bde35b4d0a9f0/packages/eas-cli/src/build/ios/bundleIdentifer.ts#L36
+ * Any changes to this code should be applied to eas-cli as well!
+ *
+ * TODO: move the code for configuring and validating the bundle identifier
+ * to a shared package that can be used for both eas-cli and expo-cli.
+ */
 import { ExpoConfig, getConfigFilePaths, getProjectConfigDescription } from '@expo/config';
 import { IOSConfig } from '@expo/config-plugins';
 import JsonFile from '@expo/json-file';
