4ec4f8f928f18ec13f2add6e51214d949c842c5c
Dominik Sokal <dominik.sokal@swmansion.com>
2021-02-11 16:06:18 +0100

    [config-plugins] fix detecting application target

--- packages/config-plugins/src/ios/__tests__/Target-test.ts
+++ packages/config-plugins/src/ios/__tests__/Target-test.ts
@@ -9,30 +9,60 @@ const fsReal = jest.requireActual('fs') as typeof fs;
 jest.mock('fs');
 
 describe(getApplicationTargetForSchemeAsync, () => {
-  beforeAll(async () => {
-    vol.fromJSON(
-      {
-        'ios/testproject.xcodeproj/xcshareddata/xcschemes/testproject.xcscheme': fsReal.readFileSync(
-          path.join(__dirname, 'fixtures/testproject.xcscheme'),
-          'utf-8'
-        ),
-      },
-      '/app'
-    );
-  });
+  describe('single build action entry', () => {
+    beforeAll(async () => {
+      vol.fromJSON(
+        {
+          'ios/testproject.xcodeproj/xcshareddata/xcschemes/testproject.xcscheme': fsReal.readFileSync(
+            path.join(__dirname, 'fixtures/testproject.xcscheme'),
+            'utf-8'
+          ),
+        },
+        '/app'
+      );
+    });
 
-  afterAll(() => {
-    vol.reset();
-  });
+    afterAll(() => {
+      vol.reset();
+    });
 
-  it('returns the target name for existing scheme', async () => {
-    const target = await getApplicationTargetForSchemeAsync('/app', 'testproject');
-    expect(target).toBe('testproject');
-  });
+    it('returns the target name for existing scheme', async () => {
+      const target = await getApplicationTargetForSchemeAsync('/app', 'testproject');
+      expect(target).toBe('testproject');
+    });
 
-  it('throws if the scheme does not exist', async () => {
-    await expect(() =>
-      getApplicationTargetForSchemeAsync('/app', 'nonexistentscheme')
-    ).rejects.toThrow(/does not exist/);
+    it('throws if the scheme does not exist', async () => {
+      await expect(() =>
+        getApplicationTargetForSchemeAsync('/app', 'nonexistentscheme')
+      ).rejects.toThrow(/does not exist/);
+    });
+  });
+  describe('multiple build action entries', () => {
+    beforeAll(async () => {
+      vol.fromJSON(
+        {
+          'ios/testproject.xcodeproj/xcshareddata/xcschemes/testproject.xcscheme': fsReal.readFileSync(
+            path.join(__dirname, 'fixtures/testproject-2.xcscheme'),
+            'utf-8'
+          ),
+        },
+        '/app'
+      );
+    });
+
+    afterAll(() => {
+      vol.reset();
+    });
+
+    it('returns the target name for existing scheme', async () => {
+      const target = await getApplicationTargetForSchemeAsync('/app', 'testproject');
+      expect(target).toBe('testproject');
+    });
+
+    it('throws if the scheme does not exist', async () => {
+      await expect(() =>
+        getApplicationTargetForSchemeAsync('/app', 'nonexistentscheme')
+      ).rejects.toThrow(/does not exist/);
+    });
   });
 });
