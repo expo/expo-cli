9ae8ab8416ddc6c63c4c196da0858d1fbdbf99a9
Evan Bacon <baconbrix@gmail.com>
2021-02-16 00:02:38 -0800

    Remove var from sendTo

--- packages/expo-cli/src/sendTo.ts
+++ packages/expo-cli/src/sendTo.ts
@@ -24,10 +24,10 @@ export async function sendUrlAsync(url: string, recipient: string) {
   const email = Log.chalk.bold(recipient);
   const spinner = ora(`Sending URL to ${email}`).start();
   try {
-    var result = await Exp.sendAsync(recipient, url);
+    const result = await Exp.sendAsync(recipient, url);
     spinner.succeed(`Sent URL to ${email}`);
+    return result;
   } catch (e) {
     spinner.fail(`Failed to email ${email}: ${e.message}`);
   }
-  return result;
 }
