ad635ef1407028e915ecfb7bfa8ba4451f8cdc4e
Evan Bacon <baconbrix@gmail.com>
2021-02-25 17:42:32 -0800

    updated format

--- packages/expo-cli/src/commands/ios/XcodeBuild.ts
+++ packages/expo-cli/src/commands/ios/XcodeBuild.ts
@@ -119,11 +119,37 @@ export type BuildProps = {
   scheme: string;
 };
 
+function moduleNameFromPath(modulePath: string) {
+  if (modulePath.startsWith('@')) {
+    const [org, packageName] = modulePath.split('/');
+    if (org && packageName) {
+      return [org, packageName].join('/');
+    }
+    return modulePath;
+  }
+  const [packageName] = modulePath.split('/');
+  return packageName ? packageName : modulePath;
+}
+
+function getNodeModuleName(filePath: string): string | null {
+  // '/Users/evanbacon/Documents/GitHub/lab/yolo5/node_modules/react-native-reanimated/ios/Nodes/REACallFuncNode.m'
+  const [, modulePath] = filePath.split('/node_modules/');
+  if (modulePath) {
+    return moduleNameFromPath(modulePath);
+  }
+  return null;
+}
 class ExpoFormatter extends Formatter {
   shouldShowCompileWarning(filePath: string, lineNumber?: string, columnNumber?: string): boolean {
     if (Log.isDebug) return true;
     return !filePath.match(/node_modules/) && !filePath.match(/\/ios\/Pods\//);
   }
+
+  // formatCompile(fileName: string, filePath: string): string {
+  //   const moduleName = getNodeModuleName(filePath);
+  //   const moduleNameTag = moduleName ? chalk.dim(`(${moduleName})`) : undefined;
+  //   return ['\u203A', 'Compiling', fileName, moduleNameTag].filter(Boolean).join(' ');
+  // }
 }
 
 export function buildAsync({
