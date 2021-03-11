d9e9f0de735b8b5aea2a65c13c29b49fe3f487c5
Ville Immonen <ville.immonen@iki.fi>
2021-03-11 17:11:17 +0200

    Remove --platform flag and prompt

--- packages/expo-cli/src/commands/run/index.ts
+++ packages/expo-cli/src/commands/run/index.ts
@@ -1,7 +1,6 @@
 import { Command } from 'commander';
 
 import CommandError from '../../CommandError';
-import prompt from '../../prompts';
 import buildAndroidClientAsync from './buildAndroidClientAsync';
 
 type Options = {
@@ -9,37 +8,13 @@ type Options = {
   buildVariant?: string;
 };
 
-async function runAsync(projectRoot: string, options: Options) {
-  const platform = options.platform?.toLowerCase() ?? (await promptForPlatformAsync());
-  if (platform === 'android') {
-    if (typeof options.buildVariant !== 'string') {
-      throw new CommandError('--build-variant must be a string');
-    }
-    await buildAndroidClientAsync(projectRoot, {
-      buildVariant: options.buildVariant,
-    });
-  } else {
-    throw new Error('platform not implemented');
+async function runAndroidAsync(projectRoot: string, options: Options) {
+  if (typeof options.buildVariant !== 'string') {
+    throw new CommandError('--build-variant must be a string');
   }
-}
-
-async function promptForPlatformAsync(): Promise<'android' | 'ios'> {
-  const { platform } = await prompt({
-    type: 'select',
-    message: 'Choose platform',
-    name: 'platform',
-    choices: [
-      {
-        title: 'Android',
-        value: 'android',
-      },
-      {
-        title: 'iOS',
-        value: 'ios',
-      },
-    ],
+  await buildAndroidClientAsync(projectRoot, {
+    buildVariant: options.buildVariant,
   });
-  return platform;
 }
 
 export default function (program: Command) {
@@ -47,7 +22,6 @@ export default function (program: Command) {
     .command('run:android [path]')
     .helpGroup('experimental')
     .description('Build a development client and run it in on a device.')
-    .option('-p --platform <platform>', 'Platform: [android|ios]', /^(android|ios)$/i)
     .option('--build-variant [name]', '(Android) build variant', 'release')
-    .asyncActionProjectDir(runAsync);
+    .asyncActionProjectDir(runAndroidAsync);
 }
