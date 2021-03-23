19739fd39bf765179f35cddb643357805f3503a4
Evan Bacon <baconbrix@gmail.com>
2021-03-22 17:00:26 -0700

    Remove -showBuildSettings usages

--- packages/expo-cli/src/commands/run/ios/resolveOptionsAsync.ts
+++ packages/expo-cli/src/commands/run/ios/resolveOptionsAsync.ts
@@ -37,7 +37,13 @@ function findXcodeProjectPaths(
 function resolveXcodeProject(projectRoot: string): ProjectInfo {
   let paths = findXcodeProjectPaths(projectRoot, 'xcworkspace');
   if (paths.length) {
-    return { name: paths[0], isWorkspace: true };
+    return {
+      // Use full path instead of relative project root so that warnings and errors contain full paths as well, this helps with filtering.
+      // Also helps keep things consistent in monorepos.
+      name: paths[0],
+      // name: path.relative(projectRoot, paths[0]),
+      isWorkspace: true,
+    };
   }
   paths = findXcodeProjectPaths(projectRoot, 'xcodeproj');
   if (paths.length) {
