85d9c89c8586942d2e9f9182cfa271390a76216b
Evan Bacon <baconbrix@gmail.com>
2021-03-24 19:54:33 -0700

    Update ora.ts

--- packages/expo-cli/src/utils/ora.ts
+++ packages/expo-cli/src/utils/ora.ts
@@ -4,9 +4,12 @@ import oraReal from 'ora';
 // A custom ora spinner that sends the stream to stdout in CI, non-TTY, or expo's non-interactive flag instead of stderr (the default).
 export function ora(options?: oraReal.Options | string): oraReal.Ora {
   const inputOptions = typeof options === 'string' ? { text: options } : options || {};
+  const disabled = program.nonInteractive;
   return oraReal({
+    // Ensure our non-interactive mode emulates CI mode.
+    isEnabled: !disabled,
     // In non-interactive mode, send the stream to stdout so it prevents looking like an error.
-    stream: program.nonInteractive ? process.stdout : process.stderr,
+    stream: disabled ? process.stdout : process.stderr,
     ...inputOptions,
   });
 }
