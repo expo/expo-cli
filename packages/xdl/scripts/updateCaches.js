16746b94dd353dea088ef69f51a4269354f7311e
Ville Immonen <ville.immonen@iki.fi>
2021-03-26 18:13:57 +0200

    Remove Gulp

--- packages/xdl/scripts/updateCaches.js
+++ packages/xdl/scripts/updateCaches.js
@@ -1,8 +1,28 @@
-#!/usr/bin/env node
+const axios = require('axios');
+const { writeJsonSync } = require('fs-extra');
+const path = require('path');
 
-async function updateCaches() {}
+axios
+  .get('https://exp.host/--/versions')
+  .then(async ({ data }) => {
+    writeJsonSync(path.join(__dirname, '../caches/versions.json'), data);
 
-updateCaches().catch(error => {
-  console.error(error);
-  process.exit(1);
-});
+    for (const version of Object.keys(data.sdkVersions)) {
+      if (data.sdkVersions[version].isDeprecated) {
+        continue;
+      }
+      const {
+        data: { data: schema },
+      } = await axios.get(`https://exp.host/--/api/v2/project/configuration/schema/${version}`);
+
+      const filePath = path.join(__dirname, `../caches/schema-${version}.json`);
+      console.log('Writing', filePath);
+      writeJsonSync(filePath, schema);
+    }
+  })
+  .then(() => console.log('Caches updated.'))
+  .catch(error => {
+    console.error(error);
+    console.error('Updating caches failed.');
+    process.exit(1);
+  });
