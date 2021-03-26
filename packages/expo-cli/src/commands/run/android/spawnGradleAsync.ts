289af172ef23d3786cecbc2e0b89fd2bd2ea41de
Evan Bacon <baconbrix@gmail.com>
2021-03-26 15:42:19 -0700

    Open apk on device

--- packages/expo-cli/src/commands/run/android/spawnGradleAsync.ts
+++ packages/expo-cli/src/commands/run/android/spawnGradleAsync.ts
@@ -7,23 +7,23 @@ function capitalize(name: string) {
   return name.charAt(0).toUpperCase() + name.slice(1);
 }
 
-function getGradleTask(buildVariant: string): string {
-  return `install${capitalize(buildVariant)}`;
+function getGradleTask(variant: string): string {
+  return `install${capitalize(variant)}`;
 }
 
 function resolveGradleWPath(androidProjectPath: string): string {
   return path.join(androidProjectPath, process.platform === 'win32' ? 'gradlew.bat' : 'gradlew');
 }
 
-export async function spawnGradleTaskAsync({
+export async function spawnGradleAsync({
   androidProjectPath,
-  buildVariant,
+  variant,
 }: {
   androidProjectPath: string;
-  buildVariant: string;
+  variant: string;
 }) {
   const gradlew = resolveGradleWPath(androidProjectPath);
-  const task = getGradleTask(buildVariant);
+  const task = getGradleTask(variant);
   const args = [
     task,
     // ignore linting errors
