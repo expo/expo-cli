41068c93d5937f6ba1b93a160fd301df5a59c83b
Evan Bacon <baconbrix@gmail.com>
2021-04-01 00:20:25 -0700

    Activate android emulator window

--- packages/xdl/src/Android.ts
+++ packages/xdl/src/Android.ts
@@ -1,5 +1,6 @@
 import { ExpoConfig, getConfig, readExpRcAsync } from '@expo/config';
 import { AndroidConfig } from '@expo/config-plugins';
+import * as osascript from '@expo/osascript';
 import spawnAsync from '@expo/spawn-async';
 import chalk from 'chalk';
 import child_process, { execFileSync } from 'child_process';
@@ -551,6 +552,43 @@ async function _openUrlAsync({
   return openProject;
 }
 
+function getUnixPID(port: number | string) {
+  return execFileSync('lsof', [`-i:${port}`, '-P', '-t', '-sTCP:LISTEN'], {
+    encoding: 'utf8',
+    stdio: ['pipe', 'pipe', 'ignore'],
+  })
+    .split('\n')[0]
+    .trim();
+}
+
+export async function activateEmulatorWindowAsync(device: Device) {
+  if (
+    // only mac is supported for now.
+    process.platform !== 'darwin' ||
+    // can only focus emulators
+    device.type !== 'emulator'
+  ) {
+    return;
+  }
+
+  // Google Emulator ID: `emulator-5554` -> `5554`
+  const androidPid = device.pid!.match(/-(\d+)/)?.[1];
+  if (!androidPid) {
+    return;
+  }
+  // Unix PID
+  const pid = getUnixPID(androidPid);
+
+  try {
+    await osascript.execAsync(`
+  tell application "System Events"
+    set frontmost of the first process whose unix id is ${pid} to true
+  end tell`);
+  } catch {
+    // noop -- this feature is very specific and subject to failure.
+  }
+}
+
 export async function openAppAsync(
   device: Pick<Device, 'pid'>,
   {
@@ -635,6 +673,9 @@ async function openUrlAsync({
     if (!bootedDevice) {
       return;
     }
+
+    await activateEmulatorWindowAsync(bootedDevice);
+
     device = bootedDevice;
 
     let installedExpo = false;
