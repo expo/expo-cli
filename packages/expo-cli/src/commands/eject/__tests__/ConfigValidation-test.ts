f0b277723e82402e0adfed3516d215a9601f9791
Evan Bacon <baconbrix@gmail.com>
2021-04-03 17:30:24 -0700

    Update ConfigValidation-test.ts

--- packages/expo-cli/src/commands/eject/__tests__/ConfigValidation-test.ts
+++ packages/expo-cli/src/commands/eject/__tests__/ConfigValidation-test.ts
@@ -4,6 +4,7 @@ describe(validateBundleId, () => {
   it(`validates`, () => {
     expect(validateBundleId('bacon')).toBe(true);
     expect(validateBundleId('...b.a.-c.0.n...')).toBe(true);
+    expect(validateBundleId('.')).toBe(true);
     expect(validateBundleId('. ..')).toBe(false);
     expect(validateBundleId('_')).toBe(false);
     expect(validateBundleId(',')).toBe(false);
