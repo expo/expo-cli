d5bea1605d9be67d51442a38fd4fad553fc146f9
Evan Bacon <baconbrix@gmail.com>
2021-04-06 22:57:34 -0700

    Update Env.ts

--- packages/api/src/Env.ts
+++ packages/api/src/Env.ts
@@ -16,7 +16,3 @@ export function isStaging(): boolean {
 export function isLocal(): boolean {
   return getenv.boolish('EXPO_LOCAL', false);
 }
-
-export function maySkipManifestValidation(): boolean {
-  return !!getenv.string('EXPO_SKIP_MANIFEST_VALIDATION_TOKEN');
-}
