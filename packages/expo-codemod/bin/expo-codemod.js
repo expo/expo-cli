c073a72cb8d9505869267e7b9563c583c8977b59
cruzach <charliecruzan@gmail.com>
2021-04-14 13:45:09 -0400

    [codemod] don't throw on node 14

--- packages/expo-codemod/bin/expo-codemod.js
+++ packages/expo-codemod/bin/expo-codemod.js
@@ -2,7 +2,7 @@
 const semver = require('semver');
 const version = process.versions.node;
 
-if (semver.satisfies(version, '^10.13.0 || ^12.0.0 || ^13.0.0')) {
+if (semver.satisfies(version, '^10.13.0 || ^12.0.0 || ^13.0.0 || ^14.0.0')) {
   require('../build/cli.js')
     .runAsync(process.argv)
     .catch(error => {
@@ -10,5 +10,5 @@ if (semver.satisfies(version, '^10.13.0 || ^12.0.0 || ^13.0.0')) {
       process.exit(1);
     });
 } else {
-  throw new Error('expo-codemod supports Node versions ^10.13.0, ^12.0.0 and ^13.0.0.');
+  throw new Error('expo-codemod supports Node versions ^10.13.0, ^12.0.0, ^13.0.0, and ^14.0.0.');
 }
