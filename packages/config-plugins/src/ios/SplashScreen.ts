4f40c276ffe1c52f5bc13b914818ddc36646e460
Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>
2021-04-19 22:35:09 +0200

    [config-plugins] feat: enable splash screen plugin for any SDK greater than 38 (#3397)

--- packages/config-plugins/src/ios/SplashScreen.ts
+++ packages/config-plugins/src/ios/SplashScreen.ts
@@ -38,12 +38,16 @@ export function getSplashScreen(config: ExpoConfig): IosSplashScreenConfig | und
 }
 
 export async function setSplashScreenAsync(config: ExpoConfig, projectRoot: string) {
+  const majorVersionString =
+    config.sdkVersion === 'UNVERSIONED' ? null : config.sdkVersion?.split('.').shift();
   const splashScreenIsSupported =
-    config.sdkVersion === '39.0.0' || config.sdkVersion === '40.0.0' || !config.sdkVersion;
+    (majorVersionString && Number(majorVersionString) >= 39) ||
+    /** UNVERSIONED CASE */ !majorVersionString;
+
   if (!splashScreenIsSupported) {
     WarningAggregator.addWarningIOS(
       'splash',
-      'Unable to automatically configure splash screen. Please refer to the expo-splash-screen README for more information: https://github.com/expo/expo/tree/master/packages/expo-splash-screen'
+      `Unable to automatically configure splash screen. Automatic splash screen configuration is available since SDK 39. Please upgrade to the newer SDK version. Please refer to the expo-splash-screen README for more information: https://github.com/expo/expo/tree/master/packages/expo-splash-screen`
     );
     return;
   }
