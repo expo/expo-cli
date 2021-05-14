31cbde9945985021360320e7632856f1108f83b2
Brent Vatne <brentvatne@gmail.com>
2021-05-14 14:11:13 -0700

    Update packages/xdl/src/Android.ts

--- packages/xdl/src/Android.ts
+++ packages/xdl/src/Android.ts
@@ -529,6 +529,7 @@ async function _openUrlAsync({
   // launch the project!
   // https://github.com/expo/expo/issues/7772
   // adb shell monkey -p host.exp.exponent -c android.intent.category.LAUNCHER 1
+  // Note: this is not needed in Expo Development Client, it only applies to Expo Go
   if (applicationId === 'host.exp.exponent') {
     const openClient = await getAdbOutputAsync(
       adbPidArgs(
