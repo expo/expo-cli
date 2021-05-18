c2254a1b0beadfe316fd35ad28bd7c4cf77ddfd4
Evan Bacon <baconbrix@gmail.com>
2021-05-18 12:01:06 -0700

    Removed default push notifications entitlement

--- packages/config-plugins/src/ios/Entitlements.ts
+++ packages/config-plugins/src/ios/Entitlements.ts
@@ -142,8 +142,6 @@ const ENTITLEMENTS_TEMPLATE = `
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
-<key>aps-environment</key>
-<string>development</string>
 </dict>
 </plist>
 `;
