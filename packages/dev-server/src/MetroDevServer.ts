455daad72d5c84b2f437c2bbf8459528aeb29262
Test User <kudo@expo.io>
2021-05-20 16:41:29 +0800

    [poc] hook metro and cli to allow devtools from chrome-devtools-frontend.appspot.com

--- packages/dev-server/src/MetroDevServer.ts
+++ packages/dev-server/src/MetroDevServer.ts
@@ -1,11 +1,16 @@
 import Log from '@expo/bunyan';
 import * as ExpoMetroConfig from '@expo/metro-config';
-import { createDevServerMiddleware } from '@react-native-community/cli-server-api';
+import {
+  createDevServerMiddleware,
+  securityHeadersMiddleware,
+} from '@react-native-community/cli-server-api';
 import bodyParser from 'body-parser';
-import type { HandleFunction } from 'connect';
+import type { Server as ConnectServer, HandleFunction } from 'connect';
 import http from 'http';
+import type { IncomingMessage, ServerResponse } from 'http';
 import type Metro from 'metro';
 import resolveFrom from 'resolve-from';
+import { URL } from 'url';
 
 import LogReporter from './LogReporter';
 import clientLogsMiddleware from './middleware/clientLogsMiddleware';
@@ -51,6 +56,10 @@ export async function runMetroDevServerAsync(
     port: metroConfig.server.port,
     watchFolders: metroConfig.watchFolders,
   });
+
+  replaceCliSecurityHeaderMiddleware(middleware as ConnectServer);
+  middleware.use(remoteDevtoolsCorsMiddleware);
+
   middleware.use(bodyParser.json());
   middleware.use('/logs', clientLogsMiddleware(options.logger));
 
@@ -190,4 +199,67 @@ function importPluginMiddlewares(projectRoot: string): HandleFunction[] {
     }
   }
   return middlewares;
+}
+
+function replaceCliSecurityHeaderMiddleware(app: ConnectServer) {
+  const index = app.stack.findIndex(middleware => middleware.handle === securityHeadersMiddleware);
+  if (index >= 0) {
+    app.stack[index].handle = remoteDevtoolsSecurityHeadersMiddleware;
+  }
+}
+
+// Same as securityHeadersMiddleware but allow cross-origin requests from https://chrome-devtools-frontend.appspot.com/
+function remoteDevtoolsSecurityHeadersMiddleware(
+  req: IncomingMessage,
+  res: ServerResponse,
+  next: (err?: Error) => void
+) {
+  // Block any cross origin request.
+  if (
+    typeof req.headers.origin === 'string' &&
+    !req.headers.origin.match(/^https?:\/\/localhost:/) &&
+    !req.headers.origin.match(/^https:\/\/chrome-devtools-frontend\.appspot\.com/)
+  ) {
+    next(
+      new Error(
+        'Unauthorized request from ' +
+          req.headers.origin +
+          '. This may happen because of a conflicting browser extension. Please try to disable it and try again.'
+      )
+    );
+    return;
+  }
+
+  // Block MIME-type sniffing.
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+
+  next();
+}
+
+// Hook before metro to process *.map that will
+function remoteDevtoolsCorsMiddleware(
+  req: IncomingMessage,
+  res: ServerResponse,
+  next: (err?: Error) => void
+) {
+  if (req.url) {
+    // we just need the url.pathname,
+    // so it is okay for passing http://localhost:8081 here to make new URL() works like legacy url.parse.
+    const url = new URL(req.url, 'http://localhost:8081');
+    const origin = req.headers.origin;
+    const isValidOrigin =
+      origin &&
+      ['devtools://devtools', 'https://chrome-devtools-frontend.appspot.com'].includes(origin);
+    if (url.pathname.endsWith('.map') && origin && isValidOrigin) {
+      res.setHeader('Access-Control-Allow-Origin', origin);
+      const setHeader = res.setHeader.bind(res);
+      res.setHeader = (key, ...args) => {
+        if (key === 'Access-Control-Allow-Origin') {
+          return;
+        }
+        setHeader(key, ...args);
+      };
+    }
+  }
+  next();
 }
