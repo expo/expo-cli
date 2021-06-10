fcbe8a7dcc955add3817cfef1f1d92fbaa413ad8
Evan Bacon <baconbrix@gmail.com>
2021-06-10 13:33:00 -0500

    [config] ios splash screen plugin (#2706)
    * Import ios code from splash migration
    
    * working variation with limited functionality
    
    * Update SplashScreen.ts
    
    * Update SplashScreen.ts
    
    * added tests
    
    * updated plugins
    
    * Update Paths.ts
    
    * revert
    
    * Move xcode util
    
    * extract status bar code
    
    * Update SplashScreen.ts
    
    * Update StatusBar.ts
    
    * Update SplashScreen.ts
    
    * Updated types
    
    * updated tests
    
    * support incoming parameters
    
    * Update SplashScreen.ts
    
    * move StatusBar
    
    * Update index.ts
    
    * Update SplashScreen.ts
    
    * improve testing
    
    * XML splash screen API
    
    * run once
    
    * Update SplashScreen-test.ts
    
    * update splash screen plugin
    
    * Update Xcodeproj.ts
    
    * moved status bar changes to #3131
    
    * Update SplashScreen.ts
    
    * update
    
    * Delete SplashScreen-test.ts
    
    * Update InterfaceBuilder.ts
    
    * Update withIosSplashScreen.ts
    
    * Added support for more test props
    
    * Update withIosSplashScreen.ts
    
    * Update withIosSplashScreen-test.ts
    
    * refactor
    
    * allow missing image
    
    * revise debug label
    
    * Updated tests
    
    * Update withIosSplashInfoPlist.ts
    
    * Update withIosSplashInfoPlist.ts
    
    * Update InterfaceBuilder.ts

--- packages/prebuild-config/src/plugins/unversioned/expo-splash-screen/withIosSplashScreen.ts
+++ packages/prebuild-config/src/plugins/unversioned/expo-splash-screen/withIosSplashScreen.ts
@@ -1,62 +1,36 @@
-import { ConfigPlugin, WarningAggregator, withDangerousMod } from '@expo/config-plugins';
-import { ExpoConfig } from '@expo/config-types';
-import {
-  configureIosSplashScreen,
-  IosSplashScreenConfig,
-  SplashScreenImageResizeMode,
-} from '@expo/configure-splash-screen';
+import { ConfigPlugin, withPlugins } from '@expo/config-plugins';
+import Debug from 'debug';
 
-export const withIosSplashScreen: ConfigPlugin = config => {
-  return withDangerousMod(config, [
-    'ios',
-    async config => {
-      await setSplashScreenAsync(config, config.modRequest.projectRoot);
-      return config;
-    },
+import {
+  getIosSplashConfig,
+  IOSSplashConfig,
+  warnUnsupportedSplashProperties,
+} from './getIosSplashConfig';
+import { withIosSplashAssets } from './withIosSplashAssets';
+import { withIosSplashInfoPlist } from './withIosSplashInfoPlist';
+import { withIosSplashXcodeProject } from './withIosSplashXcodeProject';
+
+const debug = Debug('expo:prebuild-config:expo-splash-screen:ios');
+
+export const withIosSplashScreen: ConfigPlugin<IOSSplashConfig | undefined | null | void> = (
+  config,
+  splash
+) => {
+  // only warn once
+  warnUnsupportedSplashProperties(config);
+
+  // If the user didn't specify a splash object, infer the splash object from the Expo config.
+  if (!splash) {
+    splash = getIosSplashConfig(config);
+  } else {
+    debug(`custom splash config provided`);
+  }
+
+  debug(`config:`, splash);
+
+  return withPlugins(config, [
+    [withIosSplashInfoPlist, splash],
+    [withIosSplashAssets, splash],
+    [withIosSplashXcodeProject, splash],
   ]);
 };
-
-export function getSplashScreen(config: ExpoConfig): IosSplashScreenConfig | undefined {
-  if (!config.splash && !config.ios?.splash) {
-    return;
-  }
-
-  const result: IosSplashScreenConfig = {
-    imageResizeMode:
-      config.ios?.splash?.resizeMode ??
-      config.splash?.resizeMode ??
-      SplashScreenImageResizeMode.CONTAIN,
-    backgroundColor:
-      config.ios?.splash?.backgroundColor ?? config.splash?.backgroundColor ?? '#FFFFFF', // white
-    image: config.ios?.splash?.image ?? config.splash?.image,
-  };
-
-  return result;
-}
-
-export async function setSplashScreenAsync(config: ExpoConfig, projectRoot: string) {
-  const majorVersionString =
-    config.sdkVersion === 'UNVERSIONED' ? null : config.sdkVersion?.split('.').shift();
-  const splashScreenIsSupported =
-    (majorVersionString && Number(majorVersionString) >= 39) ||
-    /** UNVERSIONED CASE */ !majorVersionString;
-
-  if (!splashScreenIsSupported) {
-    WarningAggregator.addWarningIOS(
-      'splash',
-      `Unable to automatically configure splash screen. Automatic splash screen configuration is available since SDK 39. Please upgrade to the newer SDK version. Please refer to the expo-splash-screen README for more information: https://github.com/expo/expo/tree/master/packages/expo-splash-screen`
-    );
-    return;
-  }
-
-  const splashConfig = getSplashScreen(config);
-
-  if (!splashConfig) {
-    return;
-  }
-  try {
-    await configureIosSplashScreen(projectRoot, splashConfig);
-  } catch (e) {
-    WarningAggregator.addWarningIOS('splash', e);
-  }
-}
