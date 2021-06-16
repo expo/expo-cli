69cadefd03530ac7a5303cd6659e726f88c625a5
Evan Bacon <baconbrix@gmail.com>
2021-06-16 15:30:10 -0500

    Upggrade expo e2e test to 41

--- packages/expo-cli/e2e/TestUtils.ts
+++ packages/expo-cli/e2e/TestUtils.ts
@@ -39,27 +39,28 @@ export async function tryRunAsync(args: string[], options?: SpawnOptions): Promi
 export const minimumNativePkgJson = {
   main: 'node_modules/expo/AppEntry.js',
   dependencies: {
-    expo: '37.0.11',
-    react: '16.9.0',
-    // speed up test by using the unstable branch
-    'react-native': 'https://github.com/expo/react-native/archive/unstable/sdk-37.tar.gz',
+    expo: '~41.0.0',
+    react: '16.13.1',
+    'react-native': 'https://github.com/expo/react-native/archive/sdk-41.0.0.tar.gz',
   },
   devDependencies: {
-    '@babel/core': '7.9.0',
+    '@babel/core': '^7.9.0',
+  },
+  resolutions: {
+    'hermes-engine': '0.0.0',
   },
   scripts: {
     start: 'expo start',
     android: 'expo start --android',
     ios: 'expo start --ios',
     web: 'expo web',
-    eject: 'expo eject',
   },
   private: true,
 };
 
 export const minimumAppJson = {
   expo: {
-    sdkVersion: '37.0.0',
+    // sdkVersion: '41.0.0',
     android: { package: 'com.example.minimal' },
     ios: { bundleIdentifier: 'com.example.minimal' },
   },
