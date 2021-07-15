bf63c4105f3ee7930abcbe2cc7e6f7e372a00f13
Kudo Chien <kudo@expo.io>
2021-07-15 22:41:55 +0800

    install react-native-unimodules in migration

--- packages/expo-modules-migrate/src/index.ts
+++ packages/expo-modules-migrate/src/index.ts
@@ -1,22 +1,31 @@
 import { getConfig } from '@expo/config';
 import { compileModsAsync, ModPlatform } from '@expo/config-plugins';
+import * as PackageManager from '@expo/package-manager';
+import chalk from 'chalk';
 
 import { withAndroidModules } from './plugins/android/withAndroidModules';
 import { getProjectRoot } from './utils/getProjectRoot';
 
 async function runAsync(programName: string) {
   const projectRoot = getProjectRoot();
-  const platforms: ModPlatform[] = ['android', 'ios'];
+  const platforms: ModPlatform[] = ['android'];
   let { exp: config } = getConfig(projectRoot, {
     skipSDKVersionRequirement: true,
     isModdedConfig: true,
   });
 
   config = withAndroidModules(config);
-  config = await compileModsAsync(config, {
+
+  await compileModsAsync(config, {
     projectRoot,
     platforms,
   });
+
+  console.log(chalk.magenta('\u203A Installing react-native-unimodules...'));
+  const packageManager = PackageManager.createForProject(projectRoot);
+  await packageManager.addAsync('react-native-unimodules');
+
+  console.log(chalk.magenta('\u203A [expo-modules-migrate] Migration completed!'));
 }
 
 export function run(programName: string = 'expo-modules-migrate') {
