c06a8bf1872ca0f70ca55f27587138728783c381
Evan Bacon <baconbrix@gmail.com>
2021-07-19 17:13:23 -0600

    Drop support for worker-loader

--- packages/webpack-config/src/loaders/createWorkerLoader.ts
+++
@@ -1,7 +0,0 @@
-import { Rule } from 'webpack';
-
-export default (): Rule => ({
-  // Cannot exclude any node modules yet but in the future we should just target a select few.
-  test: /\.worker\.(js|mjs|ts)$/,
-  use: { loader: require.resolve('worker-loader') },
-});
