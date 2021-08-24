919859ee93e0a317f42b5e098e48db1b3dc287d0
Evan Bacon <9664363+EvanBacon@users.noreply.github.com>
2021-08-23 22:46:51 -0600

    Update exportAppAsync.ts

--- packages/expo-cli/src/commands/export/exportAppAsync.ts
+++ packages/expo-cli/src/commands/export/exportAppAsync.ts
@@ -22,24 +22,28 @@ import {
 export const ANONYMOUS_USERNAME = 'anonymous';
 
 /**
- * If the `eas` flag is true, the stucture of the outputDir will be:
-├── assets
-│   └── *
-├── bundles
-│   ├── android-01ee6e3ab3e8c16a4d926c91808d5320.js
-│   └── ios-ee8206cc754d3f7aa9123b7f909d94ea.js
-└── metadata.json
-
- * If the `eas` flag is not true, then this function is for self hosting 
+ * If the `eas` flag is true, the structure of the outputDir will be:
+ * ```
+ * ├── assets
+ * │   └── *
+ * ├── bundles
+ * │   ├── android-01ee6e3ab3e8c16a4d926c91808d5320.js
+ * │   └── ios-ee8206cc754d3f7aa9123b7f909d94ea.js
+ * └── metadata.json
+ * ```
+ *
+ * If the `eas` flag is not true, then this function is for self hosting
  * and the outputDir will have the files created in the project directory the following way:
-.
-├── android-index.json
-├── ios-index.json
-├── assets
-│   └── 1eccbc4c41d49fd81840aef3eaabe862
-└── bundles
-      ├── android-01ee6e3ab3e8c16a4d926c91808d5320.js
-      └── ios-ee8206cc754d3f7aa9123b7f909d94ea.js
+ *
+ * ```
+ * ├── android-index.json
+ * ├── ios-index.json
+ * ├── assets
+ * │   └── 1eccbc4c41d49fd81840aef3eaabe862
+ * └── bundles
+ *       ├── android-01ee6e3ab3e8c16a4d926c91808d5320.js
+ *       └── ios-ee8206cc754d3f7aa9123b7f909d94ea.js
+ * ```
  */
 export async function exportAppAsync(
   projectRoot: string,
