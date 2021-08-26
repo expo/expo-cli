dcd7cffbecc2560c8dfb27add2a73e3ee2aed81d
Axel Delafosse <axeldelafosse@gmail.com>
2021-08-26 18:19:24 +0200

    [expo-cli] generate smaller QR codes

--- ts-declarations/qrcode-terminal/index.d.ts
+++ ts-declarations/qrcode-terminal/index.d.ts
@@ -1,3 +1,3 @@
 declare module 'qrcode-terminal' {
-  export function generate(url: string, cb: (code: string) => void): void;
+  export function generate(url: string, opts: { small: boolean }, cb: (code: string) => void): void;
 }
