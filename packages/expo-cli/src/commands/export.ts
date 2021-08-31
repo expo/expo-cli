bdc2e44abc0c1e066a43f4842a8ad23aaeb28a7d
Evan Bacon <baconbrix@gmail.com>
2021-08-31 13:18:43 -0600

    [expo cli] refactor export command (#3794)
    * Added platform support to expo export
    
    * refactor more
    
    * Update exportAppAsync.ts
    
    * Update writeContents.ts
    
    * Update publishAsync.ts
    
    * Update ProjectAssets.ts
    
    * Update truncateLastLinesAsync.ts
    
    * moved warning
    
    * Update platformOptions.ts
    
    * updated types
    
    * Update exportAppAsync.ts
    
    * Added tests
    
    * Added export tests
    
    * pr feedback
    
    * Update writeContents-test.ts.snap
    
    * Update export.ts
    
    * Update createBundlesAsync.ts
    
    Co-authored-by: Evan Bacon <9664363+EvanBacon@users.noreply.github.com>

--- packages/expo-cli/src/commands/export.ts
+++ packages/expo-cli/src/commands/export.ts
@@ -13,38 +13,29 @@ export default function (program: Command) {
       .command('export [path]')
       .description('Export the static files of the app for hosting it on a web server')
       .helpGroup('core')
-      .option(
-        '-p, --public-url <url>',
-        'The public url that will host the static files. (Required)'
-      )
+      .option('--platform <all|android|ios>', 'Platforms: android, ios, all', 'all')
+      .option('-p, --public-url <url>', 'The public url that will host the static files (required)')
       .option('-c, --clear', 'Clear the Metro bundler cache')
-      .option(
-        '--output-dir <dir>',
-        'The directory to export the static files to. Default directory is `dist`',
-        'dist'
-      )
+      .option('--output-dir <dir>', 'The directory to export the static files to', 'dist')
       .option(
         '-a, --asset-url <url>',
-        "The absolute or relative url that will host the asset files. Default is './assets', which will be resolved against the public-url.",
+        'The absolute or relative url that will host the asset files',
         './assets'
       )
-      .option('-d, --dump-assetmap', 'Dump the asset map for further processing.')
+      .option('-d, --dump-assetmap', 'Dump the asset map for further processing')
       .option('--dev', 'Configure static files for developing locally using a non-https server')
-      .option('-s, --dump-sourcemap', 'Dump the source map for debugging the JS bundle.')
-      .option('-q, --quiet', 'Suppress verbose output.')
-      .option(
-        '-t, --target <managed|bare>',
-        'Target environment for which this export is intended.'
-      )
-      .option('--merge-src-dir <dir>', 'A repeatable source dir to merge in.', collect, [])
+      .option('-s, --dump-sourcemap', 'Dump the source map for debugging the JS bundle')
+      .option('-q, --quiet', 'Suppress verbose output')
+      .option('-t, --target <managed|bare>', 'Target environment for which this export is intended')
+      .option('--merge-src-dir <dir>', 'A repeatable source dir to merge in', collect, [])
       .option(
         '--merge-src-url <url>',
-        'A repeatable source tar.gz file URL to merge in.',
+        'A repeatable source tar.gz file URL to merge in',
         collect,
         []
       )
-      .option('--max-workers <num>', 'Maximum number of tasks to allow Metro to spawn.')
-      .option('--experimental-bundle', 'export bundles for use with EAS updates.'),
+      .option('--max-workers <num>', 'Maximum number of tasks to allow Metro to spawn')
+      .option('--experimental-bundle', 'export bundles for use with EAS updates'),
     () => import('./exportAsync'),
     { checkConfig: true }
   );
