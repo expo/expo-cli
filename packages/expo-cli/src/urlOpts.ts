90331c69138660155dd7db93767d05ec4c678476
Evan Bacon <9664363+EvanBacon@users.noreply.github.com>
2021-08-31 18:19:06 -0600

    Remove indent-string

--- packages/expo-cli/src/urlOpts.ts
+++ packages/expo-cli/src/urlOpts.ts
@@ -1,5 +1,4 @@
 import type { Command } from 'commander';
-import indentString from 'indent-string';
 import qrcodeTerminal from 'qrcode-terminal';
 import { Android, ConnectionStatus, ProjectSettings, Simulator, Webpack } from 'xdl';
 
