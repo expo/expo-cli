c84592aef2f158e255ed0974c353a8a2dbd82b1c
Evan Bacon <9664363+EvanBacon@users.noreply.github.com>
2021-09-20 16:12:01 -0600

    Update createMatcher-test.ts

--- packages/metro-config/src/transformer/__tests__/createMatcher-test.ts
+++ packages/metro-config/src/transformer/__tests__/createMatcher-test.ts
@@ -1,9 +1,23 @@
 import { createKnownCommunityMatcher } from '../createMatcher';
 
 describe(createKnownCommunityMatcher, () => {
-  it(`tests application code`, () => {
-    expect(
-      createKnownCommunityMatcher().test('node_modules/@react-native-community/foobar.js')
-    ).toBe(false);
+  it(`tests known community packages`, () => {
+    for (const id of [
+      'node_modules/native-base/src/index.js',
+      'node_modules/@react-native-community/somn/foobar.js',
+      'node_modules/@react-native/polyfills/index.js',
+    ]) {
+      expect(createKnownCommunityMatcher().test(id)).toBe(true);
+    }
+  });
+  it(`tests in monorepos`, () => {
+    for (const id of [
+      'custom/native-base/src/index.js',
+      'node_modules/@react-native-community/foobar.js',
+    ]) {
+      expect(createKnownCommunityMatcher({ folders: ['node_modules', 'custom'] }).test(id)).toBe(
+        true
+      );
+    }
   });
 });
