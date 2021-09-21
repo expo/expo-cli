139f0532b0208d952bcb3689d8ab24a84ef86820
Evan Bacon <9664363+EvanBacon@users.noreply.github.com>
2021-09-21 14:43:25 -0600

    Disable lint warnings

--- packages/next-adapter/src/babel.ts
+++ packages/next-adapter/src/babel.ts
@@ -29,7 +29,15 @@ module.exports = function (api: any) {
     presets: [
       // Only use next in the browser, it'll break your native project
       isWeb && require('next/babel'),
-      [require('babel-preset-expo'), { web: { useTransformReactJsxExperimental: true } }],
+      [
+        require('babel-preset-expo'),
+        {
+          web: { useTransformReactJsxExperimental: true },
+          // Disable the `no-anonymous-default-export` plugin in babel-preset-expo
+          // so users don't see duplicate warnings.
+          'no-anonymous-default-export': false,
+        },
+      ],
     ].filter(Boolean),
   };
 };
