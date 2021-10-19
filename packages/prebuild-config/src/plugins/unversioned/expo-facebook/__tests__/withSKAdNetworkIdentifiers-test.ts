206284d46fe1f0750bd45f90accfd80be9b555e4
Evan Bacon <bacon@expo.io>
2021-10-19 12:17:49 -0600

    Update withSKAdNetworkIdentifiers-test.ts

--- packages/prebuild-config/src/plugins/unversioned/expo-facebook/__tests__/withSKAdNetworkIdentifiers-test.ts
+++ packages/prebuild-config/src/plugins/unversioned/expo-facebook/__tests__/withSKAdNetworkIdentifiers-test.ts
@@ -1,28 +1,36 @@
 import { withSKAdNetworkIdentifiers } from '../withSKAdNetworkIdentifiers';
 
 describe(withSKAdNetworkIdentifiers, () => {
+  it(`skips adding ids to the Info.plist if facebookAppId isn't defined`, () => {
+    // @ts-ignore: ExpoConfig name and slug
+    expect(withSKAdNetworkIdentifiers({}, ['FOOBAR', 'other'])).toStrictEqual({});
+  });
   it(`adds ids to the Info.plist`, () => {
     // @ts-ignore: ExpoConfig name and slug
-    expect(withSKAdNetworkIdentifiers({}, ['FOOBAR', 'other'])).toStrictEqual({
-      ios: {
-        infoPlist: {
-          SKAdNetworkItems: [
-            {
-              SKAdNetworkIdentifier: 'foobar',
-            },
-            {
-              SKAdNetworkIdentifier: 'other',
-            },
-          ],
+    expect(withSKAdNetworkIdentifiers({ facebookAppId: 'xxx' }, ['FOOBAR', 'other'])).toStrictEqual(
+      {
+        facebookAppId: 'xxx',
+        ios: {
+          infoPlist: {
+            SKAdNetworkItems: [
+              {
+                SKAdNetworkIdentifier: 'foobar',
+              },
+              {
+                SKAdNetworkIdentifier: 'other',
+              },
+            ],
+          },
         },
-      },
-    });
+      }
+    );
   });
   it(`prevents adding duplicate ids to the Info.plist`, () => {
     expect(
       withSKAdNetworkIdentifiers(
         // @ts-ignore: ExpoConfig name and slug
         {
+          facebookAppId: 'xxx',
           ios: {
             infoPlist: {
               SKAdNetworkItems: [
@@ -36,6 +44,7 @@ describe(withSKAdNetworkIdentifiers, () => {
         ['foobar', 'other']
       )
     ).toStrictEqual({
+      facebookAppId: 'xxx',
       ios: {
         infoPlist: {
           SKAdNetworkItems: [
