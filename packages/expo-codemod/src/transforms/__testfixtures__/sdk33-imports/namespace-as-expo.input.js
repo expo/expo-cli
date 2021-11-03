6463a2686a16ea768933dec572ee248471e7e754
Evan Bacon <bacon@expo.io>
2021-11-03 15:54:45 -0700

    Update namespace-as-expo.input.js

--- packages/expo-codemod/src/transforms/__testfixtures__/sdk33-imports/namespace-as-expo.input.js
+++ packages/expo-codemod/src/transforms/__testfixtures__/sdk33-imports/namespace-as-expo.input.js
@@ -3,25 +3,33 @@ import React from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import * as Expo from 'expo';
 
+// eslint-disable-next-line import/namespace
 Expo.Updates.addListener(() => {});
 
+// eslint-disable-next-line import/namespace
 const documentDirectory = Expo.FileSystem.documentDirectory,
+  // eslint-disable-next-line import/namespace
   cacheDirectory = Expo.FileSystem.cacheDirectory;
+// eslint-disable-next-line import/namespace
 const manifest = Expo.Constants.manifest;
 
 export { documentDirectory, cacheDirectory, manifest };
 
 export function pickDocumentAsync(options) {
+  // eslint-disable-next-line import/namespace
   return Expo.DocumentPicker.getDocumentAsync(options);
 }
 
 export default () => (
   <ScrollView>
     <View style={styles.view}>
+      {/* eslint-disable-next-line import/namespace */}
       <Expo.BlurView tint="light" intensity={50} style={StyleSheet.absoluteFill}>
         <Text>Blur</Text>
+        {/* eslint-disable-next-line import/namespace */}
       </Expo.BlurView>
     </View>
+    {/* eslint-disable-next-line import/namespace */}
     <Expo.MapView style={styles.mapView} initialRegion={initialRegion} />
   </ScrollView>
 );
