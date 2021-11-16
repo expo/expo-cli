e0bb957f53e9624541b8fccbcf8e57cd6e870ae8
Evan Bacon <bacon@expo.io>
2021-11-16 14:47:09 -0700

    Added warning for using backgroundColor on iOS

--- packages/prebuild-config/src/plugins/unversioned/expo-system-ui/__tests__/withIosRootViewBackgroundColor-test.ts
+++ packages/prebuild-config/src/plugins/unversioned/expo-system-ui/__tests__/withIosRootViewBackgroundColor-test.ts
@@ -1,4 +1,26 @@
-import { setRootViewBackgroundColor } from '../withIosRootViewBackgroundColor';
+import {
+  setRootViewBackgroundColor,
+  shouldUseLegacyBehavior,
+} from '../withIosRootViewBackgroundColor';
+
+describe(shouldUseLegacyBehavior, () => {
+  it(`should use legacy behavior in SDK –43`, () => {
+    expect(shouldUseLegacyBehavior({ sdkVersion: '43.0.0' })).toBe(true);
+    expect(shouldUseLegacyBehavior({ sdkVersion: '42.0.0' })).toBe(true);
+    expect(shouldUseLegacyBehavior({ sdkVersion: '31.0.0' })).toBe(true);
+  });
+  it(`should not use legacy behavior in SDK +44`, () => {
+    expect(shouldUseLegacyBehavior({ sdkVersion: '44.0.0' })).toBe(false);
+    expect(shouldUseLegacyBehavior({ sdkVersion: '46.0.0' })).toBe(false);
+    expect(shouldUseLegacyBehavior({ sdkVersion: '100.0.0' })).toBe(false);
+  });
+  it(`should not use legacy behavior when UNVERSIONED`, () => {
+    expect(shouldUseLegacyBehavior({ sdkVersion: 'UNVERSIONED' })).toBe(false);
+  });
+  it(`should not use legacy behavior when undefined`, () => {
+    expect(shouldUseLegacyBehavior({})).toBe(false);
+  });
+});
 
 describe(setRootViewBackgroundColor, () => {
   it(`sets color`, () => {
