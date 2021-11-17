ea07fda2ed6c311968f6f17ca04778b9308ef6d5
Evan Bacon <baconbrix@gmail.com>
2021-11-16 18:35:50 -0700

    Added warning for using backgroundColor on iOS (#4012)

--- packages/prebuild-config/src/plugins/unversioned/expo-system-ui/withIosRootViewBackgroundColor.ts
+++ packages/prebuild-config/src/plugins/unversioned/expo-system-ui/withIosRootViewBackgroundColor.ts
@@ -1,7 +1,8 @@
-import { ConfigPlugin, InfoPlist, withInfoPlist } from '@expo/config-plugins';
+import { ConfigPlugin, InfoPlist, WarningAggregator, withInfoPlist } from '@expo/config-plugins';
 import { ExpoConfig } from '@expo/config-types';
 // @ts-ignore: uses flow
 import normalizeColor from '@react-native/normalize-color';
+import semver from 'semver';
 
 // Maps to the template AppDelegate.m
 const BACKGROUND_COLOR_KEY = 'RCTRootViewBackgroundColor';
@@ -10,11 +11,38 @@ const debug = require('debug')('expo:system-ui:plugin:ios');
 
 export const withIosRootViewBackgroundColor: ConfigPlugin = config => {
   config = withInfoPlist(config, config => {
-    config.modResults = setRootViewBackgroundColor(config, config.modResults);
+    if (shouldUseLegacyBehavior(config)) {
+      config.modResults = setRootViewBackgroundColor(config, config.modResults);
+    } else {
+      warnSystemUIMissing(config);
+    }
     return config;
   });
   return config;
 };
+
+/** The template was changed in SDK 43 to move the background color logic to the `expo-system-ui` module */
+export function shouldUseLegacyBehavior(config: Pick<ExpoConfig, 'sdkVersion'>): boolean {
+  try {
+    return !!(config.sdkVersion && semver.lt(config.sdkVersion, '44.0.0'));
+  } catch {}
+  return false;
+}
+
+export function warnSystemUIMissing(
+  config: Pick<ExpoConfig, 'sdkVersion' | 'backgroundColor' | 'ios'>
+) {
+  const backgroundColor = getRootViewBackgroundColor(config);
+
+  if (backgroundColor) {
+    // Background color needs to be set programmatically
+    WarningAggregator.addWarningIOS(
+      'ios.backgroundColor',
+      'Install expo-system-ui to enable this feature',
+      'https://docs.expo.dev/build-reference/migrating/#expo-config--backgroundcolor--depends-on'
+    );
+  }
+}
 
 export function setRootViewBackgroundColor(
   config: Pick<ExpoConfig, 'backgroundColor' | 'ios'>,
