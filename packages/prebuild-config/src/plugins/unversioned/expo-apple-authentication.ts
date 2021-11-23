d662b9d1be9d60d53464999b29a5b4f29a28c945
Evan Bacon <bacon@expo.io>
2021-11-23 16:03:12 -0700

    Drop unversioned apple authentication support

--- packages/prebuild-config/src/plugins/unversioned/expo-apple-authentication.ts
+++ packages/prebuild-config/src/plugins/unversioned/expo-apple-authentication.ts
@@ -1,28 +1,22 @@
-import { ConfigPlugin, withEntitlementsPlist } from '@expo/config-plugins';
-import { ExpoConfig } from '@expo/config-types';
-import { JSONObject } from '@expo/json-file';
+import { ConfigPlugin, WarningAggregator, withEntitlementsPlist } from '@expo/config-plugins';
 
 import { createLegacyPlugin } from './createLegacyPlugin';
 
-const withAppleSignInEntitlement: ConfigPlugin = config => {
+const withAppleSignInWarning: ConfigPlugin = config => {
   return withEntitlementsPlist(config, config => {
-    config.modResults = setAppleSignInEntitlement(config, config.modResults);
+    if (config.ios?.usesAppleSignIn) {
+      WarningAggregator.addWarningIOS(
+        'ios.usesAppleSignIn',
+        'Install expo-apple-authentication to enable the ios.usesAppleSignIn feature',
+        'https://docs.expo.dev/versions/latest/sdk/apple-authentication/#eas-build'
+      );
+    }
+
     return config;
   });
 };
 
-function setAppleSignInEntitlement(config: ExpoConfig, entitlementsPlist: JSONObject): JSONObject {
-  if (config.ios?.usesAppleSignIn) {
-    return {
-      ...entitlementsPlist,
-      'com.apple.developer.applesignin': ['Default'],
-    };
-  }
-
-  return entitlementsPlist;
-}
-
 export default createLegacyPlugin({
   packageName: 'expo-apple-authentication',
-  fallback: withAppleSignInEntitlement,
+  fallback: withAppleSignInWarning,
 });
