ae6abf58f8e96e4f070451df3198b1b00867c66b
Evan Bacon <bacon@expo.io>
2021-12-13 22:06:31 -0700

    Fix react-native-maps regex

--- packages/config-plugins/src/ios/__tests__/Maps-test.ts
+++ packages/config-plugins/src/ios/__tests__/Maps-test.ts
@@ -10,7 +10,11 @@ import {
   removeMapsCocoaPods,
   setGoogleMapsApiKey,
 } from '../Maps';
-import { ExpoModulesAppDelegate, UnimodulesAppDelegate } from './fixtures/AppDelegate';
+import {
+  ExpoModulesAppDelegate,
+  ExpoReactAppDelegate,
+  UnimodulesAppDelegate,
+} from './fixtures/AppDelegate';
 import { PodfileBasic } from './fixtures/Podfile';
 
 describe('MATCH_INIT', () => {
@@ -156,6 +160,16 @@ describe(addGoogleMapsAppDelegateInit, () => {
   });
   it(`adds maps import to Expo Modules AppDelegate`, () => {
     const results = addGoogleMapsAppDelegateInit(ExpoModulesAppDelegate, 'mykey');
+    // matches a static snapshot
+    expect(results.contents).toMatchSnapshot();
+    expect(results.contents).toMatch(/97501819d6911e5f50d66c63d369b0cec62853c2/);
+    // did add new content
+    expect(results.didMerge).toBe(true);
+    // didn't remove old content
+    expect(results.didClear).toBe(false);
+  });
+  it(`adds maps import to Expo Modules SDK 44 AppDelegate`, () => {
+    const results = addGoogleMapsAppDelegateInit(ExpoReactAppDelegate, 'mykey');
     // matches a static snapshot
     expect(results.contents).toMatchSnapshot();
     expect(results.contents).toMatch(/97501819d6911e5f50d66c63d369b0cec62853c2/);
