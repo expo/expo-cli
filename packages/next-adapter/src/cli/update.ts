968da5461f324776f7ba36ab1c24e33c8e8e0cae
Evan Bacon <baconbrix@gmail.com>
2021-12-21 19:01:19 -0700

    Remove all `yarn global add X` prompts (#4086)
    Yarn CLI has a lot of issues installing modules globally. This PR removes the prompt to globally install tools with `yarn global add` and instead just always uses `npm i -g`.

--- packages/next-adapter/src/cli/update.ts
+++ packages/next-adapter/src/cli/update.ts
@@ -1,18 +1,7 @@
 import chalk from 'chalk';
-import { execSync } from 'child_process';
-// @ts-ignore
 import checkForUpdate from 'update-check';
 
 const packageJson = () => require('@expo/next-adapter/package.json');
-
-function shouldUseYarn() {
-  try {
-    execSync('yarnpkg --version', { stdio: 'ignore' });
-    return true;
-  } catch {
-    return false;
-  }
-}
 
 export default async function shouldUpdate() {
   const update = checkForUpdate(packageJson()).catch(() => null);
@@ -20,17 +9,10 @@ export default async function shouldUpdate() {
   try {
     const res = await update;
     if (res && res.latest) {
-      const isYarn = shouldUseYarn();
-
       const _packageJson = packageJson();
       console.log();
       console.log(chalk.yellow.bold(`A new version of \`${_packageJson.name}\` is available`));
-      console.log(
-        'You can update by running: ' +
-          chalk.cyan(
-            isYarn ? `yarn global add ${_packageJson.name}` : `npm i -g ${_packageJson.name}`
-          )
-      );
+      console.log('You can update by running: ' + chalk.cyan(`npm i -g ${_packageJson.name}`));
       console.log();
     }
   } catch {
