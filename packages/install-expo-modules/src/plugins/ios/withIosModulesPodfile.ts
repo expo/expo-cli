a69d47105eeb79125adea0e9933701129e4c8094
Kudo Chien <kudo@expo.dev>
2021-12-22 11:47:31 +0800

    [install-expo-modules] Add expo_patch_react_imports! for sdk 44 (#4090)
    # Why
    
    Support https://github.com/expo/expo/pull/15655
    
    # How
    
    when the target installation is sdk 44 or above, insert `expo_patch_react_imports!(installer)` inside the `post_integrate do |installer|` block. if there's no existing post_integrate hook, append one right after `use_expo_modules!`
    
    # Test Plan
    
    # Unit Test
    
    ```
     PASS   install-expo-modules  src/plugins/ios/__tests__/withIosModulesPodfile-test.ts
      updatePodfile
        ✓ should support classic rn podfile (14 ms)
        ✓ minimum support for existing post_integrate hook (1 ms)
      withIosModulesPodfile sdkVersion snapshots
        ✓ sdkVersion 43.0.0 (1 ms)
        ✓ sdkVersion 44.0.0
    ```

--- packages/install-expo-modules/src/plugins/ios/withIosModulesPodfile.ts
+++ packages/install-expo-modules/src/plugins/ios/withIosModulesPodfile.ts
@@ -1,7 +1,7 @@
 import { ConfigPlugin, IOSConfig, withDangerousMod } from '@expo/config-plugins';
-import { insertContentsAtOffset } from '@expo/config-plugins/build/utils/commonCodeMod';
 import fs from 'fs';
 import path from 'path';
+import semver from 'semver';
 
 const { getProjectName } = IOSConfig.XcodeUtils;
 
@@ -12,7 +12,7 @@ export const withIosModulesPodfile: ConfigPlugin = config => {
       const podfile = path.join(config.modRequest.platformProjectRoot, 'Podfile');
       let contents = await fs.promises.readFile(podfile, 'utf8');
       const projectName = getProjectName(config.modRequest.projectRoot);
-      contents = updatePodfile(contents, projectName);
+      contents = updatePodfile(contents, projectName, config.sdkVersion);
 
       await fs.promises.writeFile(podfile, contents);
       return config;
@@ -20,19 +20,43 @@ export const withIosModulesPodfile: ConfigPlugin = config => {
   ]);
 };
 
-export function updatePodfile(contents: string, projectName: string): string {
+export function updatePodfile(
+  contents: string,
+  projectName: string,
+  sdkVersion: string | undefined
+): string {
+  // require autolinking module
   if (!contents.match(/^require.+'expo\/package\.json.+scripts\/autolinking/m)) {
     contents = `require File.join(File.dirname(\`node --print "require.resolve('expo/package.json')"\`), "scripts/autolinking")\n${contents}`;
   }
 
+  // use_expo_modules!
   if (!contents.match(/^\s*use_expo_modules!\s*$/m)) {
-    const targetRegExp = new RegExp(`^\\s*target\\s+['"]${projectName}['"]\\s+do\\s*$`, 'm');
-    const matched = contents.match(targetRegExp);
-    if (!matched) {
-      throw new Error(`Cannot find target at Podfile - targetName[${projectName}]`);
+    const targetRegExp = new RegExp(`(^\\s*target\\s+['"]${projectName}['"]\\s+do\\s*$)`, 'm');
+    contents = contents.replace(targetRegExp, '$1\n  use_expo_modules!');
+  }
+
+  // expo_patch_react_imports!
+  if (sdkVersion && semver.gte(sdkVersion, '44.0.0')) {
+    if (!contents.match(/\bexpo_patch_react_imports!\(installer\)\b/)) {
+      const regExpPostIntegrate = /(\bpost_integrate do \|installer\|)/;
+      if (contents.match(regExpPostIntegrate)) {
+        contents = contents.replace(
+          regExpPostIntegrate,
+          '$1\n    expo_patch_react_imports!(installer)'
+        );
+      } else {
+        // If there's no existing post_integrate hook,
+        // use the `use_expo_modules!` as the insertion anchor.
+        contents = contents.replace(
+          /(\buse_expo_modules!\n)/gm,
+          `$1\
+  post_integrate do |installer|
+    expo_patch_react_imports!(installer)
+  end\n`
+        );
+      }
     }
-    const offset = (matched?.index ?? 0) + matched[0].length;
-    contents = insertContentsAtOffset(contents, '\n  use_expo_modules!', offset);
   }
 
   return contents;
