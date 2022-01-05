8cd85e6593fffcce1bf15f0f6472fa8b9f7c949e
Quinlan Jung <quin@expo.io>
2022-01-04 18:49:41 -0800

    [xdl] switch cloudfront domain to classic-assets.eascdn.net (#4115)
    * [xdl] switch cloudfront domain to classic-assets.eascdn.net
    
    * changelog

--- packages/xdl/src/detach/AssetBundle.ts
+++ packages/xdl/src/detach/AssetBundle.ts
@@ -8,7 +8,7 @@ import urlJoin from 'url-join';
 import { AnyStandaloneContext, ExponentTools } from '../internal';
 
 const EXPO_DOMAINS = ['expo.io', 'exp.host', 'expo.test', 'localhost'];
-export const DEFAULT_CDN_HOST = 'https://d1wp6m56sqw74a.cloudfront.net';
+export const DEFAULT_CDN_HOST = 'https://classic-assets.eascdn.net';
 export const ASSETS_DIR_DEFAULT_URL = `${DEFAULT_CDN_HOST}/~assets`;
 
 type UrlResolver = (hash: string) => string;
