f9c5b6df62e3fd7d75071f5048a6ebeaffcc8675
Eric Samelson <eric@expo.io>
2022-01-13 16:41:59 -0800

    list all files under a single header in the readme

--- packages/xdl/src/ProjectSettings.ts
+++ packages/xdl/src/ProjectSettings.ts
@@ -259,17 +259,11 @@ export function dotExpoProjectDirectory(projectRoot: string): string {
 
 The ".expo" folder is created when an Expo project is started using "expo start" command.
 
-> What does the "packager-info.json" file contain?
+> What do the files contain?
 
-The "packager-info.json" file contains port numbers and process PIDs that are used to serve the application to the mobile device/simulator.
-
-> What does the "settings.json" file contain?
-
-The "settings.json" file contains the server configuration that is used to serve the application manifest.
-
-> What is \`devices.json\`?
-
-The "devices.json" file contains information about devices that have recently opened this project. This is used to populate the "Development sessions" list in your development builds.
+- "devices.json": contains information about devices that have recently opened this project. This is used to populate the "Development sessions" list in your development builds.
+- "packager-info.json": contains port numbers and process PIDs that are used to serve the application to the mobile device/simulator.
+- "settings.json": contains the server configuration that is used to serve the application manifest.
 
 > Should I commit the ".expo" folder?
 
