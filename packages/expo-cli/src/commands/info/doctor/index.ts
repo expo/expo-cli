be8358e00592732c489032ae2a26e969802c3d8d
kbrandwijk <kim.brandwijk@gmail.com>
2022-01-20 19:32:05 +0100

    fix: restore path info

--- packages/expo-cli/src/commands/info/doctor/index.ts
+++ packages/expo-cli/src/commands/info/doctor/index.ts
@@ -5,7 +5,7 @@ import { applyAsyncActionProjectDir } from '../../utils/applyAsyncAction';
 export default function (program: Command) {
   applyAsyncActionProjectDir(
     program
-      .command('doctor')
+      .command('doctor [path]')
       .description('Diagnose issues with the project')
       .helpGroup('info')
       .option('--fix-dependencies', 'Fix incompatible dependency versions'),
