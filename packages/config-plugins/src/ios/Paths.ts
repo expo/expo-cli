71a2837641524b130b0d00c80b0a76be815ba282
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2022-04-05 14:14:21 +0200

    match `xcodeproj` in ios directory only

--- packages/config-plugins/src/ios/Paths.ts
+++ packages/config-plugins/src/ios/Paths.ts
@@ -139,11 +139,11 @@ export function findSchemeNames(projectRoot: string): string[] {
 
 export function getAllXcodeProjectPaths(projectRoot: string): string[] {
   const iosFolder = 'ios';
-  const pbxprojPaths = globSync('**/*.xcodeproj', { cwd: projectRoot, ignore: ignoredPaths })
+  const pbxprojPaths = globSync('ios/**/*.xcodeproj', { cwd: projectRoot, ignore: ignoredPaths })
     .filter(project => !/test|example|sample/i.test(project) || path.dirname(project) === iosFolder)
-    .sort(project => (path.dirname(project) === iosFolder ? -1 : 1))
     // sort alphabetically to ensure this works the same across different devices (Fail in CI (linux) without this)
-    .sort();
+    .sort()
+    .sort(project => (path.dirname(project) === iosFolder ? -1 : 1));
 
   if (!pbxprojPaths.length) {
     throw new UnexpectedError(
