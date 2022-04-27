2b8093c066d0dd4a195c4bc08bf04afba8788131
Kudo Chien <kudo@expo.dev>
2022-04-27 23:59:36 +0800

    [install-expo-modules] Add react-native 0.68 and expo sdk 45 support (#4333)
    # Why
    
    close ENG-4618
    
    # How
    
    - install expo sdk based on target project's react-native version
    - will try to install `expo@^45.0.0` first and fallback to install `expo@>=45.0.0-0 <46.0.0` for prerelease version
    - add react-native 0.68 template support
    
    # Test Plan
    
    #### Unit Test
    
    ```
     PASS   install-expo-modules  src/utils/__tests__/expoVersionMappings-test.ts
      getDefaultSdkVersion
        ✓ should resolve as sdk 45 from react-native 0.68 project (16 ms)
        ✓ should resolve as sdk 45 from react-native 0.65 project (1 ms)
        ✓ should resolve as sdk 44 from react-native 0.64 project
    
     PASS   install-expo-modules  src/plugins/android/__tests__/withAndroidModulesMainApplication-test.ts
      setModulesMainApplication
        ✓ should able to update from react-native@>=0.68.0 template (2 ms)
    
     PASS   install-expo-modules  src/plugins/android/__tests__/withAndroidModulesMainActivity-test.ts
      setModulesMainActivity
        ✓ should add ReactActivityDelegateWrapper for react-native@>=0.68.0 - java (1 ms)
        ✓ should add ReactActivityDelegateWrapper for react-native@>=0.68.0 - kotlin (1 ms)
    ```
    
    #### Integration Test
    
    ```sh
    $ npx react-native init RN068 --version 0.68
    $ npx install-expo-modules
    
    $ expo run:android
    
    $ expo run:ios
    
    $ export ORG_GRADLE_PROJECT_newArchEnabled=true ; expo run:android
    
    $ cd ios; rm -rf Pods build; RCT_NEW_ARCH_ENABLED=1 pod install
    $ expo run:ios
    ```

--- packages/install-expo-modules/src/plugins/ios/__tests__/withIosModulesAppDelegate-test.ts
+++ packages/install-expo-modules/src/plugins/ios/__tests__/withIosModulesAppDelegate-test.ts
@@ -1,231 +1,79 @@
 import fs from 'fs';
 import path from 'path';
 
-import { getDefaultVersion } from '../../../utils/expoVersionMappings';
+import { getLatestSdkVersion } from '../../../utils/expoVersionMappings';
 import {
   updateModulesAppDelegateObjcHeader,
   updateModulesAppDelegateObjcImpl,
   updateModulesAppDelegateSwift,
 } from '../withIosModulesAppDelegate';
 
+const fixturesPath = path.resolve(__dirname, 'fixtures');
+
 describe(updateModulesAppDelegateObjcHeader, () => {
-  it('should migrate from classic RN AppDelegate header', () => {
-    const rawContents = `
-#import <React/RCTBridgeDelegate.h>
-#import <UIKit/UIKit.h>
+  it('should migrate from classic RN AppDelegate header', async () => {
+    const [rawContents, expectContents] = await Promise.all([
+      fs.promises.readFile(path.join(fixturesPath, 'AppDelegate.h'), 'utf8'),
+      fs.promises.readFile(path.join(fixturesPath, 'AppDelegate-updated.h'), 'utf8'),
+    ]);
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
-
-@property (nonatomic, strong) UIWindow *window;
-
-@end`;
-
-    const expectContents = `
-#import <React/RCTBridgeDelegate.h>
-#import <Expo/Expo.h>
-#import <UIKit/UIKit.h>
-
-@interface AppDelegate : EXAppDelegateWrapper <UIApplicationDelegate, RCTBridgeDelegate>
-
-@property (nonatomic, strong) UIWindow *window;
-
-@end`;
-
-    const contents = updateModulesAppDelegateObjcHeader(rawContents, getDefaultVersion());
-    expect(updateModulesAppDelegateObjcHeader(contents, getDefaultVersion())).toEqual(
-      expectContents
-    );
+    const sdkVersion = getLatestSdkVersion().expoSdkVersion;
+    const contents = updateModulesAppDelegateObjcHeader(rawContents, sdkVersion);
+    expect(updateModulesAppDelegateObjcHeader(contents, sdkVersion)).toEqual(expectContents);
     // Try it twice...
-    const nextContents = updateModulesAppDelegateObjcHeader(contents, getDefaultVersion());
-    expect(updateModulesAppDelegateObjcHeader(nextContents, getDefaultVersion())).toEqual(
-      expectContents
-    );
+    const nextContents = updateModulesAppDelegateObjcHeader(contents, sdkVersion);
+    expect(updateModulesAppDelegateObjcHeader(nextContents, sdkVersion)).toEqual(expectContents);
   });
 });
 
 describe(updateModulesAppDelegateObjcImpl, () => {
-  it('should migrate from classic RN AppDelegate implementation', () => {
-    const rawContents = `
-#import "AppDelegate.h"
+  it('should migrate from classic react-native@>=0.68.0 AppDelegate.mm implementation', async () => {
+    const [rawContents, expectContents] = await Promise.all([
+      fs.promises.readFile(path.join(fixturesPath, 'AppDelegate-rn068.mm'), 'utf8'),
+      fs.promises.readFile(path.join(fixturesPath, 'AppDelegate-rn068-updated.mm'), 'utf8'),
+    ]);
 
-#import <React/RCTBridge.h>
-#import <React/RCTBundleURLProvider.h>
-#import <React/RCTRootView.h>
-
-@implementation AppDelegate
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-#ifdef FB_SONARKIT_ENABLED
-  InitializeFlipper(application);
-#endif
-
-  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                               moduleName:@"RN0633"
-                                               initialProperties:nil];
-
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
-  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  UIViewController *rootViewController = [UIViewController new];
-  rootViewController.view = rootView;
-  self.window.rootViewController = rootViewController;
-  [self.window makeKeyAndVisible];
-  return YES;
-}
-
-- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
-{
-#if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-#else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-#endif
-}
-
-@end`;
-
-    const expectContents = `
-#import "AppDelegate.h"
-
-#import <React/RCTBridge.h>
-#import <React/RCTBundleURLProvider.h>
-#import <React/RCTRootView.h>
-
-@implementation AppDelegate
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-#ifdef FB_SONARKIT_ENABLED
-  InitializeFlipper(application);
-#endif
-
-  RCTBridge *bridge = [self.reactDelegate createBridgeWithDelegate:self launchOptions:launchOptions];
-  RCTRootView *rootView = [self.reactDelegate createRootViewWithBridge:bridge
-                                               moduleName:@"RN0633"
-                                               initialProperties:nil];
-
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
-  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  UIViewController *rootViewController = [self.reactDelegate createRootViewController];
-  rootViewController.view = rootView;
-  self.window.rootViewController = rootViewController;
-  [self.window makeKeyAndVisible];
-  [super application:application didFinishLaunchingWithOptions:launchOptions];
-  return YES;
-}
-
-- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
-{
-#if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-#else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-#endif
-}
-
-@end`;
-
-    const contents = updateModulesAppDelegateObjcImpl(rawContents, getDefaultVersion());
+    const sdkVersion = getLatestSdkVersion().expoSdkVersion;
+    const contents = updateModulesAppDelegateObjcImpl(rawContents, sdkVersion);
     expect(contents).toEqual(expectContents);
     // Try it twice...
-    const nextContents = updateModulesAppDelegateObjcImpl(contents, getDefaultVersion());
+    const nextContents = updateModulesAppDelegateObjcImpl(contents, sdkVersion);
+    expect(nextContents).toEqual(expectContents);
+  });
+
+  it('should migrate from classic react-native@<0.68.0 AppDelegate.m implementation', async () => {
+    const [rawContents, expectContents] = await Promise.all([
+      fs.promises.readFile(path.join(fixturesPath, 'AppDelegate.m'), 'utf8'),
+      fs.promises.readFile(path.join(fixturesPath, 'AppDelegate-updated.m'), 'utf8'),
+    ]);
+
+    const sdkVersion = getLatestSdkVersion().expoSdkVersion;
+    const contents = updateModulesAppDelegateObjcImpl(rawContents, sdkVersion);
+    expect(contents).toEqual(expectContents);
+    // Try it twice...
+    const nextContents = updateModulesAppDelegateObjcImpl(contents, sdkVersion);
     expect(nextContents).toEqual(expectContents);
   });
 });
 
 describe(updateModulesAppDelegateSwift, () => {
-  it('should migrate from basic RN AppDelegate.swift', () => {
-    const contents = `
-import UIKit
+  it('should migrate from basic RN AppDelegate.swift', async () => {
+    const [rawContents, expectContents] = await Promise.all([
+      fs.promises.readFile(path.join(fixturesPath, 'AppDelegate.swift'), 'utf8'),
+      fs.promises.readFile(path.join(fixturesPath, 'AppDelegate-updated.swift'), 'utf8'),
+    ]);
 
-@UIApplicationMain
-class AppDelegate: NSObject, UIApplicationDelegate, RCTBridgeDelegate {
-  var window: UIWindow?
-
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    let bridge = RCTBridge(delegate: self, launchOptions: launchOptions)!
-    let rootView = RCTRootView(bridge: bridge, moduleName: "HelloWorld", initialProperties: nil)
-
-    if #available(iOS 13.0, *) {
-      rootView.backgroundColor = UIColor.systemBackground
-    } else {
-      rootView.backgroundColor = UIColor.white
-    }
-
-    self.window = UIWindow(frame: UIScreen.main.bounds);
-    let rootViewController = UIViewController()
-    rootViewController.view = rootView
-    self.window?.rootViewController = rootViewController
-    self.window?.makeKeyAndVisible()
-    return true
-  }
-
-  func sourceURL(for bridge: RCTBridge!) -> URL! {
-    #if DEBUG
-    return RCTBundleURLProvider.sharedSettings()?.jsBundleURL(forBundleRoot: "index", fallbackResource: nil)
-    #else
-    return Bundle.main.url(forResource: "main", withExtension: "jsBundle")
-    #endif
-  }
-}
-`;
-
-    const expectContents = `
-import UIKit
-
-@UIApplicationMain
-class AppDelegate: AppDelegateWrapper, RCTBridgeDelegate {
-  var window: UIWindow?
-
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    let bridge = reactDelegate.createBridge(delegate: self, launchOptions: launchOptions)!
-    let rootView = reactDelegate.createRootView(bridge: bridge, moduleName: "HelloWorld", initialProperties: nil)
-
-    if #available(iOS 13.0, *) {
-      rootView.backgroundColor = UIColor.systemBackground
-    } else {
-      rootView.backgroundColor = UIColor.white
-    }
-
-    self.window = UIWindow(frame: UIScreen.main.bounds);
-    let rootViewController = reactDelegate.createRootViewController()
-    rootViewController.view = rootView
-    self.window?.rootViewController = rootViewController
-    self.window?.makeKeyAndVisible()
-    super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    return true
-  }
-
-  func sourceURL(for bridge: RCTBridge!) -> URL! {
-    #if DEBUG
-    return RCTBundleURLProvider.sharedSettings()?.jsBundleURL(forBundleRoot: "index", fallbackResource: nil)
-    #else
-    return Bundle.main.url(forResource: "main", withExtension: "jsBundle")
-    #endif
-  }
-}
-`;
-
-    expect(updateModulesAppDelegateSwift(contents, getDefaultVersion())).toEqual(expectContents);
+    const sdkVersion = getLatestSdkVersion().expoSdkVersion;
+    expect(updateModulesAppDelegateSwift(rawContents, sdkVersion)).toEqual(expectContents);
   });
 });
 
 describe('withIosModulesAppDelegate sdkVersion snapshots', () => {
-  const objcHeaderFixture = fs.readFileSync(
-    path.join(__dirname, 'fixtures', 'AppDelegate.h'),
-    'utf-8'
-  );
-  const objcImplFixture = fs.readFileSync(
-    path.join(__dirname, 'fixtures', 'AppDelegate.m'),
-    'utf-8'
-  );
-  const swiftFixture = fs.readFileSync(
-    path.join(__dirname, 'fixtures', 'AppDelegate.swift'),
-    'utf-8'
-  );
+  const [objcHeaderFixture, objcImplFixture, swiftFixture] = [
+    fs.readFileSync(path.join(fixturesPath, 'AppDelegate.h'), 'utf8'),
+    fs.readFileSync(path.join(fixturesPath, 'AppDelegate.m'), 'utf8'),
+    fs.readFileSync(path.join(fixturesPath, 'AppDelegate.swift'), 'utf8'),
+  ];
 
   ['43.0.0', '44.0.0'].forEach(sdkVersion => {
     it(`sdkVersion ${sdkVersion}`, () => {
--- packages/install-expo-modules/src/utils/expoVersionMappings.ts
+++ packages/install-expo-modules/src/utils/expoVersionMappings.ts
@@ -1,26 +1,52 @@
-interface VersionInfo {
+import resolveFrom from 'resolve-from';
+import semver from 'semver';
+
+export interface VersionInfo {
+  expoSdkVersion: string;
   iosDeploymentTarget: string;
+  reactNativeVersionRange: string;
 }
 
-const DEFAULT_VERSION = '44.0.0';
-
-export const ExpoVersionMappings: Record<string, VersionInfo> = {
-  '44.0.0': {
+export const ExpoVersionMappings: VersionInfo[] = [
+  // Please keep sdk versions in sorted order (latest sdk first)
+  {
+    expoSdkVersion: '45.0.0',
     iosDeploymentTarget: '12.0',
+    reactNativeVersionRange: '>= 0.65.0',
   },
-  '43.0.0': {
+  {
+    expoSdkVersion: '44.0.0',
     iosDeploymentTarget: '12.0',
+    reactNativeVersionRange: '< 0.68.0',
   },
-};
+  {
+    expoSdkVersion: '43.0.0',
+    iosDeploymentTarget: '12.0',
+    reactNativeVersionRange: '< 0.68.0',
+  },
+];
 
-export function getDefaultVersion(): string {
-  return DEFAULT_VERSION;
+export function getDefaultSdkVersion(projectRoot: string): VersionInfo {
+  const reactNativePackageJsonPath = resolveFrom.silent(projectRoot, 'react-native/package.json');
+  if (!reactNativePackageJsonPath) {
+    throw new Error(`Unable to find react-native package - projectRoot[${projectRoot}]`);
+  }
+  const reactNativeVersion = require(reactNativePackageJsonPath).version;
+  const versionInfo = ExpoVersionMappings.find(info =>
+    semver.satisfies(reactNativeVersion, info.reactNativeVersionRange)
+  );
+  if (!versionInfo) {
+    throw new Error(
+      `Unable to find compatible expo sdk version - reactNativeVersion[${reactNativeVersion}]`
+    );
+  }
+  return versionInfo;
 }
 
-export function isSupportedVersion(version: string): boolean {
-  return !!ExpoVersionMappings[version];
+export function getLatestSdkVersion(): VersionInfo {
+  return ExpoVersionMappings[0];
 }
 
-export function getIosDeploymentTarget(version: string): string {
-  return ExpoVersionMappings[version]?.iosDeploymentTarget;
+export function getVersionInfo(sdkVersion: string): VersionInfo | null {
+  return ExpoVersionMappings.find(info => info.expoSdkVersion === sdkVersion) ?? null;
 }
