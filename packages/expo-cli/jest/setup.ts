0c1379343cd19a2ff0cf270903391128f488e088
evanbacon <bacon@expo.io>
2022-05-20 18:16:48 -0500

    Update setup.ts

--- packages/expo-cli/jest/setup.ts
+++ packages/expo-cli/jest/setup.ts
@@ -1,19 +1,19 @@
 /* eslint-disable no-console */
 
-// const originalError = console.error;
-// const originalWarn = console.warn;
-// const originalLog = console.log;
+const originalError = console.error;
+const originalWarn = console.warn;
+const originalLog = console.log;
 
-// beforeAll(() => {
-//   console.error = jest.fn();
-//   console.warn = jest.fn();
-//   console.log = jest.fn();
-// });
+beforeAll(() => {
+  console.error = jest.fn();
+  console.warn = jest.fn();
+  console.log = jest.fn();
+});
 
-// afterAll(() => {
-//   console.error = originalError;
-//   console.warn = originalWarn;
-//   console.log = originalLog;
-// });
+afterAll(() => {
+  console.error = originalError;
+  console.warn = originalWarn;
+  console.log = originalLog;
+});
 
 jest.mock('@expo/rudder-sdk-node');
