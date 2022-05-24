87de1596f11a31186d5a47f9c6fc8a9731d46530
ger88555 <germanadolf@protonmail.com>
2022-05-23 19:13:05 -0700

    improvement(package-manager): move conditionals out of execSync trycatch

--- packages/package-manager/src/utils/shouldUseYarn.ts
+++ packages/package-manager/src/utils/shouldUseYarn.ts
@@ -3,17 +3,16 @@ import { execSync } from 'child_process';
 import { isUsingNpm } from './nodeWorkspaces';
 
 export default function shouldUseYarn(): boolean {
+  if (process.env.npm_config_user_agent?.startsWith('yarn')) {
+    return true;
+  }
+
+  if (isUsingNpm(process.cwd())) {
+    return false;
+  }
+
   try {
-    if (process.env.npm_config_user_agent?.startsWith('yarn')) {
-      return true;
-    }
-
     execSync('yarnpkg --version', { stdio: 'ignore' });
-
-    if (isUsingNpm(process.cwd())) {
-      return false;
-    }
-
     return true;
   } catch {
     return false;
