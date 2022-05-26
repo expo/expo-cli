5641e0d5b8b474cf0cee0da981e1a8713e16b715
Brent Vatne <brentvatne@gmail.com>
2022-05-26 16:12:51 -0700

    [doctor] Add version validation for expo-modules-autolinking, @expo/config-plugins, @expo/config (#4392)
    Co-authored-by: evanbacon <bacon@expo.io>

--- packages/expo-cli/src/commands/info/doctor/doctorAsync.ts
+++ packages/expo-cli/src/commands/info/doctor/doctorAsync.ts
@@ -1,20 +1,46 @@
 import { getConfig } from '@expo/config';
 import chalk from 'chalk';
-import { Doctor } from 'xdl';
+import { Doctor, Versions } from 'xdl';
 
 import Log from '../../../log';
+import { getRemoteVersionsForSdk } from '../../../utils/getRemoteVersionsForSdk';
 import { profileMethod } from '../../utils/profileMethod';
 import { validateDependenciesVersionsAsync } from '../../utils/validateDependenciesVersions';
+import { warnAboutDeepDependenciesAsync } from './depedencies/explain';
 import { warnUponCmdExe } from './windows';
 
 type Options = {
   fixDependencies?: boolean;
 };
 
+async function validateSupportPackagesAsync(sdkVersion: string) {
+  const versionsForSdk = await getRemoteVersionsForSdk(sdkVersion);
+
+  const supportPackagesToValidate = [
+    'expo-modules-autolinking',
+    '@expo/config-plugins',
+    '@expo/prebuild-config',
+  ];
+
+  for (const pkg of supportPackagesToValidate) {
+    const version = versionsForSdk[pkg];
+    if (version) {
+      await warnAboutDeepDependenciesAsync({ name: pkg, version });
+    }
+  }
+  Log.newLine();
+}
+
 export async function actionAsync(projectRoot: string, options: Options) {
   await warnUponCmdExe();
 
   const { exp, pkg } = profileMethod(getConfig)(projectRoot);
+
+  // Only use the new validation on SDK +45.
+  if (Versions.gteSdkVersion(exp, '45.0.0')) {
+    await validateSupportPackagesAsync(exp.sdkVersion!);
+  }
+
   const areDepsValid = await profileMethod(validateDependenciesVersionsAsync)(
     projectRoot,
     exp,
