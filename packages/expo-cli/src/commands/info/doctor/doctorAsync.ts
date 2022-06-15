0c4b7a7033b844fcf56233a42a9431ac1a3095a8
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2022-06-15 14:58:19 +0200

    [expo-cli] return non-zero exit code if package versions are incorrect

--- packages/expo-cli/src/commands/info/doctor/doctorAsync.ts
+++ packages/expo-cli/src/commands/info/doctor/doctorAsync.ts
@@ -13,7 +13,7 @@ type Options = {
   fixDependencies?: boolean;
 };
 
-async function validateSupportPackagesAsync(sdkVersion: string) {
+async function validateSupportPackagesAsync(sdkVersion: string): Promise<boolean> {
   const versionsForSdk = await getRemoteVersionsForSdk(sdkVersion);
 
   const supportPackagesToValidate = [
@@ -22,38 +22,54 @@ async function validateSupportPackagesAsync(sdkVersion: string) {
     '@expo/prebuild-config',
   ];
 
+  let allPackagesValid = true;
   for (const pkg of supportPackagesToValidate) {
     const version = versionsForSdk[pkg];
     if (version) {
-      await warnAboutDeepDependenciesAsync({ name: pkg, version });
+      const isVersionValid = await warnAboutDeepDependenciesAsync({ name: pkg, version });
+      if (!isVersionValid) {
+        allPackagesValid = false;
+      }
     }
   }
   Log.newLine();
+  return allPackagesValid;
 }
 
 export async function actionAsync(projectRoot: string, options: Options) {
   await warnUponCmdExe();
 
   const { exp, pkg } = profileMethod(getConfig)(projectRoot);
+  let foundSomeIssues = false;
 
   // Only use the new validation on SDK +45.
   if (Versions.gteSdkVersion(exp, '45.0.0')) {
-    await validateSupportPackagesAsync(exp.sdkVersion!);
+    if (!(await validateSupportPackagesAsync(exp.sdkVersion!))) {
+      foundSomeIssues = true;
+    }
   }
 
-  const areDepsValid = await profileMethod(validateDependenciesVersionsAsync)(
-    projectRoot,
-    exp,
-    pkg,
-    options.fixDependencies
-  );
+  if (
+    !(await profileMethod(validateDependenciesVersionsAsync)(
+      projectRoot,
+      exp,
+      pkg,
+      options.fixDependencies
+    ))
+  ) {
+    foundSomeIssues = true;
+  }
 
   // note: this currently only warns when something isn't right, it doesn't fail
   await Doctor.validateExpoServersAsync(projectRoot);
 
-  if ((await Doctor.validateWithNetworkAsync(projectRoot)) === Doctor.NO_ISSUES && areDepsValid) {
-    Log.log(chalk.green(`🎉 Didn't find any issues with the project!`));
-  } else {
+  if ((await Doctor.validateWithNetworkAsync(projectRoot)) !== Doctor.NO_ISSUES) {
+    foundSomeIssues = true;
+  }
+
+  if (foundSomeIssues) {
     process.exitCode = 1;
+  } else {
+    Log.log(chalk.green(`🎉 Didn't find any issues with the project!`));
   }
 }
