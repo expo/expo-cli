4ac50c75065b9562d843e40addd9f5f2cee773c0
Evan Bacon <bacon@expo.io>
2022-06-23 18:38:27 +0200

    fix license (#4436)

--- packages/webpack-config/src/plugins/ExpectedErrors/ExpectedErrorsPlugin.ts
+++ packages/webpack-config/src/plugins/ExpectedErrors/ExpectedErrorsPlugin.ts
@@ -1,6 +1,6 @@
 /**
- * Copyright (c) 2021 Expo, Inc.
- * Copyright (c) 2021 Vercel, Inc.
+ * Copyright © 2021 650 Industries.
+ * Copyright © 2021 Vercel, Inc.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
