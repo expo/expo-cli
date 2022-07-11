eaba63f8fd4633f9d95a41014f0c8ea4e90cd149
Evan Bacon <bacon@expo.io>
2022-07-11 21:28:00 +0200

    Update packages/dev-tools/components/DeprecatedBanner.js

--- packages/dev-tools/components/DeprecatedBanner.js
+++ packages/dev-tools/components/DeprecatedBanner.js
@@ -19,7 +19,7 @@ export default function DeprecatedBanner() {
     <div className={STYLES_BANNER}>
       <span className={STYLES_INDICATOR}>
         This Development UI is deprecated, please use the Terminal UI instead.{' '}
-        <a style={{ color: 'white' }} href="#">
+        <a style={{ color: 'white' }} href="https://blog.expo.dev/sunsetting-the-web-ui-for-expo-cli-ab12936d2206">
           Learn more
         </a>
         .
