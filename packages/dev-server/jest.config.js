fdacd1d183db5da61cfd3de2b61f18ca31973422
Evan Bacon <bacon@expo.io>
2022-07-15 12:51:49 +0200

    Deleted metro packages (#4465)
    * Deleted metro packages
    
    * add dev deps back

--- packages/dev-server/jest.config.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  preset: '../../jest/unit-test-config',
-  rootDir: path.resolve(__dirname),
-  displayName: require('./package').name,
-  roots: ['src'],
-};
