2c0cd3ef7ca50323532430f6f114e4be3fb32a96
Keith Kurak <keith.kurak@gmail.com>
2023-02-07 09:04:10 -0500

    remove unused types

--- packages/expo-doctor/src/api/getVersionsAsync.ts
+++ packages/expo-doctor/src/api/getVersionsAsync.ts
@@ -2,18 +2,6 @@ import fetch from 'node-fetch';
 
 /** Represents version info for a particular SDK. */
 export type SDKVersion = {
-  /** @example "2.16.1" */
-  iosVersion?: string;
-  /** @example "https://dpq5q02fu5f55.cloudfront.net/Exponent-2.17.4.tar.gz" */
-  iosClientUrl?: string;
-  /** @example "https://dev.to/expo/expo-sdk-38-is-now-available-5aa0" */
-  releaseNoteUrl?: string;
-  /** @example "2.17.4" */
-  iosClientVersion?: string;
-  /** @example "https://d1ahtucjixef4r.cloudfront.net/Exponent-2.16.1.apk" */
-  androidClientUrl?: string;
-  /** @example "2.16.1" */
-  androidClientVersion?: string;
   /** @example { "typescript": "~3.9.5" } */
   relatedPackages?: Record<string, string>;
 
@@ -27,10 +15,6 @@ export type SDKVersion = {
 export type SDKVersions = Record<string, SDKVersion>;
 
 export type Versions = {
-  androidUrl: string;
-  androidVersion: string;
-  iosUrl: string;
-  iosVersion: string;
   sdkVersions: SDKVersions;
 };
 
