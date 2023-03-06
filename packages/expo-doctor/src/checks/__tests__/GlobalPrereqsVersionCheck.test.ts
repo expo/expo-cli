fb1ddf2ccb3305a9152839cf39f5ef1034ba38bc
Keith Kurak <keith.kurak@gmail.com>
2023-03-06 11:57:37 -0500

    removed redundant describe blocks

--- packages/expo-doctor/src/checks/__tests__/GlobalPrereqsVersionCheck.test.ts
+++ packages/expo-doctor/src/checks/__tests__/GlobalPrereqsVersionCheck.test.ts
@@ -15,46 +15,44 @@ const additionalProjectProps = {
   pkg: {},
 };
 
-describe(GlobalPrereqsVersionCheck, () => {
-  describe('runAsync', () => {
-    it('returns result with isSuccessful = true if yarnpkg is installed', async () => {
-      asMock(spawnAsync).mockResolvedValueOnce({
-        status: 0,
-      } as any);
-      const check = new GlobalPrereqsVersionCheck();
-      const result = await check.runAsync({
-        projectRoot: '/path/to/project',
-        ...additionalProjectProps,
-      });
-      expect(result.isSuccessful).toBeTruthy();
+describe('runAsync', () => {
+  it('returns result with isSuccessful = true if yarnpkg is installed', async () => {
+    asMock(spawnAsync).mockResolvedValueOnce({
+      status: 0,
+    } as any);
+    const check = new GlobalPrereqsVersionCheck();
+    const result = await check.runAsync({
+      projectRoot: '/path/to/project',
+      ...additionalProjectProps,
     });
+    expect(result.isSuccessful).toBeTruthy();
+  });
 
-    it('returns result with isSuccessful = true if yarnpkg is not installed and npm version is in acceptable range', async () => {
-      asMock(spawnAsync).mockResolvedValueOnce({
-        status: 1,
-      } as any);
+  it('returns result with isSuccessful = true if yarnpkg is not installed and npm version is in acceptable range', async () => {
+    asMock(spawnAsync).mockResolvedValueOnce({
+      status: 1,
+    } as any);
 
-      asMock(execSync).mockReturnValueOnce('8.19.3');
-      const check = new GlobalPrereqsVersionCheck();
-      const result = await check.runAsync({
-        projectRoot: '/path/to/project',
-        ...additionalProjectProps,
-      });
-      expect(result.isSuccessful).toBeTruthy();
+    asMock(execSync).mockReturnValueOnce('8.19.3');
+    const check = new GlobalPrereqsVersionCheck();
+    const result = await check.runAsync({
+      projectRoot: '/path/to/project',
+      ...additionalProjectProps,
     });
+    expect(result.isSuccessful).toBeTruthy();
+  });
 
-    it('returns result with isSuccessful = false if yarnpkg is not installed and npm version is not in acceptable range', async () => {
-      asMock(spawnAsync).mockResolvedValueOnce({
-        status: 1,
-      } as any);
+  it('returns result with isSuccessful = false if yarnpkg is not installed and npm version is not in acceptable range', async () => {
+    asMock(spawnAsync).mockResolvedValueOnce({
+      status: 1,
+    } as any);
 
-      asMock(execSync).mockReturnValueOnce('5.0.0');
-      const check = new GlobalPrereqsVersionCheck();
-      const result = await check.runAsync({
-        projectRoot: '/path/to/project',
-        ...additionalProjectProps,
-      });
-      expect(result.isSuccessful).toBeFalsy();
+    asMock(execSync).mockReturnValueOnce('5.0.0');
+    const check = new GlobalPrereqsVersionCheck();
+    const result = await check.runAsync({
+      projectRoot: '/path/to/project',
+      ...additionalProjectProps,
     });
+    expect(result.isSuccessful).toBeFalsy();
   });
 });
--- packages/expo-doctor/src/checks/__tests__/InstalledDependencyVersionCheck.test.ts
+++ packages/expo-doctor/src/checks/__tests__/InstalledDependencyVersionCheck.test.ts
@@ -12,63 +12,58 @@ const additionalProjectProps = {
   pkg: {},
 };
 
-describe(InstalledDependencyVersionCheck, () => {
-  describe('runAsync', () => {
-    it('returns result with isSuccessful = true if check passes', async () => {
-      asMock(spawnAsync).mockResolvedValueOnce({
-        stdout: '',
-      } as any);
-      const check = new InstalledDependencyVersionCheck();
-      const result = await check.runAsync({
-        projectRoot: '/path/to/project',
-        ...additionalProjectProps,
-      });
-      expect(result.isSuccessful).toBeTruthy();
+describe('runAsync', () => {
+  it('returns result with isSuccessful = true if check passes', async () => {
+    asMock(spawnAsync).mockResolvedValueOnce({
+      stdout: '',
+    } as any);
+    const check = new InstalledDependencyVersionCheck();
+    const result = await check.runAsync({
+      projectRoot: '/path/to/project',
+      ...additionalProjectProps,
     });
+    expect(result.isSuccessful).toBeTruthy();
+  });
 
-    it('calls npx expo install --check', async () => {
-      const mockSpawnAsync = asMock(spawnAsync).mockResolvedValueOnce({
-        stdout: '',
-      } as any);
-      const check = new InstalledDependencyVersionCheck();
-      await check.runAsync({ projectRoot: '/path/to/project', ...additionalProjectProps });
-      expect(mockSpawnAsync.mock.calls[0][0]).toBe('sh');
-      expect(mockSpawnAsync.mock.calls[0][1]).toEqual([
-        '-c',
-        'echo "n" | npx expo install --check',
-      ]);
-    });
+  it('calls npx expo install --check', async () => {
+    const mockSpawnAsync = asMock(spawnAsync).mockResolvedValueOnce({
+      stdout: '',
+    } as any);
+    const check = new InstalledDependencyVersionCheck();
+    await check.runAsync({ projectRoot: '/path/to/project', ...additionalProjectProps });
+    expect(mockSpawnAsync.mock.calls[0][0]).toBe('sh');
+    expect(mockSpawnAsync.mock.calls[0][1]).toEqual(['-c', 'echo "n" | npx expo install --check']);
+  });
 
-    it('returns result with isSuccessful = false if check fails', async () => {
-      asMock(spawnAsync).mockImplementationOnce(() => {
-        const error: any = new Error();
-        error.stderr = 'error';
-        error.stdout = '';
-        error.status = 1;
-        throw error;
-      });
-      const check = new InstalledDependencyVersionCheck();
-      const result = await check.runAsync({
-        projectRoot: '/path/to/project',
-        ...additionalProjectProps,
-      });
-      expect(result.isSuccessful).toBeFalsy();
+  it('returns result with isSuccessful = false if check fails', async () => {
+    asMock(spawnAsync).mockImplementationOnce(() => {
+      const error: any = new Error();
+      error.stderr = 'error';
+      error.stdout = '';
+      error.status = 1;
+      throw error;
     });
+    const check = new InstalledDependencyVersionCheck();
+    const result = await check.runAsync({
+      projectRoot: '/path/to/project',
+      ...additionalProjectProps,
+    });
+    expect(result.isSuccessful).toBeFalsy();
+  });
 
-    it('pushes npx expo install --check stderr to issues list', async () => {
-      asMock(spawnAsync).mockImplementationOnce(() => {
-        const error: any = new Error();
-        error.stderr = 'error';
-        error.stdout = '';
-        error.status = 1;
-        throw error;
-      });
-      const check = new InstalledDependencyVersionCheck();
-      const result = await check.runAsync({
-        projectRoot: '/path/to/project',
-        ...additionalProjectProps,
-      });
-      expect(result.issues).toEqual(['error']);
+  it('pushes npx expo install --check stderr to issues list', async () => {
+    asMock(spawnAsync).mockImplementationOnce(() => {
+      const error: any = new Error();
+      error.stderr = 'error';
+      error.stdout = '';
+      error.status = 1;
+      throw error;
     });
+    const check = new InstalledDependencyVersionCheck();
+    const result = await check.runAsync({
+      projectRoot: '/path/to/project',
+      ...additionalProjectProps,
+    });
+    expect(result.issues).toEqual(['error']);
   });
 });
