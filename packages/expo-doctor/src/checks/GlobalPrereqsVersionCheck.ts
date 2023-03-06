580c2ad3432d1878ce9d702425b840e3bd1a0747
Keith Kurak <keith.kurak@gmail.com>
2023-03-06 12:03:29 -0500

    [doctor] remove watchman check (#4660)
    * [doctor] remove watchman check
    
    * added unit tests
    
    * removed redundant describe blocks

--- packages/expo-doctor/src/checks/GlobalPrereqsVersionCheck.ts
+++ packages/expo-doctor/src/checks/GlobalPrereqsVersionCheck.ts
@@ -7,7 +7,6 @@ import semver from 'semver';
 
 import { DoctorCheck, DoctorCheckParams, DoctorCheckResult } from './checks.types';
 
-const MIN_WATCHMAN_VERSION = '4.6.0';
 const MIN_NPM_VERSION = '3.0.0';
 const CORRECT_NPM_VERSION = 'latest';
 const WARN_NPM_VERSION_RANGES = ['>= 5.0.0 < 5.7.0'];
@@ -43,50 +42,6 @@ async function checkNpmVersionAsync(): Promise<string | null> {
   return null;
 }
 
-async function checkWatchmanVersionAsync(): Promise<string | null> {
-  const homebrewInstallNote = `\n\nIf you are using homebrew, try:\nbrew uninstall watchman; brew install watchman`;
-
-  let watchmanVersion;
-  // There's no point in checking any of this stuff if watchman isn't supported on this platform
-  if (process.platform !== 'darwin') {
-    return null;
-  }
-
-  // all checks going forward assume macOS, which should have watchman
-
-  const cannotDetectWatchmanInstallationMessage =
-    'Cannot detect watchman installation.' + homebrewInstallNote;
-
-  try {
-    watchmanVersion = JSON.parse((await spawnAsync('watchman', ['version'])).stdout.trim()).version;
-  } catch (e: any) {
-    return cannotDetectWatchmanInstallationMessage;
-  }
-
-  if (!watchmanVersion) {
-    // watchman is probably just not installed
-    return cannotDetectWatchmanInstallationMessage;
-  }
-
-  // Watchman versioning through homebrew is changed from semver to date since "2021.05.31.00"
-  // see: https://github.com/Homebrew/homebrew-core/commit/d299c2867503cb6ad8d90792343993c80e745071
-  const validSemver =
-    !!semver.valid(watchmanVersion) && semver.gte(watchmanVersion, MIN_WATCHMAN_VERSION);
-
-  // This new format is used later than the MIN_WATCHMAN_VERSION, we assume/estimate if the version is correct here.
-  const validNewVersion =
-    !validSemver && /[0-9]{4}\.[0-9]{2}\.[0-9]{2}\.[0-9]{2}/.test(watchmanVersion);
-
-  if (!validSemver && !validNewVersion) {
-    return (
-      `Warning: You are using an old version of watchman (v${watchmanVersion}).\n\nIt is recommend to always use the latest version, or at least v${MIN_WATCHMAN_VERSION}.` +
-      homebrewInstallNote
-    );
-  }
-
-  return null;
-}
-
 export class GlobalPrereqsVersionCheck implements DoctorCheck {
   description = 'Validating global prerequisites versions';
 
@@ -95,15 +50,11 @@ export class GlobalPrereqsVersionCheck implements DoctorCheck {
   async runAsync({ exp, projectRoot }: DoctorCheckParams): Promise<DoctorCheckResult> {
     const issues: string[] = [];
 
-    const checkResults = await Promise.all([
-      await checkWatchmanVersionAsync(),
-      await checkNpmVersionAsync(),
-    ]);
-    checkResults.forEach(result => {
-      if (result) {
-        issues.push(result);
-      }
-    });
+    const checkResult = await checkNpmVersionAsync();
+
+    if (checkResult) {
+      issues.push(checkResult);
+    }
 
     // Should we check for node LTS? Maybe warn if 18+ but configured for web?
 
