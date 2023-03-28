2c77f63080877cbc7bcb3a43a65e05ab8fdf6b88
Cedric van Putten <me@bycedric.com>
2023-03-28 15:46:03 +0200

    fix(doctor): replace commander-based interactivity check with tty/ci check (#4672)

--- packages/expo-doctor/src/utils/env.ts
+++ packages/expo-doctor/src/utils/env.ts
@@ -1,6 +1,11 @@
 import { boolish } from 'getenv';
 
 class Env {
+  /** Is running in non-interactive CI mode */
+  get CI() {
+    return boolish('CI', false);
+  }
+
   /** Enable debug logging */
   get EXPO_DEBUG() {
     return boolish('EXPO_DEBUG', false);
