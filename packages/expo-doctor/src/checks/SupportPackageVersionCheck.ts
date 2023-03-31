0b97723de2ba755084da462b5f8c0c54857824c8
Keith Kurak <keith.kurak@gmail.com>
2023-03-31 14:59:29 -0400

    Run some deep dependency checks in parallel (#4675)
    * [doctor] run deep dependency checks in parallel
    
    * unit tests

--- packages/expo-doctor/src/checks/SupportPackageVersionCheck.ts
+++ packages/expo-doctor/src/checks/SupportPackageVersionCheck.ts
@@ -13,20 +13,26 @@ export class SupportPackageVersionCheck implements DoctorCheck {
 
     const versionsForSdk = await getRemoteVersionsForSdkAsync(exp.sdkVersion);
 
+    // only check for packages that have a required semver for the current SDK version
     const supportPackagesToValidate = [
       'expo-modules-autolinking',
       '@expo/config-plugins',
       '@expo/prebuild-config',
-    ];
-    for (const pkg of supportPackagesToValidate) {
-      const version = versionsForSdk[pkg];
-      if (version) {
-        const warning = await getDeepDependenciesWarningAsync({ name: pkg, version }, projectRoot);
-        if (warning) {
-          issues.push(warning);
-        }
+    ].filter(pkg => versionsForSdk[pkg]);
+
+    // check that a specific semver is installed for each package
+    const possibleWarnings = await Promise.all(
+      supportPackagesToValidate.map(pkg =>
+        getDeepDependenciesWarningAsync({ name: pkg, version: versionsForSdk[pkg] }, projectRoot)
+      )
+    );
+
+    possibleWarnings.forEach(possibleWarning => {
+      if (possibleWarning) {
+        issues.push(possibleWarning);
       }
-    }
+    });
+
     if (issues.length) {
       advice.push(`Upgrade dependencies that are using the invalid package versions.`);
     }
