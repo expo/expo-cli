d6d0e7f4c07b5a02bd35f278df46b82e0997e865
Quinlan Jung <quin@expo.io>
2023-04-19 20:49:35 +0200

    Update packages/expo-cli/src/commands/publish/deprecationNotice.ts
    Co-authored-by: James Ide <ide@users.noreply.github.com>

--- packages/expo-cli/src/commands/publish/deprecationNotice.ts
+++ packages/expo-cli/src/commands/publish/deprecationNotice.ts
@@ -2,7 +2,7 @@ import Log from '../../log';
 import { learnMore } from '../utils/TerminalLink';
 
 export async function printDeprecationNotice(): Promise<void> {
-  Log.warn('The last day to use expo publish is 2024-02-12. Migrate to eas update.');
+  Log.warn('The last day to use expo publish is 2024-02-12 and SDK 49 is the last version to support it. Migrate to eas update.');
   Log.warn(
     `${learnMore('https://blog.expo.dev/sunsetting-expo-publish-and-classic-updates-6cb9cd295378')}`
   );
