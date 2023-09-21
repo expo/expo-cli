a100d1bfd469ddff08213e85a7bde4b3ce350484
Kadi Kraman <kadi@expo.io>
2023-09-21 11:29:35 +0100

    [image-utils] [ENG-9956] add a function to check whether a PNG is valid (#4761)
    * Add a function to check whether a PNG is valid
    
    * Remove unused assignment
    
    * Add missing type
    
    * Add another missing type
    
    * Convert isValidPng to getPngInfo

--- packages/image-utils/src/index.ts
+++ packages/image-utils/src/index.ts
@@ -1,5 +1,10 @@
 import * as Cache from './Cache';
-import { compositeImagesAsync, generateFaviconAsync, generateImageAsync } from './Image';
+import {
+  compositeImagesAsync,
+  generateFaviconAsync,
+  generateImageAsync,
+  getPngInfo,
+} from './Image';
 import { ImageFormat, ImageOptions, ResizeMode } from './Image.types';
 import { convertFormat, jimpAsync } from './jimp';
 import { findSharpInstanceAsync, isAvailableAsync, sharpAsync } from './sharp';
@@ -27,6 +32,7 @@ export {
   generateFaviconAsync,
   Cache,
   compositeImagesAsync,
+  getPngInfo,
 };
 
 export { SharpGlobalOptions, SharpCommandOptions, ResizeMode, ImageFormat, ImageOptions };
