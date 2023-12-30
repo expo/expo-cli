29cfc7afc02a73f7ea896700f6214f05c53d9479
Cedric van Putten <me@bycedric.com>
2023-12-30 14:28:15 +0100

    refactor: move webpack integrations to `expo/expo-webpack-integrations` (#4802)
    * refactor: move webpack integrations to `expo/expo-webpack-integrations`
    
    * fix(expo-cli): remove unused `@ts-expect-error` directive
    
    * chore: rebuild lockfile

--- packages/webpack-config/e2e/custom-homepage/index.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { AppRegistry, View } from 'react-native';
-
-const App = () => <View testID="e2e-default-view" />;
-AppRegistry.registerComponent('main', () => App);
-
-const rootTag = document.getElementById('root');
-AppRegistry.runApplication('main', { rootTag });
